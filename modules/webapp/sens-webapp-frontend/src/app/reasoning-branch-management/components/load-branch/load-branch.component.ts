97293710333b1afd6cb016a3d564e02eb5c7863f
Patryk Górski <patryk.gorski@silenteight.com>
2020-02-21 08:47:43 +0000

    Enhancement: Validation for the RB ID input field
    Enhancement: Validation for the RB ID input field
    
    ![screenshot.293](/uploads/b0d6b3aaa7dc6aa69dd6508a8ade122e/screenshot.293.png)
    
    ![screenshot.294](/uploads/dbafec86f59d45a8ea0ac7713f31c44d/screenshot.294.png)
    
    ![screenshot.295](/uploads/45efdc10134f2520c822897e7b8cab7f/screenshot.295.png)
    
    ![screenshot.296](/uploads/f754cb960ecf07798c69a04fa45b19d9/screenshot.296.png)
    
    Reviewed-by: Karol Lotkowski <klotkowski@silenteight.com>
    Reviewed-by: Bogdan Gulowaty <bgulowaty@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/sens-webapp/merge_requests/79

--- modules/webapp/sens-webapp-frontend/src/app/reasoning-branch-management/components/load-branch/load-branch.component.ts
+++ modules/webapp/sens-webapp-frontend/src/app/reasoning-branch-management/components/load-branch/load-branch.component.ts
@@ -8,15 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class LoadBranchComponent implements OnInit {
 
   enteredID: string;
+  validInput = true;
+
+  basicIdVerification = new RegExp('[1-9]+|[1-9]+-+[0-9]+');
+  fullIdCheck = new RegExp('[1-9]\d*-\d+');
 
   constructor() { }
 
   onInput(value: string) {
     this.enteredID = value;
+    this.validateInput(this.basicIdVerification);
   }
 
   onPaste(event: ClipboardEvent) {
     this.enteredID = event.clipboardData.toString();
+    this.validateInput(this.basicIdVerification);
+  }
+
+  validateInput(value) {
+    if (this.enteredID) {
+      this.validInput = value.test(this.enteredID);
+    } else {
+      this.validInput = true;
+    }
   }
 
   ngOnInit() {
