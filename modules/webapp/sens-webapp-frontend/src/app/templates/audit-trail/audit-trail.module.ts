a7902e47880b0ba662bad689519190d2b9f11bc0
Patryk Górski <patryk.gorski@silenteight.com>
2020-03-02 09:07:19 +0000

    Enhancement: Rebuild the page layout with reports (WA-408)
    Closes WA-408
    
    ![screenshot.343](/uploads/fa2c9d6f5c4ff8f74917628f7993ebe6/screenshot.343.png)
    
    Reviewed-by: Karol Lotkowski <klotkowski@silenteight.com>
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/sens-webapp/merge_requests/113

--- modules/webapp/sens-webapp-frontend/src/app/templates/audit-trail/audit-trail.module.ts
+++ modules/webapp/sens-webapp-frontend/src/app/templates/audit-trail/audit-trail.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { ApplicationHeaderModule } from '@app/components/application-header/application-header.module';
 import { SharedModule } from '../../shared/shared.module';
 import { AuditTrailComponent } from './audit-trail.component';
 import { GenerateReportComponent } from './generate-report/generate-report.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    ApplicationHeaderModule
   ],
   declarations: [
     GenerateReportComponent,
