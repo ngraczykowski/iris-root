35574fe53dc4c0f767cbaceb2f721f8948d289f6
Mateusz Bartecki <mbartecki@silenteight.com>
2020-05-04 09:37:55 +0000

    INTERNAL: Add plugin for chrome extention follow up 2 (RP-1545)
    Closes RP-1545
    
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Reviewed-by: Marcin Świerczyński <mswierczynski@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/sens-webapp/merge_requests/301

---
+++ modules/webapp/sens-webapp-scb-chrome-extension/src/main/java/com/silenteight/sens/webapp/scb/chrome/extension/ChromeExtensionProperties.java
@@ -0,0 +1,25 @@
+package com.silenteight.sens.webapp.scb.chrome.extension;
+
+import lombok.Data;
+
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Validated
+public class ChromeExtensionProperties {
+
+  private String authUrl;
+  private String recommendationUrl;
+  private String openRecordUrlPattern;
+  private String solutionUrlPattern;
+  private String hitsUrlPattern;
+  @NotBlank
+  private String logLevel;
+  @Min(1)
+  private int commentLengthThreshold;
+  @Min(1)
+  private int refreshIntervalInMs;
+}
---
+++ modules/webapp/sens-webapp-scb-chrome-extension/src/test/java/com/silenteight/sens/webapp/scb/chrome/extension/rest/ChromeExtensionRestControllerTest.java
@@ -0,0 +1,42 @@
+package com.silenteight.sens.webapp.scb.chrome.extension.rest;
+
+import com.silenteight.sens.webapp.common.testing.rest.BaseRestControllerTest;
+import com.silenteight.sens.webapp.common.testing.rest.testwithrole.TestWithRole;
+import com.silenteight.sens.webapp.scb.chrome.extension.GetChromeExtensionConfigurationUseCase;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+import static com.silenteight.sens.webapp.common.testing.rest.TestRoles.ADMIN;
+import static com.silenteight.sens.webapp.common.testing.rest.TestRoles.ANALYST;
+import static com.silenteight.sens.webapp.common.testing.rest.TestRoles.AUDITOR;
+import static com.silenteight.sens.webapp.common.testing.rest.TestRoles.BUSINESS_OPERATOR;
+import static com.silenteight.sens.webapp.scb.chrome.extension.rest.ChromeExtensionRestControllerFixtures.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.HttpStatus.OK;
+
+@Import({ ChromeExtensionRestController.class })
+class ChromeExtensionRestControllerTest extends BaseRestControllerTest {
+
+  private static final String GET_CONFIGURATION_PATH = "/chrome-extension/config";
+
+  @MockBean
+  private GetChromeExtensionConfigurationUseCase getConfigurationUseCase;
+
+  @TestWithRole(roles = { ADMIN, ANALYST, AUDITOR, BUSINESS_OPERATOR })
+  void its200WithCorrectBody_whenAvailable() {
+    given(getConfigurationUseCase.apply()).willReturn(CHROME_EXTENSION_CONFIGURATION);
+
+    get(GET_CONFIGURATION_PATH)
+        .statusCode(OK.value())
+        .body("authUrl", equalTo(AUTH_URL))
+        .body("recommendationUrl", equalTo(RECOMMENDATION_URL))
+        .body("gnsUrlPattern.openRecord", equalTo(OPEN_RECORD))
+        .body("gnsUrlPattern.solution", equalTo(SOLUTION))
+        .body("gnsUrlPattern.hits", equalTo(HITS))
+        .body("sensLogLevel", equalTo(SENS_LOG_LEVEL))
+        .body("commentLengthThreshold", equalTo(COMMENT_LENGTH_THRESHOLD))
+        .body("refreshIntervalInMs", equalTo(REFRESH_INTERVAL_IN_MS));
+  }
+}
