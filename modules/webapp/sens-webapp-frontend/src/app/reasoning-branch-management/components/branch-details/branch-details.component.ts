0d3ca0d7f19faaf5516ace51e702fc500096a416
Patryk Górski <patryk.gorski@silenteight.com>
2020-06-02 10:57:49 +0000

    Enhancement: Removed old Reasoning Branch Management
    Enhancement: Removed old Reasoning Branch Management
    
    Closes WA-797
    
    Reviewed-by: Adam Kożuchowski <akozuchowski@silenteight.com>
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/sens-webapp/-/merge_requests/388

--- modules/webapp/sens-webapp-frontend/src/app/reasoning-branch-management/components/branch-details/branch-details.component.ts
+++ modules/webapp/sens-webapp-frontend/src/app/reasoning-branch-management/components/branch-details/branch-details.component.ts
@@ -1,81 +1,53 @@
 import {
-  ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
-  OnChanges,
   OnInit,
   Output
 } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReasoningBranchDetails } from '@app/reasoning-branch-management/models/reasoning-branch-management';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-branch-details',
   templateUrl: './branch-details.component.html',
   styleUrls: ['./branch-details.component.scss']
 })
-export class BranchDetailsComponent implements OnInit, OnChanges {
-  isOriginalValue = true;
+export class BranchDetailsComponent implements OnInit {
 
   branchForm: FormGroup = new FormGroup({
     aiSolution: new FormControl(null, Validators.required),
     active: new FormControl(null, Validators.required)
   });
 
-  aiSolutions = [
-    {
-      label: 'No Decision',
-      key: 'NO_DECISION'
-    },
-    {
-      label: 'False Positive',
-      key: 'FALSE_POSITIVE'
-    },
-    {
-      label: 'Potential True Positive',
-      key: 'POTENTIAL_TRUE_POSITIVE'
-    }
-  ];
-
   @Input() fullId: string;
   @Input() branchDetails: ReasoningBranchDetails;
   @Output() confirm = new EventEmitter();
 
   constructor(
-    private changeDetector: ChangeDetectorRef
+      private translate: TranslateService
   ) { }
 
   ngOnInit() {
-    this.reset();
-
-    this.branchForm.valueChanges.subscribe(data => {
-      this.isOriginalValue =
-        this.checkIsFormValueOriginal(data, {aiSolution: this.branchDetails.aiSolution, active: this.branchDetails.active});
-    });
-
-    this.branchForm.valueChanges.subscribe(data => {
-      this.isOriginalValue =
-        this.checkIsFormValueOriginal(data, {aiSolution: this.branchDetails.aiSolution, active: this.branchDetails.active});
-    });
   }
 
-  ngOnChanges() {
-    this.changeDetector.detectChanges();
-    this.reset();
-  }
+  convertSolution(solution) {
+    const solutionTranslatePrefix = 'aiSolutions.';
 
-  reset() {
-    this.branchForm.controls.aiSolution.setValue(this.branchDetails.aiSolution);
-    this.branchForm.controls.active.setValue(this.branchDetails.active);
-    this.branchForm.markAsPristine();
-  }
-
-  submit() {
-    this.confirm.emit(this.branchForm.value);
-  }
-
-  private checkIsFormValueOriginal(currentData, originalData): boolean {
-    return JSON.stringify(currentData) === JSON.stringify(originalData);
+    switch (solution) {
+      case 'NO_DECISION': {
+        return this.translate.instant(solutionTranslatePrefix + 'noDecision');
+      }
+      case 'FALSE_POSITIVE': {
+        return this.translate.instant(solutionTranslatePrefix + 'falsePositive');
+      }
+      case 'POTENTIAL_TRUE_POSITIVE': {
+        return this.translate.instant(solutionTranslatePrefix + 'potentialTruePositive');
+      }
+      default: {
+        return solution;
+      }
+    }
   }
 }
