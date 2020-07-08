cf22d84df430dfefa933b29337382a5b63806128
Marek Koronski <mkoronski@silenteight.com>
2020-07-08 13:05:33 +0000

    Bugfix: Circuit breaker dashboard - error state doesn't disappear
    Reviewed-by: Patryk Górski <pgorski@silenteight.com>
    Reviewed-by: Marek Mastyło <mmastylo@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/sens-webapp/-/merge_requests/482

--- modules/webapp/sens-webapp-frontend/src/app/circuit-breaker-dashboard/containers/circuit-breaker-dashboard/circuit-breaker-dashboard.component.ts
+++ modules/webapp/sens-webapp-frontend/src/app/circuit-breaker-dashboard/containers/circuit-breaker-dashboard/circuit-breaker-dashboard.component.ts
@@ -63,6 +63,7 @@ export class CircuitBreakerDashboardComponent implements OnInit {
   resetView() {
     this.circuitBreakerLoading = false;
     this.circuitBreakerEmptyState = false;
+    this.circuitBreakerError = false;
     this.circuitBreakerList = false;
   }
 
