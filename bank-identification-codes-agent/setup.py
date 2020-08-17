b27af1a0144d11b97200de223c74d19252c84013
jenkins <jenkins@silenteight.com>
2020-08-17 07:30:49 +0000

    Bump version: 1.1.0-dev → 1.2.0-dev

--- bank-identification-codes-agent/setup.py
+++ bank-identification-codes-agent/setup.py
@@ -12,7 +12,7 @@ tests_require = [
 ]
 setup(
     name="ts-identification-mismatch-agent",
-    version="1.1.0-dev",
+    version="1.2.0-dev",
     description="Transaction Screening Identification Mismatch Agent",
     author="Silent Eight Pte. Ltd.",
     author_email="proj-scb-tsaas@silenteight.com",
