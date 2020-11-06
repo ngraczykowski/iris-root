de5b9ed544b4efaf45779600833a54959ef5ac3b
Michał Kozioł <mkoziol@silenteight.com>
2020-11-06 08:32:04 +0000

    INTERNAL: SERP-198 fixing serp logs - bridge log
    This fixes the ugly exception during startup of bridge:
    
    ```
    2020-11-05 15:03:37.286 DEBUG [scb-bridge,,,] 413986 --- [           main] o.s.b.a.t.TransactionManagerCustomizers  : Non-matching PlatformTransactionManager type for callback PlatformTransactionManagerCustomizer: com.silenteight.sep.base.common.database.TransactionManagerAutoConfiguration$$Lambda$1256/0x0000000100ddfc40@5b8ee649
    
    java.lang.ClassCastException: class org.springframework.jdbc.datasource.DataSourceTransactionManager cannot be cast to class org.springframework.orm.jpa.JpaTransactionManager (org.springframework.jdbc.datasource.DataSourceTransactionManager and org.springframework.orm.jpa.JpaTransactionManager are in unnamed module of loader 'app')
    ...
    ```
    
    Reviewed-by: Bartosz Martofel <bmartofel@silenteight.com>
    Reviewed-by: Bogdan Gulowaty <bgulowaty@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sep/sep-base/-/merge_requests/25

--- modules/sep-base/sep-base-common/src/main/java/com/silenteight/sep/base/common/database/TransactionManagerAutoConfiguration.java
+++ modules/sep-base/sep-base-common/src/main/java/com/silenteight/sep/base/common/database/TransactionManagerAutoConfiguration.java
@@ -6,13 +6,19 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.transaction.PlatformTransactionManagerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @RequiredArgsConstructor
 @ConditionalOnClass(JpaTransactionManager.class)
 public class TransactionManagerAutoConfiguration {
 
   @Bean
-  PlatformTransactionManagerCustomizer<JpaTransactionManager> jpaTransactionManagerCustomizer() {
-    return transactionManager -> transactionManager.setNestedTransactionAllowed(true);
+  PlatformTransactionManagerCustomizer<PlatformTransactionManager>
+      jpaTransactionManagerCustomizer() {
+    return transactionManager -> {
+      if (transactionManager instanceof JpaTransactionManager) {
+        ((JpaTransactionManager) transactionManager).setNestedTransactionAllowed(true);
+      }
+    };
   }
 }
