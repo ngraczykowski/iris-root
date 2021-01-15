e455a1487a4a0a00a1b2cd241dab8f4bfd5ddea8
Kamil Dzięcioł <kdzieciol+gitlab@silenteight.com>
2021-01-15 11:39:43 +0000

    INTERNAL: Rename FeatureEntityDto to MatchConditionDto (WEB-460)
    Closes WEB-460
    
    Reviewed-by: Adam Nowicki <anowicki@silenteight.com>
    Reviewed-by: Marek Mastyło <mmastylo@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/serp-governance/-/merge_requests/30

--- modules/governance/governance-policy/src/main/java/com/silenteight/serp/governance/policy/domain/dto/MatchConditionDto.java
+++ modules/governance/governance-policy/src/main/java/com/silenteight/serp/governance/policy/domain/dto/MatchConditionDto.java
@@ -2,16 +2,20 @@ package com.silenteight.serp.governance.policy.domain.dto;
 
 import lombok.*;
 
+import com.silenteight.serp.governance.policy.domain.Condition;
+
 import java.util.Collection;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class FeatureDto {
+public class MatchConditionDto {
 
   @NonNull
   private String name;
+  @NonNull
+  private Condition condition;
   @NonNull
   private Collection<String> values;
 }
