35ee54d4e00ee2ee5d8fe80688930bc47dbf2157
Kamil Dzięcioł <kdzieciol+gitlab@silenteight.com>
2021-02-05 13:04:30 +0000

    Enhancement: Add warnings in logs when no policy or when no steps in policy (WEB-462)
    Closes WEB-462
    
    Reviewed-by: Adam Nowicki <anowicki@silenteight.com>
    Reviewed-by: Marek Mastyło <mmastylo@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/serp-governance/-/merge_requests/65

--- modules/governance/governance-policy/src/main/java/com/silenteight/serp/governance/policy/solve/InUsePolicyStepsSupplier.java
+++ modules/governance/governance-policy/src/main/java/com/silenteight/serp/governance/policy/solve/InUsePolicyStepsSupplier.java
@@ -2,6 +2,7 @@ package com.silenteight.serp.governance.policy.solve;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.serp.governance.policy.domain.PolicyPromotedEvent;
 import com.silenteight.serp.governance.policy.step.PolicyStepsConfigurationQuery;
@@ -10,12 +11,18 @@ import org.springframework.context.event.EventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor
+@Slf4j
 class InUsePolicyStepsSupplier implements StepsConfigurationSupplier, InUsePolicyLoader {
+
+  private static final String NO_IN_USE_POLICY_WARNING_MESSAGE =
+      "No Policy Version is in status 'Used for Solving'. All alerts will be solved "
+          + "as 'No Decision' due to no active policy.";
 
   @NonNull
   private final PolicyStepsConfigurationQuery stepsConfigurationQuery;
@@ -24,11 +31,26 @@ class InUsePolicyStepsSupplier implements StepsConfigurationSupplier, InUsePolic
   @NonNull
   private final StepMapper stepMapper;
 
+  private UUID policyId;
+
   private List<Step> steps = new ArrayList<>();
 
   @Override
   public List<Step> get() {
+    logIfNoPolicy();
+    logIfNoSteps();
     return steps;
+  }
+
+  private void logIfNoPolicy() {
+    if (policyId == null)
+      log.warn("No policy configured. Alert will be solved as 'No Decision'");
+  }
+
+  private void logIfNoSteps() {
+    if (policyId != null && steps.isEmpty())
+      log.warn("No steps configured in policy {}. Alert will be solved as 'No Decision'.",
+               policyId.toString());
   }
 
   @EventListener
@@ -38,10 +60,18 @@ class InUsePolicyStepsSupplier implements StepsConfigurationSupplier, InUsePolic
 
   @Override
   public void loadInUsePolicy() {
-    inUsePolicyQuery.getPolicyInUse().ifPresent(this::loadSteps);
+    Optional<UUID> policyInUse = inUsePolicyQuery.getPolicyInUse();
+    if (policyInUse.isEmpty()) {
+      log.warn(NO_IN_USE_POLICY_WARNING_MESSAGE);
+      policyId = null;
+      return;
+    }
+
+    loadSteps(policyInUse.get());
   }
 
   private void loadSteps(UUID policyId) {
+    this.policyId = policyId;
     steps = stepsConfigurationQuery
         .listStepsConfiguration(policyId)
         .stream()
