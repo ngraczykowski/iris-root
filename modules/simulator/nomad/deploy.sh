efb6721ed6aaf37ad085bd39276a2def06069b6b
Tomasz Bożek <tbozek@silenteight.com>
2021-02-18 07:22:19 +0000

    INTERNAL: add checksum to artifact file for nomad deployment

--- modules/simulator/nomad/deploy.sh
+++ modules/simulator/nomad/deploy.sh
@@ -16,6 +16,7 @@ export NOMAD_ADDR="${NOMAD_ADDR:-http://localhost:4646}"
 
 simulator_artifact=$(basename -- "$(ls "$scriptdir"/../simulator-app/build/libs/simulator*-exec.jar)")
 export NOMAD_VAR_simulator_artifact=${NOMAD_VAR_simulator_artifact:-"s3::${MINIO_ADDR}/artifacts/simulator/${simulator_artifact}"}
+export NOMAD_VAR_simulator_artifact_checksum=${NOMAD_VAR_simulator_artifact_checksum:-"sha256:$(sha256sum "$scriptdir"/../simulator-app/build/libs/"$simulator_artifact" | awk '{print $1}')"}
 
 cd "$scriptdir"
 
