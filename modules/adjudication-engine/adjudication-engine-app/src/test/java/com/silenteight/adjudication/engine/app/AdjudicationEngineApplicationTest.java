bc5b77835e52786c00dbff1699de9d67bd8e81b8
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2021-03-03 13:08:49 +0100

    Add test to generate test publisher report

---
+++ modules/adjudication-engine/adjudication-engine-app/src/test/java/com/silenteight/adjudication/engine/app/AdjudicationEngineApplicationTest.java
@@ -0,0 +1,12 @@
+package com.silenteight.adjudication.engine.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AdjudicationEngineApplication.class)
+class AdjudicationEngineApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+}
