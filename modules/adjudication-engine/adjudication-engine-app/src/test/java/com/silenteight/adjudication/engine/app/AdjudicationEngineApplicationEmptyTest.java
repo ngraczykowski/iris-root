d1f84c6aa6a95557e23f43cd3c3cbf160691e6a8
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2021-03-03 13:11:21 +0100

    Change test to normal junit test

---
+++ modules/adjudication-engine/adjudication-engine-app/src/test/java/com/silenteight/adjudication/engine/app/AdjudicationEngineApplicationEmptyTest.java
@@ -0,0 +1,11 @@
+package com.silenteight.adjudication.engine.app;
+
+import org.junit.jupiter.api.Test;
+
+// TODO: Empty test to generate test reports, remove when real tests will be added
+class AdjudicationEngineApplicationEmptyTest {
+
+  @Test
+  void emptyTest() {
+  }
+}
