8c7a2516849c80582271b75c28bcc95f18dee8fb
Kamil Dzięcioł <kdzieciol@silenteight.com>
2021-03-04 08:26:18 +0100

    INTERNAL: Add changelog and dummy test

---
+++ modules/warehouse/warehouse-app/src/test/java/com/silenteight/warehouse/WarehouseApplicationTest.java
@@ -0,0 +1,13 @@
+package com.silenteight.warehouse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarehouseApplicationTest {
+
+  @Test
+  void name() {
+    assertTrue(true);
+  }
+}
