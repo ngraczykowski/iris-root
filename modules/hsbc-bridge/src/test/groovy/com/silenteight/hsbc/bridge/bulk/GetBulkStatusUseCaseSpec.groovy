d26f540b927607a29557c632caaf631e7a59fac2
Adam Wójcicki <awojcicki@silenteight.com>
2021-03-10 06:39:20 +0000

    Enhancement: GetBulkTestStatus (ALLIN-34) implement verification for alert case ID and bulkItem status

---
+++ modules/hsbc-bridge/src/test/groovy/com/silenteight/hsbc/bridge/bulk/GetBulkStatusUseCaseSpec.groovy
@@ -0,0 +1,34 @@
+package com.silenteight.hsbc.bridge.bulk
+
+import com.silenteight.hsbc.bridge.alert.Alert
+import com.silenteight.hsbc.bridge.bulk.repository.BulkRepository
+
+import spock.lang.Specification
+
+class GetBulkStatusUseCaseSpec extends Specification {
+
+  def bulkRepository = Mock(BulkRepository)
+  def underTest = new GetBulkStatusUseCase(bulkRepository)
+
+  def 'should get bulk status'() {
+    given:
+    var alert = new Alert(caseId: 100)
+    var bulkItem = new BulkItem(alert)
+    def bulk = new Bulk(items: [bulkItem])
+
+    when:
+    def result = underTest.getStatus(bulk.id)
+
+    then:
+    1 * bulkRepository.findById(_ as UUID) >> bulk
+    with(result) {
+      bulkId == bulk.id
+      bulkStatus.name() == bulk.status.name()
+      with(requestedAlerts) {
+        size() == bulk.items.size()
+        first().id == bulk.items[0].alert.caseId
+        first().status.name() == bulk.items[0].status.name()
+      }
+    }
+  }
+}
