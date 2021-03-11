23a82f96cf132fe4daf808c1f0ce2fbc45468cc8
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-03-11 09:40:31 +0000

    Refine AnalysisService mock with detailed responses
    Reviewed-by: Szymon Pacanowski <spacanowski@silenteight.com>
    Reviewed-by: Tadeusz Kleszcz <tkleszcz@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/adjudication-engine/-/merge_requests/10

--- modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/AnalysisGrpcServiceMock.java
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/AnalysisGrpcServiceMock.java
@@ -1,6 +1,7 @@
 package com.silenteight.adjudication.engine.analysis;
 
 import com.silenteight.adjudication.api.v1.*;
+import com.silenteight.adjudication.api.v1.Analysis.FeaturesDefinition;
 import com.silenteight.adjudication.api.v1.AnalysisServiceGrpc.AnalysisServiceImplBase;
 
 import com.google.protobuf.Timestamp;
@@ -12,7 +13,6 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
-import static com.google.protobuf.util.Timestamps.fromSeconds;
 import static io.grpc.Status.INVALID_ARGUMENT;
 import static io.grpc.Status.fromCode;
 import static java.util.concurrent.ThreadLocalRandom.current;
@@ -20,13 +20,15 @@ import static java.util.concurrent.ThreadLocalRandom.current;
 @GrpcService
 class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
 
-  private static final List<String> MATCHES = List.of("NO_MATCH", "MATCH");
   private static final List<String> SOLUTIONS =
-      List.of("SOLUTION A", "SOLUTION B", "SOLUTION C", "SOLUTION D");
+      List.of("FALSE_POSITIVE", "POTENTIAL_TRUE_POSITIVE", "NO_DECISION");
+  private static final List<String> ACTIONS =
+      List.of("INVESTIGATE", "FALSE_POSITIVE", "POTENTIAL_TRUE_POSITIVE", "HINTED_FALSE_POSITIVE");
 
   @Override
-  public void createAnalysis(CreateAnalysisRequest request,
-                             StreamObserver<Analysis> responseObserver) {
+  public void createAnalysis(
+      CreateAnalysisRequest request,
+      StreamObserver<Analysis> responseObserver) {
     if (!request.hasAnalysis() ||
         request.getAnalysis().getPolicy().isBlank() ||
         request.getAnalysis().getStrategy().isBlank()) {
@@ -37,18 +39,18 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
     }
 
     responseObserver.onNext(Analysis
-                    .newBuilder(request.getAnalysis())
-                    .setName("analysis/" + UUID.randomUUID())
-                    .setCreateTime(Timestamp.newBuilder()
-                                            .setSeconds(Instant.now().getEpochSecond()))
-                    .setState(Analysis.State.NEW)
-                    .build());
+        .newBuilder(request.getAnalysis())
+        .setName("analysis/" + UUID.randomUUID())
+        .setCreateTime(makeTimestampNow())
+        .setState(Analysis.State.NEW)
+        .build());
     responseObserver.onCompleted();
   }
 
   @Override
-  public void addDataset(AddDatasetRequest request,
-                         StreamObserver<AnalysisDataset> responseObserver) {
+  public void addDataset(
+      AddDatasetRequest request,
+      StreamObserver<AnalysisDataset> responseObserver) {
     if (request.getAnalysis().isBlank() || request.getDataset().isBlank()) {
       responseObserver.onError(fromCode(INVALID_ARGUMENT.getCode())
           .withDescription("Analysis and dataset name must be set")
@@ -66,8 +68,9 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
   }
 
   @Override
-  public void batchAddDatasets(BatchAddDatasetsRequest request,
-                               StreamObserver<BatchAddDatasetsResponse> responseObserver) {
+  public void batchAddDatasets(
+      BatchAddDatasetsRequest request,
+      StreamObserver<BatchAddDatasetsResponse> responseObserver) {
     if (request.getAnalysis().isBlank() || request.getDatasetsCount() == 0) {
       responseObserver.onError(fromCode(INVALID_ARGUMENT.getCode())
           .withDescription("Analysis name and datasets must be set")
@@ -91,8 +94,9 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
   }
 
   @Override
-  public void getAnalysis(GetAnalysisRequest request,
-                          StreamObserver<Analysis> responseObserver) {
+  public void getAnalysis(
+      GetAnalysisRequest request,
+      StreamObserver<Analysis> responseObserver) {
     if (request.getAnalysis().isBlank()) {
       responseObserver.onError(fromCode(INVALID_ARGUMENT.getCode())
           .withDescription("Analysis name must be set")
@@ -100,20 +104,33 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
       return;
     }
 
+    var pendingAlerts = current().nextInt(1, 2000);
+    var alertCount = current().nextInt(pendingAlerts, pendingAlerts + 10000);
+
     responseObserver.onNext(Analysis
         .newBuilder()
         .setName(request.getAnalysis())
-        .setPolicy("policy/54317992-a203-4e95-b145-b47d6a3aef0a")
-        .setStrategy("strategy/back_test")
-        .setCreateTime(fromSeconds(1614945644))
+        .setPolicy("policies/" + UUID.randomUUID().toString())
+        .setStrategy("strategies/back_test")
+        .addFeatures(FeaturesDefinition.newBuilder()
+            .setAgentConfig("agents/country/versions/2.0.0/config/1")
+            .addAllFeatures(List.of("features/nationality", "features/residency")))
+        .addFeatures(FeaturesDefinition.newBuilder()
+            .setAgentConfig("agents/name/versions/3.1.0/config/1")
+            .addAllFeatures(List.of("features/name")))
+        .setCreateTime(makePastTimestamp())
         .setState(Analysis.State.NEW)
+        .setPendingAlerts(pendingAlerts)
+        .setAlertCount(alertCount)
+        .putLabels("batch_type", "US_PERD_DENY")
         .build());
     responseObserver.onCompleted();
   }
 
   @Override
-  public void streamRecommendations(StreamRecommendationsRequest request,
-                                    StreamObserver<Recommendation> responseObserver) {
+  public void streamRecommendations(
+      StreamRecommendationsRequest request,
+      StreamObserver<Recommendation> responseObserver) {
     if (request.getAnalysis().isBlank()) {
       responseObserver.onError(fromCode(INVALID_ARGUMENT.getCode())
           .withDescription("Analysis name must be set")
@@ -121,19 +138,25 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
       return;
     }
 
-    IntStream.range(0, current().nextInt(1, 10))
-        .forEach(i -> responseObserver.onNext(Recommendation
-            .newBuilder()
-            .setName("analysis/" + request.getAnalysis() +
-                      "/recommendations/" + UUID.randomUUID())
-            .setCreateTime(fromSeconds(1614945644))
-            .build()));
+    IntStream.range(0, current().nextInt(1, 10)).forEach(i -> {
+      var action = ACTIONS.get(current().nextInt(0, ACTIONS.size()));
+
+      responseObserver.onNext(Recommendation
+          .newBuilder()
+          .setName(request.getAnalysis() + "/recommendations/" + i)
+          .setCreateTime(makePastTimestamp())
+          .setRecommendationComment(
+              "That is a " + action.toLowerCase().replace('_', ' ') + ", mate!")
+          .setRecommendedAction(action)
+          .build());
+    });
     responseObserver.onCompleted();
   }
 
   @Override
-  public void getRecommendation(GetRecommendationRequest request,
-                                StreamObserver<Recommendation> responseObserver) {
+  public void getRecommendation(
+      GetRecommendationRequest request,
+      StreamObserver<Recommendation> responseObserver) {
     if (request.getRecommendation().isBlank()) {
       responseObserver.onError(fromCode(INVALID_ARGUMENT.getCode())
           .withDescription("Recommendation name must be set")
@@ -141,17 +164,22 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
       return;
     }
 
+    var action = ACTIONS.get(current().nextInt(0, ACTIONS.size()));
+
     responseObserver.onNext(Recommendation
         .newBuilder()
         .setName(request.getRecommendation())
-        .setCreateTime(fromSeconds(1614945644))
+        .setCreateTime(makePastTimestamp())
+        .setRecommendationComment("That is a " + action.toLowerCase().replace('_', ' ') + ", mate!")
+        .setRecommendedAction(action)
         .build());
     responseObserver.onCompleted();
   }
 
   @Override
-  public void streamMatchSolutions(StreamMatchSolutionsRequest request,
-                                   StreamObserver<MatchSolution> responseObserver) {
+  public void streamMatchSolutions(
+      StreamMatchSolutionsRequest request,
+      StreamObserver<MatchSolution> responseObserver) {
     if (request.getAnalysis().isBlank()) {
       responseObserver.onError(fromCode(INVALID_ARGUMENT.getCode())
           .withDescription("Analysis name must be set")
@@ -162,18 +190,18 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
     IntStream.range(0, current().nextInt(1, 10))
         .forEach(i -> responseObserver.onNext(MatchSolution
             .newBuilder()
-            .setName("analysis/" + request.getAnalysis() +
-                      "/match_solutions/" + UUID.randomUUID())
-            .setMatch(MATCHES.get(current().nextInt(0, MATCHES.size())))
-            .setCreateTime(fromSeconds(1614945644))
+            .setName(request.getAnalysis() + "/match-solutions/" + i)
+            .setMatch("matches/" + current().nextInt(1000, 10000))
+            .setCreateTime(makePastTimestamp())
             .setSolution(SOLUTIONS.get(current().nextInt(0, SOLUTIONS.size())))
             .build()));
     responseObserver.onCompleted();
   }
 
   @Override
-  public void getMatchSolution(GetMatchSolutionRequest request,
-                               StreamObserver<MatchSolution> responseObserver) {
+  public void getMatchSolution(
+      GetMatchSolutionRequest request,
+      StreamObserver<MatchSolution> responseObserver) {
     if (request.getMatchSolution().isBlank()) {
       responseObserver.onError(fromCode(INVALID_ARGUMENT.getCode())
           .withDescription("Match solution name must be set")
@@ -184,10 +212,27 @@ class AnalysisGrpcServiceMock extends AnalysisServiceImplBase {
     responseObserver.onNext(MatchSolution
         .newBuilder()
         .setName(request.getMatchSolution())
-        .setMatch("NO_MATCH")
-        .setCreateTime(fromSeconds(1614945644))
-        .setSolution("Investigate")
+        .setMatch("matches/" + current().nextInt(1000, 10000))
+        .setCreateTime(makePastTimestamp())
+        .setSolution(ACTIONS.get(current().nextInt(0, ACTIONS.size())))
         .build());
     responseObserver.onCompleted();
+  }
+
+  private Timestamp makeTimestampNow() {
+    return makeTimestamp(Instant.now());
+  }
+
+  private Timestamp makePastTimestamp() {
+    // NOTE(ahaczewski): 7*24*60*60 = 604800 seconds = 7 days.
+    return makeTimestamp(Instant.now().minusSeconds(current().nextInt(60, 7 * 24 * 60 * 60)));
+  }
+
+  private Timestamp makeTimestamp(Instant instant) {
+    return Timestamp
+        .newBuilder()
+        .setSeconds(instant.getEpochSecond())
+        .setNanos(instant.getNano())
+        .build();
   }
 }
