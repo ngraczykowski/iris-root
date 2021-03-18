62b73a03ac979ce4e06dec0c9fbfe42cfdabff36
Adam Nowicki <anowicki@silenteight.com>
2021-03-18 13:19:11 +0000

    INTERNAL: revert back rabbitmq configuration
    Closes WEB-700
    
    Reviewed-by: Marek Mastyło <mmastylo@silenteight.com>
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/serp-governance/-/merge_requests/93

---
+++ modules/governance/governance-common-web/src/main/java/com/silenteight/serp/governance/common/integration/RabbitModuleConfiguration.java
@@ -0,0 +1,26 @@
+package com.silenteight.serp.governance.common.integration;
+
+import com.silenteight.sep.base.common.protocol.MessageRegistry;
+import com.silenteight.sep.base.common.protocol.MessageRegistryFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+class RabbitModuleConfiguration {
+
+  @Bean
+  @Primary
+  MessageRegistry messageRegistryOverwrite() {
+    MessageRegistryFactory factory = new MessageRegistryFactory(
+        "com.silenteight.model.api",
+        "com.silenteight.solving.api",
+        "com.google.protobuf",
+        "com.google.rpc",
+        "com.google.type"
+    );
+
+    return factory.create();
+  }
+}
