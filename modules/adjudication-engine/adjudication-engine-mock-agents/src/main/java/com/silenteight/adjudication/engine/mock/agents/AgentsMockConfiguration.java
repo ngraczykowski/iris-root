9e661afc195b1a3f168aa6512e08faa64ba43cf0
Tomasz Bożek <tbozek@silenteight.com>
2021-03-26 12:24:40 +0000

    Mock agent responses for testing  (CS-356)
    Mock Rabbitmq connection factory to handle agent or other services response.
    Use mock-agents profile to enable rabbitmq mocks. To mock responses you just need to add Spring Integration definitions to listen on queues like agents and send mocked response back via rabbitmq response queues. This allow to test the full path of request/response via mocked rabbitmq.
    
    Reviewed-by: Andrzej Haczewski <ahaczewski@silenteight.com>
    Reviewed-by: Tadeusz Kleszcz <tkleszcz@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/adjudication-engine/-/merge_requests/28

---
+++ modules/adjudication-engine/adjudication-engine-mock-agents/src/main/java/com/silenteight/adjudication/engine/mock/agents/AgentsMockConfiguration.java
@@ -0,0 +1,22 @@
+package com.silenteight.adjudication.engine.mock.agents;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.github.fridujo.rabbitmq.mock.MockConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("mock-agents")
+@Slf4j
+class AgentsMockConfiguration {
+
+  @Bean
+  CachingConnectionFactory connectionFactory() {
+    log.info("Mock Agents RabbitMQ connection factory injected");
+    return new CachingConnectionFactory(new MockConnectionFactory());
+  }
+
+}
---
+++ modules/adjudication-engine/adjudication-engine-mock-agents/src/main/java/com/silenteight/adjudication/engine/mock/agents/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package com.silenteight.adjudication.engine.mock.agents;
+
+import javax.annotation.ParametersAreNonnullByDefault;
