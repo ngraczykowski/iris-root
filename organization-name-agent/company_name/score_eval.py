cc3494e1f55a707eba36e96529842e4cff5a1f4b
Michał <mdominik@silenteight.com>
2021-04-16 13:32:44 +0200

    reworked the comment_generator.py and added several example cases in resolve_pair.py

--- organization-name-agent/company_name/score_eval.py
+++ organization-name-agent/company_name/score_eval.py
@@ -4,10 +4,16 @@ from company_name.score_outcomes import Outcomes
 def evaluate_scores(scores, feature_cfg):
     evals = {}
     for feature, params in feature_cfg.items():
-        if scores[params['score_name']] < params['low_thr']:
+        score_obj = scores[params['score_name']]
+        if not score_obj.compared[0] and not score_obj.compared[1]:
+            continue
+
+        if scores[params['score_name']].value < params['low_thr']:
             evals[feature] = Outcomes.NO_MATCH
-        elif scores['score_name'] < params['high_thr']:
+        elif scores[params['score_name']].value < params['high_thr']:
             evals[feature] = Outcomes.PARTIAL_MATCH
         else:
             evals[feature] = Outcomes.MATCH
+
+        evals[feature] = (evals[feature], score_obj.compared)
     return evals
