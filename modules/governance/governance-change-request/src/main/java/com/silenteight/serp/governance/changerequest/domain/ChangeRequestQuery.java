4c25aea0fab598d6c49a2c414bf058d254ee1582
Marek Mastyło <mmastylo@silenteight.com>
2021-04-22 09:29:42 +0000

    Feature: Get Change Request details (WEB-828)
    Get Change Request details.
    
    Closes WEB-828
    
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Reviewed-by: Adam Nowicki <anowicki@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/serp-governance/-/merge_requests/116

--- modules/governance/governance-change-request/src/main/java/com/silenteight/serp/governance/changerequest/domain/ChangeRequestQuery.java
+++ modules/governance/governance-change-request/src/main/java/com/silenteight/serp/governance/changerequest/domain/ChangeRequestQuery.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 
 import com.silenteight.serp.governance.changerequest.closed.ClosedChangeRequestQuery;
 import com.silenteight.serp.governance.changerequest.closed.dto.ClosedChangeRequestDto;
+import com.silenteight.serp.governance.changerequest.details.ChangeRequestDetailsQuery;
+import com.silenteight.serp.governance.changerequest.details.dto.ChangeRequestDetailsDto;
+import com.silenteight.serp.governance.changerequest.domain.exception.ChangeRequestNotFoundException;
 import com.silenteight.serp.governance.changerequest.pending.PendingChangeRequestQuery;
 import com.silenteight.serp.governance.changerequest.pending.dto.PendingChangeRequestDto;
 
@@ -13,6 +16,7 @@ import org.springframework.data.domain.PageRequest;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import static com.silenteight.serp.governance.changerequest.domain.ChangeRequestState.APPROVED;
 import static com.silenteight.serp.governance.changerequest.domain.ChangeRequestState.CANCELLED;
@@ -22,7 +26,8 @@ import static java.util.stream.Collectors.toList;
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RequiredArgsConstructor
-class ChangeRequestQuery implements PendingChangeRequestQuery, ClosedChangeRequestQuery {
+class ChangeRequestQuery
+    implements PendingChangeRequestQuery, ClosedChangeRequestQuery, ChangeRequestDetailsQuery {
 
   private static final int CLOSED_MAX_PAGE_SIZE = 20;
   private static final String SORT_CLOSED_BY_FIELD = "decidedAt";
@@ -51,6 +56,14 @@ class ChangeRequestQuery implements PendingChangeRequestQuery, ClosedChangeReque
         .collect(toList());
   }
 
+  @Override
+  public ChangeRequestDetailsDto details(@NonNull UUID changeRequestId) {
+    return repository
+        .findByChangeRequestId(changeRequestId)
+        .map(ChangeRequestQuery::mapDetails)
+        .orElseThrow(() -> new ChangeRequestNotFoundException(changeRequestId));
+  }
+
   private static PendingChangeRequestDto mapPending(ChangeRequest changeRequest) {
     return PendingChangeRequestDto.builder()
         .id(changeRequest.getChangeRequestId())
@@ -62,6 +75,19 @@ class ChangeRequestQuery implements PendingChangeRequestQuery, ClosedChangeReque
 
   private static ClosedChangeRequestDto mapClosed(ChangeRequest changeRequest) {
     return ClosedChangeRequestDto.builder()
+        .id(changeRequest.getChangeRequestId())
+        .createdBy(changeRequest.getCreatedBy())
+        .createdAt(changeRequest.getCreatedAt())
+        .creatorComment(changeRequest.getCreatorComment())
+        .decidedBy(changeRequest.getDecidedBy())
+        .deciderComment(changeRequest.getDeciderComment())
+        .decidedAt(changeRequest.getDecidedAt())
+        .state(changeRequest.getState().name())
+        .build();
+  }
+
+  private static ChangeRequestDetailsDto mapDetails(ChangeRequest changeRequest) {
+    return ChangeRequestDetailsDto.builder()
         .id(changeRequest.getChangeRequestId())
         .createdBy(changeRequest.getCreatedBy())
         .createdAt(changeRequest.getCreatedAt())
---
+++ modules/governance/governance-change-request/src/test/java/com/silenteight/serp/governance/changerequest/details/ChangeRequestDetailsRestControllerTest.java
@@ -0,0 +1,73 @@
+package com.silenteight.serp.governance.changerequest.details;
+
+import com.silenteight.sens.governance.common.testing.rest.BaseRestControllerTest;
+import com.silenteight.sens.governance.common.testing.rest.testwithrole.TestWithRole;
+import com.silenteight.serp.governance.changerequest.details.dto.ChangeRequestDetailsDto;
+import com.silenteight.serp.governance.common.web.exception.GenericExceptionControllerAdvice;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+import java.util.UUID;
+
+import static com.silenteight.sens.governance.common.testing.rest.TestRoles.*;
+import static java.time.OffsetDateTime.parse;
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+import static java.util.UUID.fromString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.OK;
+
+@Import({
+    ChangeRequestDetailsRestController.class,
+    GenericExceptionControllerAdvice.class
+})
+class ChangeRequestDetailsRestControllerTest extends BaseRestControllerTest {
+
+  private final Fixtures fixtures = new Fixtures();
+
+  @MockBean
+  private ChangeRequestDetailsQuery changeRequestDetailsQuery;
+
+  @TestWithRole(roles = { APPROVER, BUSINESS_OPERATOR })
+  void its200WithCorrectBody_whenFound() {
+    UUID changeRequestId = fixtures.changeRequest.getId();
+    given(changeRequestDetailsQuery.details(changeRequestId)).willReturn(fixtures.changeRequest);
+
+    get(mappingForDetails(changeRequestId))
+        .statusCode(OK.value())
+        .body("id", equalTo("05bf9714-b1ee-4778-a733-6151df70fca3"))
+        .body("createdBy", equalTo("Business Operator #1"))
+        .body("createdAt", notNullValue())
+        .body("creatorComment", equalTo("Increase efficiency by 20% on Asia markets"))
+        .body("decidedBy", equalTo("Approver 1"))
+        .body("deciderComment", equalTo("All good!"))
+        .body("decidedAt", notNullValue())
+        .body("state", equalTo("APPROVED"));
+  }
+
+  @TestWithRole(roles = { ADMINISTRATOR, ANALYST, AUDITOR, POLICY_MANAGER })
+  void its403_whenNotPermittedRole() {
+    get(mappingForDetails(fixtures.changeRequest.getId())).statusCode(FORBIDDEN.value());
+  }
+
+  private String mappingForDetails(UUID changeRequestId) {
+    return "/v1/changeRequests/" + changeRequestId.toString();
+  }
+
+  private class Fixtures {
+
+    ChangeRequestDetailsDto changeRequest = ChangeRequestDetailsDto.builder()
+        .id(fromString("05bf9714-b1ee-4778-a733-6151df70fca3"))
+        .createdBy("Business Operator #1")
+        .createdAt(parse("2020-04-15T10:15:30+01:00", ISO_OFFSET_DATE_TIME))
+        .creatorComment("Increase efficiency by 20% on Asia markets")
+        .decidedBy("Approver 1")
+        .deciderComment("All good!")
+        .decidedAt(parse("2020-04-15T10:16:40+01:00", ISO_OFFSET_DATE_TIME))
+        .state("APPROVED")
+        .build();
+  }
+}
