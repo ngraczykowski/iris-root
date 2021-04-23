b3b335d1ab30e8812fb260ccb2cf189c72007faa
Marek Mastyło <mmastylo@silenteight.com>
2021-04-23 09:38:43 +0000

    INTERNAL: Comment out getting analysis from AE
    Comment out getting analysis from AE.
    
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Reviewed-by: Adam Nowicki <anowicki@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/simulator/-/merge_requests/37

--- modules/simulator/simulator-management/src/main/java/com/silenteight/simulator/management/SimulationQuery.java
+++ modules/simulator/simulator-management/src/main/java/com/silenteight/simulator/management/SimulationQuery.java
@@ -3,12 +3,12 @@ package com.silenteight.simulator.management;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
-import com.silenteight.adjudication.api.v1.Analysis;
 import com.silenteight.simulator.management.dto.SimulationDto;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.silenteight.adjudication.api.v1.Analysis.State.DONE;
 import static java.util.stream.Collectors.toList;
 
 @AllArgsConstructor
@@ -27,14 +27,15 @@ class SimulationQuery {
   }
 
   private SimulationDto toDto(SimulationEntity simulationEntity) {
-    Analysis analysis = analysisService.getAnalysis(simulationEntity.getAnalysisName());
+    // TODO(mmastylo): Uncomment when feature is available in AE
+    // Analysis analysis = analysisService.getAnalysis(simulationEntity.getAnalysisName());
     return SimulationDto.builder()
         .id(simulationEntity.getSimulationId())
         .name(simulationEntity.getName())
         .state(simulationEntity.getState())
         .datasetNames(new ArrayList<>(simulationEntity.getDatasetNames()))
         .modelName(simulationEntity.getModelName())
-        .progressState(analysis.getState().toString())
+        .progressState(DONE.toString())
         .createdAt(simulationEntity.getCreatedAt())
         .createdBy(simulationEntity.getCreatedBy())
         .build();
