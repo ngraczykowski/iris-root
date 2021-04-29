62b9a82eb245ce11623bb22ba18e8c329cea03b7
Marek Mastyło <mmastylo@silenteight.com>
2021-04-29 10:33:57 +0000

    INTERNAL: Handle TooManyModelsException
    Handle TooManyModelsException.
    
    Reviewed-by: Adam Nowicki <anowicki@silenteight.com>
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/serp-governance/-/merge_requests/127

--- modules/governance/governance-model/src/main/java/com/silenteight/serp/governance/model/get/GetModelControllerAdvice.java
+++ modules/governance/governance-model/src/main/java/com/silenteight/serp/governance/model/get/GetModelControllerAdvice.java
@@ -3,11 +3,13 @@ package com.silenteight.serp.governance.model.get;
 import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.serp.governance.model.domain.exception.ModelNotFoundException;
+import com.silenteight.serp.governance.model.domain.exception.TooManyModelsException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
@@ -18,6 +20,12 @@ class GetModelControllerAdvice {
   public ResponseEntity<String> handle(ModelNotFoundException e) {
     log(e);
     return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
+  }
+
+  @ExceptionHandler(TooManyModelsException.class)
+  public ResponseEntity<String> handle(TooManyModelsException e) {
+    log(e);
+    return new ResponseEntity<>(e.getMessage(), INTERNAL_SERVER_ERROR);
   }
 
   private static void log(Exception e) {
