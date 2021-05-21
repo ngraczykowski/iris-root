8a751a2f177e3d5c9d37b5367885fa898707ce36
Bartosz Martofel <bmartofel@silenteight.com>
2021-05-21 14:01:12 +0200

    Bugfix: disable model change listener

--- modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/amqp/NewModelListener.java
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/amqp/NewModelListener.java
@@ -13,7 +13,8 @@ class NewModelListener {
   private final ProcessManager processManager;
 
   // ModuleInfo will be replaced by Governance object and mapped to ModuleInfo
-  @RabbitListener(queues = "${silenteight.governance.model_promoted.queue}")
+  //FIXME temporary disable queue listener, as it does not exist
+  //@RabbitListener(queues = "${silenteight.governance.model_promoted.queue}")
   void onModelChange(ModelInfo modelInfo) {
     processManager.execute(modelInfo);
   }
