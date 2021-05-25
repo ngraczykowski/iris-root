3b3a874e1c45e814f3e8f111ce8b636f1c0b2db5
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-05-25 16:31:10 +0200

    Respond with NOT_FOUND for solutions and recommendations
    This is to prevent returning useless UNIMPLEMENTED error. With NOT_FOUND the Bridge service shall not fail.

--- modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/service/GrpcAnalysisService.java
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/service/GrpcAnalysisService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import com.silenteight.adjudication.api.v1.*;
 import com.silenteight.adjudication.api.v1.AnalysisServiceGrpc.AnalysisServiceImplBase;
 
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.context.annotation.Profile;
@@ -43,5 +45,38 @@ class GrpcAnalysisService extends AnalysisServiceImplBase {
       GetAnalysisRequest request, StreamObserver<Analysis> responseObserver) {
     responseObserver.onNext(analysisService.getAnalysis(request));
     responseObserver.onCompleted();
+  }
+
+  @Override
+  public void streamRecommendations(
+      StreamRecommendationsRequest request,
+      StreamObserver<Recommendation> responseObserver) {
+    respondWithNotFound(responseObserver);
+  }
+
+  @Override
+  public void getRecommendation(
+      GetRecommendationRequest request,
+      StreamObserver<Recommendation> responseObserver) {
+    respondWithNotFound(responseObserver);
+  }
+
+  @Override
+  public void streamMatchSolutions(
+      StreamMatchSolutionsRequest request,
+      StreamObserver<MatchSolution> responseObserver) {
+    respondWithNotFound(responseObserver);
+  }
+
+  @Override
+  public void getMatchSolution(
+      GetMatchSolutionRequest request,
+      StreamObserver<MatchSolution> responseObserver) {
+    respondWithNotFound(responseObserver);
+  }
+
+  private void respondWithNotFound(StreamObserver<?> responseObserver) {
+    // XXX(ahaczewski): Mocked to return something more useful than UNIMPLEMENTED.
+    responseObserver.onError(new StatusRuntimeException(Status.NOT_FOUND));
   }
 }
