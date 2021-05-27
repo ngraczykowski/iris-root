391820d836e6b70c88685773c9b191d9b6bd9745
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-05-27 09:19:06 +0200

    Rename bean with a duplicated name

--- modules/adjudication-engine/adjudication-engine-comments/src/main/java/com/silenteight/adjudication/engine/comments/comment/ProtoStructConverterConfiguration.java
+++ modules/adjudication-engine/adjudication-engine-comments/src/main/java/com/silenteight/adjudication/engine/comments/comment/ProtoStructConverterConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 class ProtoStructConverterConfiguration {
 
   @Bean
-  ProtoStructConverter protoMessageToObjectNodeConverter() {
+  ProtoStructConverter protoStructConverter() {
     return new ProtoStructConverter();
   }
 }
