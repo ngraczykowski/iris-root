4dc4dbdcb7d8766e315754ea67a3ad23e467b80c
Bogdan Gulowaty <b.gulowaty@gmail.com>
2021-06-09 08:24:25 +0200

    Fix for finding AE in nomad

--- modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/KnownServices.java
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/KnownServices.java
@@ -2,7 +2,7 @@ package com.silenteight.hsbc.bridge;
 
 public class KnownServices {
 
-  public static final String ADJUDICATION_ENGINE = "adjudication";
+  public static final String ADJUDICATION_ENGINE = "adjudication-engine";
   public static final String GOVERNANCE = "governance";
   public static final String WORLDCHECK = "hsbc-worldcheck";
 }
