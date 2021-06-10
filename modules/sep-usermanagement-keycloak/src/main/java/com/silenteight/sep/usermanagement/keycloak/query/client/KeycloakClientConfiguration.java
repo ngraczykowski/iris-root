a4b8bd43f91734d2811675a2c0964e3d712aea58
Kamil Dzięcioł <kdzieciol@silenteight.com>
2021-06-10 10:14:10 +0200

    INTERNAL: Use RealmResource to get clientsResource.

--- modules/sep-usermanagement-keycloak/src/main/java/com/silenteight/sep/usermanagement/keycloak/query/client/KeycloakClientConfiguration.java
+++ modules/sep-usermanagement-keycloak/src/main/java/com/silenteight/sep/usermanagement/keycloak/query/client/KeycloakClientConfiguration.java
@@ -1,6 +1,6 @@
 package com.silenteight.sep.usermanagement.keycloak.query.client;
 
-import org.keycloak.admin.client.resource.ClientsResource;
+import org.keycloak.admin.client.resource.RealmResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 class KeycloakClientConfiguration {
 
   @Bean
-  KeycloakClientQuery keycloakClientQuery(ClientsResource clientsResource) {
-    return new KeycloakClientQuery(clientsResource);
+  KeycloakClientQuery keycloakClientQuery(RealmResource realmResource) {
+    return new KeycloakClientQuery(realmResource.clients());
   }
 }
