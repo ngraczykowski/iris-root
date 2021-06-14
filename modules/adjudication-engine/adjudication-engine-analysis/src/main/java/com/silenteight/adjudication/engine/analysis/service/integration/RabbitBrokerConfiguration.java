a67b0ebab6abea7ba867d800c1975f5b0af68806
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-06-14 17:15:40 +0200

    Declare dead letter queue and exchange

--- modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/service/integration/RabbitBrokerConfiguration.java
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/service/integration/RabbitBrokerConfiguration.java
@@ -13,10 +13,13 @@ import static org.springframework.amqp.core.ExchangeBuilder.topicExchange;
 class RabbitBrokerConfiguration {
 
   private static final String ERROR_QUEUE = "ae.error-queue";
+  private static final String DEAD_LETTER_QUEUE = "ae.dlq";
+  private static final String DEAD_LETTER_EXCHANGE = "ae.dlx";
 
   @Bean
   Declarables rabbitBrokerDeclarables() {
     var errorQueue = queue(ERROR_QUEUE).build();
+    var deadLetterQueue = queue(DEAD_LETTER_QUEUE).lazy().build();
 
     var eventExchange = topicExchange(EVENT_EXCHANGE_NAME).durable(true).build();
     var eventInternalExchange = topicExchange(EVENT_INTERNAL_EXCHANGE_NAME)
@@ -28,20 +31,25 @@ class RabbitBrokerConfiguration {
     var agentResponseExchange = topicExchange(AGENT_RESPONSE_EXCHANGE_NAME)
         .durable(true)
         .build();
+    var deadLetterExchange = topicExchange(DEAD_LETTER_EXCHANGE)
+        .durable(true)
+        .internal()
+        .build();
+    var deadLetterBinding = bind(deadLetterQueue, deadLetterExchange, "#");
 
-    var pendingRecommendation = queue(PENDING_RECOMMENDATION_QUEUE_NAME).build();
+    var pendingRecommendation = queueDeadLetter(PENDING_RECOMMENDATION_QUEUE_NAME).build();
     var pendingRecommendationsBinding = bind(pendingRecommendation, eventInternalExchange,
         ADDED_ANALYSIS_DATASETS_ROUTING_KEY);
 
-    var agentExchange = queue(AGENT_EXCHANGE_QUEUE_NAME).build();
+    var agentExchange = queueDeadLetter(AGENT_EXCHANGE_QUEUE_NAME).build();
     var agentExchangeBinding = bind(agentExchange, eventInternalExchange,
         PENDING_RECOMMENDATIONS_ROUTING_KEY);
 
-    var category = queue(CATEGORY_QUEUE_NAME).build();
+    var category = queueDeadLetter(CATEGORY_QUEUE_NAME).build();
     var categoryBinding = bind(category, eventInternalExchange,
         PENDING_RECOMMENDATIONS_ROUTING_KEY);
 
-    var commentInput = queue(COMMENT_INPUT_QUEUE_NAME).build();
+    var commentInput = queueDeadLetter(COMMENT_INPUT_QUEUE_NAME).build();
     var commentInputBinding = bind(commentInput, eventInternalExchange,
         PENDING_RECOMMENDATIONS_ROUTING_KEY);
 
@@ -51,7 +59,7 @@ class RabbitBrokerConfiguration {
         PENDING_RECOMMENDATIONS_ROUTING_KEY);
      */
 
-    var agentResponse = queue(AGENT_RESPONSE_QUEUE_NAME).maxPriority(10).build();
+    var agentResponse = queueDeadLetter(AGENT_RESPONSE_QUEUE_NAME).maxPriority(10).build();
     var agentResponseBinding = bind(agentResponse, agentResponseExchange, "#");
 
     var tmpAgentRequest = queue(TMP_AGENT_REQUEST_QUEUE_NAME).maxPriority(10).build();
@@ -59,6 +67,8 @@ class RabbitBrokerConfiguration {
 
     return new Declarables(
         errorQueue,
+        deadLetterExchange,
+        deadLetterQueue, deadLetterBinding,
         eventExchange, eventInternalExchange, agentRequestExchange, agentResponseExchange,
         pendingRecommendation, pendingRecommendationsBinding,
         agentExchange, agentExchangeBinding,
@@ -72,6 +82,10 @@ class RabbitBrokerConfiguration {
     return QueueBuilder
         .durable(queueName)
         .withArgument("x-queue-type", "classic");
+  }
+
+  private static QueueBuilder queueDeadLetter(String queueName) {
+    return queue(queueName).deadLetterExchange(DEAD_LETTER_EXCHANGE);
   }
 
   private static Binding bind(Queue queue, Exchange exchange, String routingKey) {
