7183cdf97d412086461b8638e77b7fd847297ae9
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-06-14 11:36:49 +0200

    Add logging of pending recommendations

--- modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/pendingrecommendation/CreatePendingRecommendationsUseCase.java
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/pendingrecommendation/CreatePendingRecommendationsUseCase.java
@@ -1,18 +1,25 @@
 package com.silenteight.adjudication.engine.analysis.pendingrecommendation;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 class CreatePendingRecommendationsUseCase {
 
   private final PendingRecommendationDataAccess dataAccess;
 
   @Transactional
   boolean createPendingRecommendations(long analysisId) {
-    return dataAccess.createPendingRecommendations(analysisId) > 0;
+    var pendingCount = dataAccess.createPendingRecommendations(analysisId);
+
+    log.info("Created pending recommendations: analysis=analysis/{}, pendingCount={}",
+        analysisId, pendingCount);
+
+    return pendingCount > 0;
   }
 }
