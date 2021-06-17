69e3a5406ea38a078338efd6b0bcaed68f466f3f
Adam Wójcicki <awojcicki@silenteight.com>
2021-06-17 05:20:37 +0000

    ENHANCEMENT: Take policy_id, step_id and fv_signature from alertMetadata to alert in open API (ALL-169)
    ENHANCEMENT: Take policy_id, step_id and fv_signature from alertMetadata to alert in open API (ALL-169)
    
    Closes ALLIN-169
    
    Reviewed-by: Bartosz Martofel <bmartofel@silenteight.com>
    Reviewed-by: Michał Mrówka <mmrowka@silenteight.com>
    Merged from: https://gitlab.silenteight.com/hsbc-bank/hsbc-bridge/-/merge_requests/182

--- modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/bulk/BulkRestController.java
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/bulk/BulkRestController.java
@@ -8,27 +8,14 @@ import com.silenteight.hsbc.bridge.bulk.exception.BatchProcessingNotCompletedExc
 import com.silenteight.hsbc.bridge.bulk.exception.BatchWithGivenIdAlreadyCreatedException;
 import com.silenteight.hsbc.bridge.bulk.rest.*;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-
-import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @RestController
 @RequestMapping("/async/batch/v1")
@@ -106,49 +93,6 @@ public class BulkRestController {
   @GetMapping("/{id}/cancel")
   public ResponseEntity<BatchCancelResponse> cancelBulk(@PathVariable String id) {
     return ResponseEntity.ok(cancelBulkUseCase.cancel(id));
-  }
-
-  @GetMapping(value = "/{id}/result/owsFile", produces = "text/csv")
-  public ResponseEntity<Resource> getResultFile(@PathVariable String id) {
-
-    var result = getBulkResultsUseCase.getResults(id);
-
-    var owsBody = result.getAlerts().stream()
-        .map(this::createOwsRow)
-        .collect(Collectors.toList());
-
-    ByteArrayInputStream byteArrayOutputStream;
-
-    try (
-        var out = new ByteArrayOutputStream();
-        var printer = new CSVPrinter(
-            new PrintWriter(out),
-            CSVFormat.DEFAULT.withHeader(OWS_HEADER))) {
-
-      for (List<String> record : owsBody) {
-        printer.printRecord(record);
-      }
-
-      printer.flush();
-
-      byteArrayOutputStream = new ByteArrayInputStream(out.toByteArray());
-    } catch (IOException e) {
-      throw new RuntimeException(e.getMessage());
-    }
-
-    var fileInputStream = new InputStreamResource(byteArrayOutputStream);
-
-    var headers = new HttpHeaders();
-    headers.set(CONTENT_DISPOSITION, "attachment; filename=" + OWS_FILE_NAME);
-    headers.set(CONTENT_TYPE, "text/csv");
-
-    return new ResponseEntity<>(fileInputStream, headers, HttpStatus.OK);
-  }
-
-  private List<String> createOwsRow(SolvedAlert a) {
-    // TODO fill it with proper data
-    return List.of(a.getId() + "", a.getRecommendation(), a.getComment(), "Code",
-        "Description");
   }
 
   @ExceptionHandler({ BatchIdNotFoundException.class, BatchProcessingNotCompletedException.class })
