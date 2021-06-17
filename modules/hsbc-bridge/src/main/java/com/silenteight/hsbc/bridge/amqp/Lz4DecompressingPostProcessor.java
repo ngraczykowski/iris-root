343ccfaa9f9d8b5269f9cf30b28674745b38243a
Bogdan Gulowaty <bgulowaty@silenteight.com>
2021-06-17 07:41:11 +0000

    Feature: Messages decompression
    Reviewed-by: Michał Mrówka <mmrowka@silenteight.com>
    Reviewed-by: Adrian Pozorski <apozorski@silenteight.com>
    Reviewed-by: Szymon Mrozowski <smrozowski@silenteight.com>
    Merged from: https://gitlab.silenteight.com/hsbc-bank/hsbc-bridge/-/merge_requests/185

---
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/amqp/Lz4DecompressingPostProcessor.java
@@ -0,0 +1,29 @@
+package com.silenteight.hsbc.bridge.amqp;
+
+import lombok.RequiredArgsConstructor;
+
+import net.jpountz.lz4.LZ4Factory;
+import net.jpountz.lz4.LZ4FrameInputStream;
+import org.springframework.amqp.support.postprocessor.AbstractDecompressingPostProcessor;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@RequiredArgsConstructor
+class Lz4DecompressingPostProcessor extends AbstractDecompressingPostProcessor {
+
+  private static final String LZ4_ENCODING = "lz4";
+
+  @Override
+  protected InputStream getDecompressorStream(InputStream stream) throws IOException {
+    return new LZ4FrameInputStream(
+        stream,
+        LZ4Factory.fastestInstance().safeDecompressor(),
+        Lz4Utils.createHash());
+  }
+
+  @Override
+  protected String getEncoding() {
+    return LZ4_ENCODING;
+  }
+}
--- modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/amqp/RabbitTemplatePostProcessor.java
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/amqp/RabbitTemplatePostProcessor.java
@@ -2,6 +2,7 @@ package com.silenteight.hsbc.bridge.amqp;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.amqp.rabbit.config.AbstractRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -13,6 +14,10 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(AmqpMessagingConfiguration.class)
 class RabbitTemplatePostProcessor implements BeanPostProcessor {
 
+  public static final int FASTEST_COMPRESSION = 1;
+  private static final Lz4DecompressingPostProcessor
+      LZ4_DECOMPRESSING_POST_PROCESSOR = new Lz4DecompressingPostProcessor();
+
   private final AmqpProtoMessageConverter amqpProtoMessageConverter;
 
   @Override
@@ -20,9 +25,23 @@ class RabbitTemplatePostProcessor implements BeanPostProcessor {
       BeansException {
     if (bean instanceof RabbitTemplate) {
       var template = (RabbitTemplate) bean;
-      template.setMessageConverter(amqpProtoMessageConverter);
+      configureRabbitTemplate(template);
+    }
+    if (AbstractRabbitListenerContainerFactory.class.isAssignableFrom(bean.getClass())) {
+      configureContainerFactory((AbstractRabbitListenerContainerFactory<?>) bean);
     }
 
     return bean;
+  }
+
+  private static void configureContainerFactory(AbstractRabbitListenerContainerFactory<?> factory) {
+    factory.setAfterReceivePostProcessors(LZ4_DECOMPRESSING_POST_PROCESSOR);
+  }
+
+  private void configureRabbitTemplate(RabbitTemplate template) {
+    template.setMessageConverter(amqpProtoMessageConverter);
+
+    template.addAfterReceivePostProcessors(LZ4_DECOMPRESSING_POST_PROCESSOR);
+    template.addBeforePublishPostProcessors(new Lz4CompressingPostProcessor(FASTEST_COMPRESSION));
   }
 }
