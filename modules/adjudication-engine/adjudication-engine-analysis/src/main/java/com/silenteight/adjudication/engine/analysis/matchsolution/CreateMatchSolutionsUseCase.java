2d7582c7be586349a66d9212901cc2dd8df75094
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-06-25 16:02:23 +0200

    Use new Governance provided reason for match solution

--- modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/matchsolution/CreateMatchSolutionsUseCase.java
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/matchsolution/CreateMatchSolutionsUseCase.java
@@ -2,11 +2,16 @@ package com.silenteight.adjudication.engine.analysis.matchsolution;
 
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.adjudication.engine.analysis.matchsolution.dto.MatchSolution;
 import com.silenteight.adjudication.engine.analysis.matchsolution.dto.MatchSolutionCollection;
+import com.silenteight.adjudication.engine.common.protobuf.ProtoMessageToObjectNodeConverter;
+import com.silenteight.solving.api.v1.SolutionResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.protobuf.ByteString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +20,11 @@ import java.util.Base64;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 class CreateMatchSolutionsUseCase {
 
   private final MatchSolutionRepository matchSolutionRepository;
+  private final ProtoMessageToObjectNodeConverter protoMessageToObjectNodeConverter;
 
   @Setter(onMethod_ = @Autowired)
   private ObjectMapper objectMapper = new ObjectMapper();
@@ -39,16 +46,32 @@ class CreateMatchSolutionsUseCase {
         .matchId(matchSolution.getMatchId())
         .solution(solutionResponse.getFeatureVectorSolution().toString());
 
-    var featureVectorSignature = Base64
-        .getEncoder()
-        .encodeToString(solutionResponse.getFeatureVectorSignature().toByteArray());
+    if (!solutionResponse.hasReason()) {
+      var reason =
+          makeDeprecatedReason(solutionResponse, solutionResponse.getFeatureVectorSignature());
+      builder.reason(reason);
+    } else {
+      protoMessageToObjectNodeConverter.convert(solutionResponse.getReason())
+          .ifPresentOrElse(builder::reason, () -> log.error(
+              "Failed to convert the solution reason to JSON: response={}",
+              solutionResponse));
+    }
 
+    return builder.build();
+  }
+
+  private ObjectNode makeDeprecatedReason(
+      SolutionResponse solutionResponse, ByteString featureVectorSignature) {
+
+    var featureVectorSignatureBase = Base64
+        .getEncoder()
+        .encodeToString(featureVectorSignature.toByteArray());
     var nodeFactory = objectMapper.getNodeFactory();
     var reason = nodeFactory.objectNode();
 
-    reason.set("featureVectorSignature", nodeFactory.textNode(featureVectorSignature));
+    reason.set("featureVectorSignature", nodeFactory.textNode(featureVectorSignatureBase));
     reason.set("stepId", nodeFactory.textNode(solutionResponse.getStepId().toString()));
 
-    return builder.reason(reason).build();
+    return reason;
   }
 }
