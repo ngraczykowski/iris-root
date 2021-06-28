44d385e74584b72e57be41d30de1dc63f96c0ff2
Adam Nowicki <anowicki@silenteight.com>
2021-06-28 05:18:21 +0000

    INTERNAL: connect healthcheck for elasticsearch
    Reviewed-by: Marek Mastyło <mmastylo@silenteight.com>
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/warehouse/-/merge_requests/80

--- modules/warehouse/warehouse-common/src/main/java/com/silenteight/warehouse/common/elastic/ElasticsearchClientConfiguration.java
+++ modules/warehouse/warehouse-common/src/main/java/com/silenteight/warehouse/common/elastic/ElasticsearchClientConfiguration.java
@@ -2,9 +2,15 @@ package com.silenteight.warehouse.common.elastic;
 
 import com.silenteight.sep.auth.token.UserAwareTokenProvider;
 
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.actuate.autoconfigure.health.CompositeHealthContributorConfiguration;
+import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
+import org.springframework.boot.actuate.elasticsearch.ElasticsearchRestHealthIndicator;
+import org.springframework.boot.actuate.health.HealthContributor;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.CustomElasticsearchRestClientConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
@@ -14,6 +20,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import static java.util.Map.of;
+
 @Configuration
 @ImportAutoConfiguration(exclude = {
     ElasticsearchRestClientAutoConfiguration.class,
@@ -21,7 +29,8 @@ import org.springframework.context.annotation.Import;
 })
 @EnableConfigurationProperties(ElasticsearchRestClientProperties.class)
 @Import(CustomElasticsearchRestClientConfiguration.class)
-public class ElasticsearchClientConfiguration {
+public class ElasticsearchClientConfiguration extends
+    CompositeHealthContributorConfiguration<ElasticsearchRestHealthIndicator, RestClient> {
 
   @Bean
   RestHighLevelElasticClientFactory restHighLevelElasticClientFactory(
@@ -45,5 +54,16 @@ public class ElasticsearchClientConfiguration {
       RestHighLevelElasticClientFactory restHighLevelElasticClientFactory) {
 
     return restHighLevelElasticClientFactory.getUserAwareClient();
+  }
+
+  @ConditionalOnEnabledHealthIndicator(value = "elasticsearch")
+  @Bean
+  @ConditionalOnMissingBean(name = {
+      "elasticsearchHealthIndicator", "elasticsearchHealthContributor" })
+  public HealthContributor elasticsearchHealthContributor(
+      RestHighLevelClient restHighLevelAdminClient) {
+
+    return createContributor(
+        of("restHighLevelAdminClient", restHighLevelAdminClient.getLowLevelClient()));
   }
 }
