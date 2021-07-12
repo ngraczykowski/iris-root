4ba1d6a3a5d2d0667087f88f3443841af8360516
Wiktor Keska <wkeska@silenteight.com>
2021-07-12 14:48:40 +0200

    dependencies

--- modules/payments-bridge/sear-payments-app/src/main/java/com/silenteight/searpaymentsmockup/SearPaymentsApplication.java
+++ modules/payments-bridge/sear-payments-app/src/main/java/com/silenteight/searpaymentsmockup/SearPaymentsApplication.java
@@ -22,34 +22,9 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 public class SearPaymentsApplication {
 
   public static void main(String[] args) {
-    setUpSystemProperties();
-    setUpSecuritySystemProperties();
-
-    Schedulers.enableMetrics();
     new SpringApplicationTemplate(args, SearPaymentsApplication.class)
         .contextCallback(new DefaultSpringApplicationContextCallback())
         .runAndExit(new Configurer());
-  }
-
-  private static void setUpSystemProperties() {
-    // NOTE(ahaczewski): Force use of fast random source.
-    setProperty("java.security.egd", "file:/dev/urandom");
-  }
-
-  private static void setUpSecuritySystemProperties() {
-    setPropertyFromEnvironment("javax.net.ssl.trustStore", "TRUSTSTORE_PATH");
-    setPropertyFromEnvironment("javax.net.ssl.trustStorePassword", "TRUSTSTORE_PASSWORD");
-    setPropertyFromEnvironment("javax.net.ssl.trustStoreType", "TRUSTSTORE_TYPE");
-    setPropertyFromEnvironment("javax.net.ssl.keyStore", "KEYSTORE_PATH");
-    setPropertyFromEnvironment("javax.net.ssl.keyStorePassword", "KEYSTORE_PASSWORD");
-    setPropertyFromEnvironment("javax.net.ssl.keyStoreType", "KEYSTORE_TYPE");
-  }
-
-  private static void setPropertyFromEnvironment(String property, String environmentVariable) {
-    var environmentVariableValue = System.getenv(environmentVariable);
-
-    if (getProperty(property) == null && isNotBlank(environmentVariableValue))
-      setProperty(property, environmentVariableValue);
   }
 
 
@@ -58,7 +33,7 @@ public class SearPaymentsApplication {
     @Override
     public ConfigurableApplicationBuilder configure(ConfigurableApplicationBuilder builder) {
       return builder
-          .bootstrapProperties("spring.application.name=ae");
+          .bootstrapProperties("spring.application.name=sp");
     }
 
     @Override
