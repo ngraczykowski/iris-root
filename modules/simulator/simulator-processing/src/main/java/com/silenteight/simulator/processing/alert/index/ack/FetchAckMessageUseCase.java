8ae690ae347b4372caa2aa3d8064ac38a00edb41
Marek Mastyło <mmastylo@silenteight.com>
2021-07-19 09:17:31 +0000

    INTERNAL: Add fluent-bit and logback config for logback
    Add fluent-bit and logback config for logback.
    
    Reviewed-by: Przemysław Lewandowski <plewandowski@silenteight.com>
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/simulator/-/merge_requests/66

--- modules/simulator/simulator-processing/src/main/java/com/silenteight/simulator/processing/alert/index/ack/FetchAckMessageUseCase.java
+++ modules/simulator/simulator-processing/src/main/java/com/silenteight/simulator/processing/alert/index/ack/FetchAckMessageUseCase.java
@@ -2,6 +2,7 @@ package com.silenteight.simulator.processing.alert.index.ack;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.adjudication.api.v1.Analysis;
 import com.silenteight.data.api.v1.DataIndexResponse;
@@ -15,6 +16,7 @@ import static com.silenteight.adjudication.api.v1.Analysis.State.DONE;
 import static com.silenteight.simulator.processing.alert.index.domain.State.SENT;
 import static java.util.List.of;
 
+@Slf4j
 @RequiredArgsConstructor
 public class FetchAckMessageUseCase implements AckMessageHandler {
 
@@ -30,6 +32,8 @@ public class FetchAckMessageUseCase implements AckMessageHandler {
   @Override
   public void handle(DataIndexResponse request) {
     String requestId = request.getRequestId();
+    log.info("Data feed acknowledged: requestId=" + requestId);
+
     indexedAlertService.ack(requestId);
     String analysisName = indexedAlertQuery.getAnalysisNameByRequestId(requestId);
     Analysis analysis = analysisService.getAnalysis(analysisName);
