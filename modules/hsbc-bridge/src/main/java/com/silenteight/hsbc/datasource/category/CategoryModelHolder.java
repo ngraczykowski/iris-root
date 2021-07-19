feaabe494c80d540686d0129bbcabd76763720e6
Bartosz Martofel <bmartofel@silenteight.com>
2021-07-19 09:51:12 +0200

    Feature: Map riskType values as in POV (ALL-232)

--- modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/datasource/category/CategoryModelHolder.java
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/datasource/category/CategoryModelHolder.java
@@ -30,7 +30,7 @@ class CategoryModelHolder {
         .type(CategoryType.ANY_STRING)
         .multiValue(true)
         .allowedValues(List.of())
-        .valueRetriever(matchData -> List.of("GB"))
+        .valueRetriever(matchData -> List.of())
         .build();
 
     var customerType = CategoryModel.builder()
@@ -47,10 +47,12 @@ class CategoryModelHolder {
         .displayName("Risk Type")
         .type(CategoryType.ENUMERATED)
         .multiValue(false)
-        .allowedValues(List.of("AML", "OTHER", "SAN", "PEP", "AM"))
+        .allowedValues(List.of("AML", "OTHER", "SAN", "PEP", "EXITS", "SSC"))
         .valueRetriever(matchData -> {
           var caseInformation = matchData.getCaseInformation();
-          return List.of(ofNullable(caseInformation.getExtendedAttribute5()).orElse(""));
+          var mappedValue = mapSourceRiskTypeValue(caseInformation.getExtendedAttribute5());
+
+          return List.of(mappedValue);
         })
         .build();
 
@@ -59,6 +61,32 @@ class CategoryModelHolder {
         country,
         customerType,
         hitType);
+  }
+
+  private static String mapSourceRiskTypeValue(String sourceValue) {
+    switch (sourceValue) {
+      case "AML":
+      case "CTF-P2":
+      case "INNIA":
+      case "MX-AML":
+      case "MX-SHCP":
+        return "AML";
+      case "AE-MEWOLF":
+      case "MENA-GREY":
+      case "MEWOLF":
+      case "MX-DARK-GREY":
+      case "SAN":
+      case "US-HBUS":
+        return "SAN";
+      case "PEP":
+        return "PEP";
+      case "SCION":
+        return "EXITS";
+      case "SSC":
+        return "SSC";
+      default:
+        return "OTHER";
+    }
   }
 
   static List<CategoryModel> getCategories() {
