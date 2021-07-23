69dad14f41273be430892ecbdd403896b0924cd2
Bartosz Martofel <bmartofel@silenteight.com>
2021-07-23 10:54:17 +0200

    get categories - add missing categories, increase deadline

--- modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/datasource/category/GetMatchCategoryValuesUseCase.java
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/datasource/category/GetMatchCategoryValuesUseCase.java
@@ -2,6 +2,7 @@ package com.silenteight.hsbc.datasource.category;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.hsbc.datasource.category.command.GetMatchCategoryValuesCommand;
 import com.silenteight.hsbc.datasource.category.dto.CategoryValueDto;
@@ -10,20 +11,47 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor
+@Slf4j
 public class GetMatchCategoryValuesUseCase {
 
   private final MatchCategoryViewRepository matchCategoryViewRepository;
 
   @Transactional(readOnly = true)
   public List<CategoryValueDto> activate(@NonNull GetMatchCategoryValuesCommand command) {
-    var matchCategoryValues = findMatchCategoryValues(command.getMatchValues());
-
-    return matchCategoryValues.stream()
+    var matchValues = command.getMatchValues();
+    var matchCategoryValues = findMatchCategoryValues(matchValues)
+        .stream()
         .map(this::mapMatchCategoryValue)
-        .collect(Collectors.toList());
+        .collect(toList());
+
+    var result = new ArrayList<>(matchCategoryValues);
+    if (result.size() < matchValues.size()) {
+      addMissingValues(matchValues, result);
+    }
+
+    return result;
+  }
+
+  private void addMissingValues(List<String> matchValues, List<CategoryValueDto> result) {
+    var returnedNames = result.stream().map(CategoryValueDto::getName).collect(toList());
+    var missingNames =
+        matchValues.stream().filter(a -> !returnedNames.contains(a)).collect(toList());
+    log.warn("Not all match categories are present, missing category values={}", missingNames);
+
+    result.addAll(addMissingValues(missingNames));
+  }
+
+  private List<CategoryValueDto> addMissingValues(List<String> names) {
+    return names.stream().map(n ->
+        CategoryValueDto.builder()
+            .name(n)
+            .values(List.of())
+            .build())
+        .collect(toList());
   }
 
   private List<MatchCategoryView> findMatchCategoryValues(List<String> matchValues) {
