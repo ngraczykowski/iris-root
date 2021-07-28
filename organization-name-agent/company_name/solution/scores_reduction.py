be0fbf8e991f9977b148252b5dc880cfc3e473b8
Marta Cieślak <mcieslak@silenteight.com>
2021-07-28 11:44:24 +0200

    Integrate new model into agent

--- organization-name-agent/company_name/solution/scores_reduction.py
+++ organization-name-agent/company_name/solution/scores_reduction.py
@@ -1,12 +1,12 @@
 import dataclasses
 import pathlib
-import pickle
 from typing import Any, Mapping, Optional, Sequence, Tuple, Union
 
 import sklearn  # noqa: F401
 import yaml
 
 from company_name.scores.score import Score
+from company_name.solution.sklearn_model import SklearnModel
 from company_name.solution.solution import Solution
 
 SolutionWithProbability = Tuple[Solution, float]
@@ -42,16 +42,14 @@ class FeatureRule(ReductionRule):
 class ModelSolutionRule:
     solution: Solution
     threshold: float
-    label: Optional[int] = None
+    label: Optional[str] = None
 
     def __post_init__(self):
         self.solution = Solution(self.solution)
         self.threshold = float(self.threshold)
-        if self.label is not None:
-            self.label = int(self.label)
 
     def check(
-        self, predicted: Union[float, Sequence[float]]
+        self, predicted: Union[float, Mapping[str, float]]
     ) -> Optional[SolutionWithProbability]:
         predicted_value = (
             predicted if isinstance(predicted, float) else predicted[self.label]
@@ -65,22 +63,12 @@ class ModelRule(ReductionRule):
     solutions: Sequence[ModelSolutionRule]
     source: pathlib.Path
     model: Any = None
-    no_data_values: Mapping[str, float] = None
 
     def __post_init__(self):
         self.solutions = [ModelSolutionRule(**v) for v in self.solutions]
-        self.no_data_values = {
-            k: float(v) for k, v in (self.no_data_values.items() or ())
-        }
 
     def check(self, scores: Mapping[str, Score]) -> Optional[SolutionWithProbability]:
-        values = [
-            v.value
-            if v.status == Score.ScoreStatus.OK
-            else self.no_data_values.get(k, 0.0)
-            for k, v in scores.items()
-        ]
-        predicted = self.model.predict_proba([values[: len(self.model.coef_[0])]])[0]
+        predicted = self.model.predict(scores)
 
         for solution_rule in self.solutions:
             solution = solution_rule.check(predicted)
@@ -102,9 +90,8 @@ class ScoresReduction:
         rules = []
         for rule in cfg["rules"]:
             if "source" in rule:
-                with open(configuration_dir / rule["source"], "rb") as model_file:
-                    rule["model"] = pickle.load(model_file)
-                    rules.append(ModelRule(**rule))
+                rule["model"] = SklearnModel(configuration_dir / rule["source"])
+                rules.append(ModelRule(**rule))
             else:
                 rules.append(FeatureRule(**rule))
         return rules
---
+++ organization-name-agent/company_name/solution/sklearn_model.py
@@ -0,0 +1,36 @@
+import pathlib
+import pickle
+from typing import Mapping
+
+import sklearn  # noqa: F401
+
+from company_name.scores.score import Score
+
+
+class SklearnModel:
+    def __init__(self, path: pathlib.Path):
+        with open(path, "rb") as f:
+            sklearn_model = pickle.load(f)
+        self.lb_classes = sklearn_model["lb_classes"]
+        self.clf = sklearn_model["clf"]
+        self.imputed_values = sklearn_model["imputed_vals"]
+
+    def __repr__(self):
+        return (
+            "SkleanModel("
+            + ", \n".join(
+                f"{key}={getattr(self, key)!r}" for key in self.__dict__.keys()
+            )
+            + ")"
+        )
+
+    def predict(self, scores: Mapping[str, Score]):
+        feature_vector = [
+            v.value
+            if v.status == Score.ScoreStatus.OK
+            else self.imputed_values.get(f"{k}_value", 0)
+            for k, v in scores.items()
+        ]
+
+        probabilities = self.clf.predict_proba([feature_vector])[0]
+        return dict(zip(self.lb_classes, probabilities))
