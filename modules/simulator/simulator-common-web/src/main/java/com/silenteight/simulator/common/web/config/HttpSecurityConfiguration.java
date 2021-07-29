cb50c62a10fef73278874a64efd8d46c188d816d
Kamil Dzięcioł <kdzieciol@silenteight.com>
2021-07-29 19:37:22 +0200

    INTERNAL: Bump sep-auth to 1.14.0 - solve problems with post requests.

--- modules/simulator/simulator-common-web/src/main/java/com/silenteight/simulator/common/web/config/HttpSecurityConfiguration.java
+++ modules/simulator/simulator-common-web/src/main/java/com/silenteight/simulator/common/web/config/HttpSecurityConfiguration.java
@@ -2,9 +2,6 @@ package com.silenteight.simulator.common.web.config;
 
 import lombok.RequiredArgsConstructor;
 
-import com.silenteight.simulator.common.web.rest.RestConstants;
-import com.silenteight.simulator.common.web.security.RestAccessDeniedHandler;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,23 +13,9 @@ import org.springframework.web.filter.CorsFilter;
 class HttpSecurityConfiguration {
 
   private final CorsFilter corsFilter;
-  private final RestAccessDeniedHandler restAccessDeniedHandler;
 
   @Autowired
-  public void configure(HttpSecurity http) throws Exception {
-    http
-        // Cross-Site Request Forgery and security Headers disabled for REST API
-        .csrf()
-        .disable()
-        .headers()
-        .disable()
-        .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-        .exceptionHandling()
-        .accessDeniedHandler(restAccessDeniedHandler)
-        .and()
-        .authorizeRequests()
-        .antMatchers(RestConstants.OPENAPI_PREFIX + "/**").permitAll()
-        .antMatchers(RestConstants.MANAGEMENT_PREFIX + "/**").permitAll()
-        .anyRequest().authenticated();
+  public void configure(HttpSecurity http) {
+    http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
   }
 }
