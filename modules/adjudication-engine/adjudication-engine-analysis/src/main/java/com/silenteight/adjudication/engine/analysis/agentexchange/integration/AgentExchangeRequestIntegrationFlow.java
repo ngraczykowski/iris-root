454d054c5e48a17ad069e66f8471c98f80b7bdcd
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-07-29 15:05:19 +0200

    fix(analysis): Log the request ID for each agent exchange
    Requested by Darek, to better measure agents performance.

--- modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/agentexchange/integration/AgentExchangeRequestIntegrationFlow.java
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/agentexchange/integration/AgentExchangeRequestIntegrationFlow.java
@@ -1,11 +1,13 @@
 package com.silenteight.adjudication.engine.analysis.agentexchange.integration;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.silenteight.adjudication.engine.analysis.agentexchange.domain.AgentExchangeRequestMessage;
-import com.silenteight.agents.v1.api.exchange.AgentExchangeRequest;
 
 import org.springframework.integration.dsl.IntegrationFlowAdapter;
 import org.springframework.integration.dsl.IntegrationFlowDefinition;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 
 import static com.silenteight.adjudication.engine.analysis.agentexchange.integration.AgentExchangeChannels.AGENT_CONFIG_HEADER;
@@ -14,6 +16,7 @@ import static com.silenteight.adjudication.engine.analysis.agentexchange.integra
 
 @SuppressWarnings({ "MethodMayBeStatic", "java:S2325" })
 @Component
+@Slf4j
 class AgentExchangeRequestIntegrationFlow extends IntegrationFlowAdapter {
 
   @Override
@@ -23,9 +26,20 @@ class AgentExchangeRequestIntegrationFlow extends IntegrationFlowAdapter {
             .correlationIdFunction(this::getCorrelationId, Boolean.TRUE)
             .priorityFunction(this::getPriority, Boolean.TRUE)
             .headerFunction(AGENT_CONFIG_HEADER, this::getAgentConfig))
-        .<AgentExchangeRequestMessage, AgentExchangeRequest>transform(
-            AgentExchangeRequestMessage::toRequest)
+        .handle(AgentExchangeRequestMessage.class, this::logRequest)
+        .transform(AgentExchangeRequestMessage::toRequest)
         .channel(AGENT_EXCHANGE_REQUEST_OUTBOUND_CHANNEL);
+  }
+
+  private AgentExchangeRequestMessage logRequest(
+      AgentExchangeRequestMessage payload, MessageHeaders headers) {
+
+    if (log.isDebugEnabled()) {
+      log.debug("Sending agent exchange request: requestId={}, agentConfig={}",
+          payload.getRequestId(), payload.getAgentConfig());
+    }
+
+    return payload;
   }
 
   private String getCorrelationId(Message<AgentExchangeRequestMessage> message) {
