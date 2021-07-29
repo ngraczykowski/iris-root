61a4b74c9ebe4d0f6d9b6c0ffbd3eb2ecb62f8b6
Kamil Dzięcioł <kdzieciol@silenteight.com>
2021-07-29 20:06:42 +0200

    INTERNAL: Forgot to update HttpSecurityDenyConfiguration.

--- modules/governance/governance-common-web/src/main/java/com/silenteight/serp/governance/common/web/config/HttpSecurityDenyConfiguration.java
+++ modules/governance/governance-common-web/src/main/java/com/silenteight/serp/governance/common/web/config/HttpSecurityDenyConfiguration.java
@@ -3,7 +3,6 @@ package com.silenteight.serp.governance.common.web.config;
 import lombok.RequiredArgsConstructor;
 
 import com.silenteight.serp.governance.common.web.rest.RestConstants;
-import com.silenteight.serp.governance.common.web.security.RestAccessDeniedHandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -15,20 +14,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 @Profile("no-rest-api")
 class HttpSecurityDenyConfiguration {
 
-  private final RestAccessDeniedHandler restAccessDeniedHandler;
-
   @Autowired
   public void configure(HttpSecurity http) throws Exception {
-    http
-        // Cross-Site Request Forgery and security Headers disabled for REST API
-        .csrf()
-        .disable()
-        .headers()
-        .disable()
-        .exceptionHandling()
-        .accessDeniedHandler(restAccessDeniedHandler)
-        .and()
-        .authorizeRequests()
+    http.authorizeRequests()
         .antMatchers(RestConstants.MANAGEMENT_PREFIX + "/**").permitAll()
         .anyRequest().denyAll();
   }
