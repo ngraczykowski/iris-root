3d8d1461c50fa1cff1e7f65f61b8613f2edd2f91
ksiwecki <ksiwecki@silenteight.com>
2021-08-18 18:30:48 +0200

    refactor due to style standards

--- organization-name-agent/company_name/utils/abbreviations_filtering.py
+++ organization-name-agent/company_name/utils/abbreviations_filtering.py
@@ -1,19 +1,22 @@
-from typing import Sequence, List
+from typing import List, Sequence
 
 from company_name.names.name_information import NameInformation
 from company_name.scores.abbreviation import get_abbreviation_score
 
 
-def remove_redundant_abbreviations(names: Sequence[NameInformation]) -> List[NameInformation]:
+def remove_redundant_abbreviations(
+    names: Sequence[NameInformation],
+) -> List[NameInformation]:
     redundant_abbreviations = []
     for name in names:
         name_len = len(name.base.cleaned_name)
-
-        for shorter_name in filter(lambda x: len(x.base.cleaned_name) < name_len, names):
+        for shorter_name in filter(
+            lambda x: len(x.base.cleaned_name) < name_len, names
+        ):
             abbreviation_score = get_abbreviation_score(name, shorter_name)
             if abbreviation_score.value > 0.75:
                 redundant_abbreviations.append(shorter_name)
-
-    names_without_redundant_abbreviations = \
-        [name for name in names if name not in redundant_abbreviations]
+    names_without_redundant_abbreviations = [
+        name for name in names if name not in redundant_abbreviations
+    ]
     return names_without_redundant_abbreviations
--- organization-name-agent/tests/agent/test_members_abbreviations_filtering.py
+++ organization-name-agent/tests/agent/test_members_abbreviations_filtering.py
@@ -14,11 +14,13 @@ from company_name.utils.abbreviations_filtering import remove_redundant_abbrevia
         ("ABC", "Alfa Beta Casa LTD"),
         ("FA", "Funny Abbreviations Limited"),
         ("BB", "Big Bank Corp."),
-        ("B&B", "Bed and Breakfast")
+        ("B&B", "Bed and Breakfast"),
     ],
 )
 def test_abbreviation_recognition(abbreviation: str, full: str):
-    abbreviation_from_full = get_abbreviation_score(parse_name(full), parse_name(abbreviation))
+    abbreviation_from_full = get_abbreviation_score(
+        parse_name(full), parse_name(abbreviation)
+    )
     assert abbreviation_from_full.status.name == "OK"
     assert abbreviation_from_full.value == 1
 
@@ -26,31 +28,53 @@ def test_abbreviation_recognition(abbreviation: str, full: str):
 @pytest.mark.parametrize(
     "names, expected_names",
     [
-        (("Bed & Breakfast Company of Kraków", "Bed & Breakfast", " B&B", "BB"),
-         ("Bed & Breakfast Company of Kraków", "Bed & Breakfast")),
-
-        (("Apple Inc.", "The Film Makers of California",
-          "Narodowy Bank Polski", "NBP", "Microsoft", "FM", "TFMoCa"),
-         ("Apple Inc.", "The Film Makers of California", "Narodowy Bank Polski", "Microsoft"))
-    ]
+        (
+            ("Bed & Breakfast Company of Kraków", "Bed & Breakfast", " B&B", "BB"),
+            ("Bed & Breakfast Company of Kraków", "Bed & Breakfast"),
+        ),
+        (
+            (
+                "Apple Inc.",
+                "The Film Makers of California",
+                "Narodowy Bank Polski",
+                "NBP",
+                "Microsoft",
+                "FM",
+                "TFMoCa",
+            ),
+            (
+                "Apple Inc.",
+                "The Film Makers of California",
+                "Narodowy Bank Polski",
+                "Microsoft",
+            ),
+        ),
+    ],
 )
-def test_members_abbreviations_removing(names: Sequence[str], expected_names: Sequence[str]):
-    without_redundant_abbreviations = \
-        remove_redundant_abbreviations([parse_name(name) for name in names])
-    names_without_redundant_abbreviations = \
-        [name.source.original for name in without_redundant_abbreviations]
+def test_members_abbreviations_removing(
+    names: Sequence[str], expected_names: Sequence[str]
+):
+    without_redundant_abbreviations = remove_redundant_abbreviations(
+        [parse_name(name) for name in names]
+    )
+    names_without_redundant_abbreviations = [
+        name.source.original for name in without_redundant_abbreviations
+    ]
     assert names_without_redundant_abbreviations == list(expected_names)
 
 
 @pytest.mark.parametrize(
     "names, valid_terms",
     [
-        (("SCB", "Standard Chartered Bank"),
-         ("Siam Commercial Bank",)),
-        (("Apple Inc.", "The Walmart", "Narodowy Bank Polski", "NBP", "Microsoft"),
-         ("New Base Project",))
+        (("SCB", "Standard Chartered Bank"), ("Siam Commercial Bank",)),
+        (
+            ("Apple Inc.", "The Walmart", "Narodowy Bank Polski", "NBP", "Microsoft"),
+            ("New Base Project",),
+        ),
     ],
 )
-def test_resolving_with_many_names_abbreviation(names: Sequence[str], valid_terms: Sequence[str]):
+def test_resolving_with_many_names_abbreviation(
+    names: Sequence[str], valid_terms: Sequence[str]
+):
     result = CompanyNameAgent().resolve(names, valid_terms)
     assert result.solution == Solution.NO_MATCH
