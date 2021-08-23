c59d516b4d2002df276a9fdfba610b715307af2a
ksiwecki <ksiwecki@silenteight.com>
2021-08-23 17:55:56 +0200

    test for parentheses separating refact.

--- organization-name-agent/tests/parse/test_separate_parentheses.py
+++ organization-name-agent/tests/parse/test_separate_parentheses.py
@@ -5,10 +5,7 @@ from company_name.names.parse.parse import _separate_parentheses
 
 @pytest.mark.parametrize(
     "name, separate_name, parentheses",
-    [
-        ("Silent Eight", "Silent Eight", []),
-        ("KGHM SA (Poland)", "KGHM SA", ['Poland'])
-    ]
+    [("Silent Eight", "Silent Eight", []), ("KGHM SA (Poland)", "KGHM SA", ["Poland"])],
 )
 def test_separate_parentheses(name: str, separate_name: str, parentheses: str):
     separated_name, separated_parentheses = _separate_parentheses(name=name)
