a4b449b377ef719a3130666e9d43a8e017763461
Adam Wójcicki <awojcicki@silenteight.com>
2021-08-26 10:18:45 +0000

    INTERNAL: Add grpc thread pool configuration for netty (ALL-314)
    Closes ALLIN-314
    
    Reviewed-by: Stefan Wrocławski <swroclawski@silenteight.com>
    Reviewed-by: Szymon Mrozowski <smrozowski@silenteight.com>
    Merged from: https://gitlab.silenteight.com/hsbc-bank/hsbc-bridge/-/merge_requests/344

---
+++ modules/hsbc-bridge/src/main/java/com/silenteight/hsbc/bridge/grpc/autoconfigure/GrpcConfiguration.java
@@ -0,0 +1,42 @@
+package com.silenteight.hsbc.bridge.grpc.autoconfigure;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import io.grpc.ServerBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import io.grpc.netty.shaded.io.netty.util.NettyRuntime;
+import net.devh.boot.grpc.server.serverfactory.GrpcServerConfigurer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+@Configuration
+@ConditionalOnClass(name = "net.devh.boot.grpc.server.serverfactory.NettyGrpcServerFactory")
+@EnableConfigurationProperties(GrpcConfigurationProperties.class)
+@RequiredArgsConstructor
+@Slf4j
+public class GrpcConfiguration {
+
+  private final GrpcConfigurationProperties properties;
+
+  @Bean
+  public GrpcServerConfigurer threadBoundingConfigurer() {
+    return this::configureThreads;
+  }
+
+  private void configureThreads(ServerBuilder<?> serverBuilder) {
+    if (serverBuilder instanceof NettyServerBuilder) {
+      var nettyServerBuilder = (NettyServerBuilder) serverBuilder;
+      var threads = properties.getThreads()
+          .filter(t -> t > 0)
+          .orElse(NettyRuntime.availableProcessors());
+
+      log.info("Using executor service with " + threads + " threads for gRPC handling.");
+      nettyServerBuilder.executor(newFixedThreadPool(threads));
+    }
+  }
+}
