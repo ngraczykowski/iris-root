112cbe5e1ecce47fd0af8ebbd3ff202afd6cd15f
Rafał Pajdak <rpajdak@silenteight.com>
2021-08-30 13:18:01 +0000

    INTERNAL: Remove getAllFiles method.
    Reviewed-by: Przemysław Lewandowski <plewandowski@silenteight.com>
    Reviewed-by: Marek Mastyło <mmastylo@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sep/sep-filestorage-api/-/merge_requests/9

--- modules/sep-filestorage-api/src/main/java/com/silenteight/sep/filestorage/api/FileRetriever.java
+++ modules/sep-filestorage-api/src/main/java/com/silenteight/sep/filestorage/api/FileRetriever.java
@@ -2,11 +2,7 @@ package com.silenteight.sep.filestorage.api;
 
 import com.silenteight.sep.filestorage.api.dto.FileDto;
 
-import java.util.List;
-
 public interface FileRetriever {
 
   FileDto getFile(String storageName, String fileName);
-
-  List<FileDto> getAllFiles(String storageName);
 }
