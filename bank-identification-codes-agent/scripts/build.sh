3e804e049cdfd0982588eb28e3cca78bef954720
Marta Cieślak <mcieslak@silenteight.com>
2021-09-07 20:26:37 +0200

    I have no idea why this scripts are so inconsistent

--- bank-identification-codes-agent/scripts/build.sh
+++ bank-identification-codes-agent/scripts/build.sh
@@ -5,5 +5,10 @@ scriptdir="$(cd -- "$(dirname -- "${0}")" && pwd -P)"
 basedir="$(cd -- "$scriptdir"/.. && pwd -P)"
 cd "$basedir"
 
+if [ -d venv ]; then
+    source venv/bin/activate
+fi
+
+
 python setup.py bdist_wheel
 tox --installpkg ./dist/*.whl "$@"
