e52dc4f596aa601a536e54381fdff1c19b339cbc
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-09-08 16:20:36 +0200

    perf(or is it?): Tune down the auto-analyze ridiculously low
    I was forced to tune that so low, even if the science was telling otherwise.
    Please... Don't blame me...

---
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/resources/db/changelog/sql/alter_table_set_ridiculously_low_autovacuum_analyze_threshold.sql
@@ -0,0 +1,20 @@
+ALTER TABLE ae_agent_exchange_match_feature
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_alert
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_alert_comment_input
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_analysis_alert
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_dataset_alert
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_match
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_match_category_value
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_match_feature_value
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_match_solution
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
+ALTER TABLE ae_recommendation
+  SET (autovacuum_analyze_threshold = 1000, autovacuum_analyze_scale_factor = 0, autovacuum_vacuum_threshold = 1000, autovacuum_vacuum_scale_factor = 0);
