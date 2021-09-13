8f459f0093f03599904a4c13382cc74bd504bda3
ksiwecki <ksiwecki@silenteight.com>
2021-09-13 13:58:53 +0200

    test check no_match all reasons

---
+++ bank-identification-codes-agent/tests/test_check_codes_no_match.py
@@ -0,0 +1,47 @@
+import pytest
+
+from bank_identification_codes_agent.bank_identification_codes import BankIdentificationCodes
+from data_models.reasons import (
+    MatchingTextMatchesWlBicCodeReason,
+    MatchingTextMatchesWlSearchCodeReason,
+)
+from data_models.result import Solution
+
+WL_TYPE = "Some text"  # this param is constant, not modified by any check rule
+
+
+@pytest.mark.parametrize(
+    "ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes",
+    [
+        (
+            "Text with bic codeXXX",  # XXX - headquarters string
+            "bic code",
+            ["some search code"],
+            ["some other bic", "matching biccode"],
+        )
+    ],
+)
+def test_wl_matching_text_in_bic_codes_headquarters_string(
+    ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes
+):
+    codes = BankIdentificationCodes(
+        ap_matching_field, wl_matching_text, WL_TYPE, wl_search_codes, wl_bic_codes
+    )
+    result = codes.check()
+    assert result.solution == Solution.NO_MATCH
+    assert isinstance(result.reason, MatchingTextMatchesWlBicCodeReason)
+
+
+@pytest.mark.parametrize(
+    "ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes",
+    [("search code", "search code", ["some other code", "searchcode"], [])],
+)
+def test_exact_match_in_search_code(
+    ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes
+):
+    codes = BankIdentificationCodes(
+        ap_matching_field, wl_matching_text, WL_TYPE, wl_search_codes, wl_bic_codes
+    )
+    result = codes.check()
+    assert result.solution == Solution.NO_MATCH
+    assert isinstance(result.reason, MatchingTextMatchesWlSearchCodeReason)
