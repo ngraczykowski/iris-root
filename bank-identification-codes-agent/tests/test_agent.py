781b2b4fe75b0906aa1a196b4bfcf4f2861330c1
ksiwecki <ksiwecki@silenteight.com>
2021-09-16 13:15:22 +0200

    agent error test

--- bank-identification-codes-agent/tests/test_agent.py
+++ bank-identification-codes-agent/tests/test_agent.py
@@ -1,6 +1,7 @@
 import pytest
 
 from bank_identification_codes_agent.agent import BankIdentificationCodesAgent
+from data_models.result import Solution
 from tests.conftest import load_test_cases
 
 
@@ -11,6 +12,11 @@ def test_bank_identification_codes_agent(test_case):
     expected_solution, expected_reason = test_case.expected_values
 
     # then
-    print(result.reason)
     assert result.solution == expected_solution
     assert result.reason == expected_reason
+
+
+@pytest.mark.parametrize("agent_input", [(), "Some random text"])
+def test_agent_error(agent_input):
+    result = BankIdentificationCodesAgent().resolve(agent_input)
+    assert result.solution == Solution.AGENT_ERROR
