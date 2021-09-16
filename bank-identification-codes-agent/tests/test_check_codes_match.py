2b647ab2f59897c716866bc1dced9e8626a1c73f
ksiwecki <ksiwecki@silenteight.com>
2021-09-16 13:56:45 +0200

    match specific reason fields checked

--- bank-identification-codes-agent/tests/test_check_codes_match.py
+++ bank-identification-codes-agent/tests/test_check_codes_match.py
@@ -14,7 +14,7 @@ WL_TYPE = "Some text"  # this param is constant, not modified by any check rule
 @pytest.mark.parametrize(
     "ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes",
     [
-        ("bic code", "bic code", ["some search code"], ["different bic", "matching biccode"]),
+        ("bic code", "bic code", ["some search code"], ["matching biccode", "different bic"]),
         ("Your code is: 1234", "1234", ["some search code"], ["bic: 1234"]),
     ],
 )
@@ -27,17 +27,20 @@ def test_wl_matching_text_is_part_of_one_of_bic_codes(
     result = codes.check()
     assert result.solution == Solution.MATCH
     assert isinstance(result.reason, MatchingTextMatchesWlBicCodeReason)
+    assert result.reason.watchlist_matching_text == wl_matching_text
+    assert result.reason.watchlist_type == WL_TYPE
+    assert result.reason.watchlist_bic_code == wl_bic_codes[0].upper()
 
 
 @pytest.mark.parametrize(
-    "ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes",
+    "ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes, expected_ap_matching_text",
     [
-        ("search code123", "search code", ["different search code", "matching searchcode"], []),
-        ("December2020", "2020", ["12 2020"], ["not matching bic"]),
+        ("search code123 XYZ", "search code", ["matching searchcode", "different search code"], [], 'SEARCH CODE123'),
+        ("December2020", "2020", ["12 2020"], ["not matching bic"], "DECEMBER2020"),
     ],
 )
 def test_wl_matching_text_is_part_of_one_of_search_codes_longer_ap(
-    ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes
+    ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes, expected_ap_matching_text,
 ):
     codes = BankIdentificationCodes(
         ap_matching_field, wl_matching_text, WL_TYPE, wl_search_codes, wl_bic_codes
@@ -45,12 +48,17 @@ def test_wl_matching_text_is_part_of_one_of_search_codes_longer_ap(
     result = codes.check()
     assert result.solution == Solution.MATCH
     assert isinstance(result.reason, MatchingTextIsPartOfLongerSequenceReason)
+    assert result.reason.watchlist_matching_text == wl_matching_text
+    assert result.reason.watchlist_type == WL_TYPE
+    assert result.reason.altered_party_matching_field == ap_matching_field
+    assert result.reason.partial_match_text == wl_search_codes[0].upper()
+    assert result.reason.altered_party_matching_sequence == expected_ap_matching_text
 
 
 @pytest.mark.parametrize(
     "ap_matching_field, wl_matching_text, wl_search_codes, wl_bic_codes",
     [
-        ("search code xyz", "search code", ["some other search code", "matching searchcode"], []),
+        ("search code xyz", "search code", ["matching searchcode", "some other search code"], []),
         ("2020", "2020", ["Year 2020"], ["not matching bic"]),
     ],
 )
@@ -63,3 +71,6 @@ def test_wl_matching_text_is_part_of_one_of_search_codes(
     result = codes.check()
     assert result.solution == Solution.MATCH
     assert isinstance(result.reason, MatchingTextIsOnlyPartialMatchForSearchCodeReason)
+    assert result.reason.watchlist_matching_text == wl_matching_text
+    assert result.reason.watchlist_type == WL_TYPE
+    assert result.reason.watchlist_search_codes == [wl_search_codes[0].upper()]
