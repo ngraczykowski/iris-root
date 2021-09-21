a81159919b009e5eab4accff4bd54450875e4c58
Tomasz Bożek <tbozek@silenteight.com>
2021-09-21 11:26:59 +0000

    fear(build): Helm chart deployment

---
+++ modules/payments-bridge/scripts/aws-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x -o pipefail
+export ECR_REPO=278875966478.dkr.ecr.us-east-1.amazonaws.com
+export CHART_NAME=sear-payments-bridge-app
+export IMAGE_REPO=$ECR_REPO/$CHART_NAME
+export IMAGE_SOURCE=docker://docker.repo.silenteight.com/pb/$CHART_NAME:$INSTALLER_VERSION
+export ECR_DESTINATION=docker://$IMAGE_REPO
+echo "Docker image version copied to AWS:" $INSTALLER_VERSION "build number:" $BUILD_NUMBER
+aws ecr get-login-password --region us-east-1 | skopeo login --username AWS --password-stdin $ECR_REPO
+
+skopeo copy $IMAGE_SOURCE $ECR_DESTINATION:$INSTALLER_VERSION
+skopeo copy $IMAGE_SOURCE $ECR_DESTINATION:latest
+
