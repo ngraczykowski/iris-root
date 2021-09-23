93d84ffcaef09def219aabfcdad661cb22444e85
Mateusz Bartecki <mbartecki@silenteight.com>
2021-09-23 10:11:46 +0200

    feat(governance): Design API to get SolvingModel - follow-up (AEP-309)

--- modules/payments-bridge/sear-payments-bridge-governance/src/main/java/com/silenteight/payments/bridge/governance/core/solvingmodel/port/PolicyUpdatedPort.java
+++
@@ -1,7 +0,0 @@
-package com.silenteight.payments.bridge.governance.core.solvingmodel.port;
-
-import com.silenteight.proto.payments.bridge.internal.v1.event.PolicyUpdated;
-
-public interface PolicyUpdatedPort {
-  void send(PolicyUpdated policyUpdated);
-}
