d7a7035f409e8530fa42ed4f1235de64bced6f47
Marek Mastyło <mmastylo@silenteight.com>
2021-09-24 09:51:03 +0000

    INTERNAL: Configure RabbitMQ queues (WEB-1640)
    Configure RabbitMQ queues.
    
    Closes WEB-1640
    
    Reviewed-by: Kamil Dzięcioł <kdzieciol@silenteight.com>
    Reviewed-by: Tomasz Biedrzycki <tbiedrzycki@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/simulator/-/merge_requests/100

---
+++ modules/simulator/simulator-common-web/src/main/java/com/silenteight/simulator/common/integration/BrokerProperties.java
@@ -0,0 +1,39 @@
+package com.silenteight.simulator.common.integration;
+
+import lombok.Data;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+
+@Data
+@Validated
+@ConfigurationProperties(prefix = "simulator.messaging.broker")
+class BrokerProperties {
+
+  @Valid
+  @NestedConfigurationProperty
+  private AmpqProperties recommendations;
+
+  @Valid
+  @NestedConfigurationProperty
+  private AmpqProperties indexResponse;
+
+  String recommendationsQueueName() {
+    return recommendations.getQueueName();
+  }
+
+  String recommendationsRoutingKey() {
+    return recommendations.getRoutingKey();
+  }
+
+  String indexResponseQueueName() {
+    return indexResponse.getQueueName();
+  }
+
+  String indexResponseRoutingKey() {
+    return indexResponse.getRoutingKey();
+  }
+}
