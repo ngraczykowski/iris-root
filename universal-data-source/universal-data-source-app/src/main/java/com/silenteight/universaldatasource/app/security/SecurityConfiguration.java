c10affd2f7e60308e1830d3fe4a804fcfea10a26
jgajewski <Tnt9991&>
2021-10-01 10:33:59 +0200

    fix: Checkstyle

--- universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/security/SecurityConfiguration.java
+++ universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/security/SecurityConfiguration.java
@@ -12,14 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.intercept.aopalliance.MethodSecurityInterceptor;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
