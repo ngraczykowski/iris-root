5c446384938f5162f819bf41d539eb9a37720104
jgajewski <Tnt9991&>
2021-10-01 10:25:23 +0200

    feat(app): Disable security by default and remove keycloak dependency

--- universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/security/SecurityConfiguration.java
+++ universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/security/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.security.authentication.BasicGrpcAuthenticationReader;
 import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
 import org.aopalliance.intercept.MethodInterceptor;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.AdviceMode;
@@ -14,41 +13,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.intercept.aopalliance.MethodSecurityInterceptor;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-
-import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, mode = AdviceMode.ASPECTJ)
 @EnableConfigurationProperties(SecurityProperties.class)
 @RequiredArgsConstructor
-class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   private final SecurityProperties properties;
 
   @Override
-  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-  }
-
-  @Override
   protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http);
 
     if (properties.isDisableSecurity()) {
       http.authorizeRequests().anyRequest().permitAll();
@@ -71,28 +54,6 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
         .formLogin().disable();
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) {
-    var authMapper = new SimpleAuthorityMapper();
-    authMapper.setConvertToUpperCase(true);
-
-    var authProvider = keycloakAuthenticationProvider();
-    authProvider.setGrantedAuthoritiesMapper(authMapper);
-
-    auth.authenticationProvider(authProvider);
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-    return new InMemoryUserDetailsManager();
-  }
-
-  @Override
-  @Bean
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
-
   @Bean
   AccessDecisionManager accessDecisionManager(MethodInterceptor methodSecurityInterceptor) {
     return ((MethodSecurityInterceptor) methodSecurityInterceptor).getAccessDecisionManager();
@@ -101,15 +62,5 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
   @Bean
   GrpcAuthenticationReader grpcAuthenticationReader() {
     return new BasicGrpcAuthenticationReader();
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    var encoders = Map.of(
-        "bcrypt", new BCryptPasswordEncoder(),
-        "pbkdf2", new Pbkdf2PasswordEncoder(),
-        "argon2", new Argon2PasswordEncoder()
-    );
-    return new DelegatingPasswordEncoder("bcrypt", encoders);
   }
 }
