abd75131757dbb28f5dae22936329c171b95ed41
tbozek <tbozek@silenteight.com>
2021-10-05 12:06:08 +0200

    Add snapshot release to ECR

--- modules/adjudication-engine/scripts/aws-deploy.sh
+++ modules/adjudication-engine/scripts/aws-deploy.sh
@@ -2,4 +2,3 @@
 set -x -o pipefail
 echo "Docker image version copied to AWS:" $INSTALLER_VERSION "build number:" $BUILD_NUMBER
 skopeo copy --additional-tag=latest docker://docker.repo.silenteight.com/ae/adjudication-engine-app:$INSTALLER_VERSION docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/adjudication-engine-app:$INSTALLER_VERSION
-skopeo copy docker://docker.repo.silenteight.com/ae/adjudication-engine-app:snapshot docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/adjudication-engine-app:snapshot
