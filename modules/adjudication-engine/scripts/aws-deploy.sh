c21c555b1cba19874ba6b4f4b57bf8c638eea049
tbozek <tbozek@silenteight.com>
2021-10-06 12:05:29 +0200

    Fix deployment

--- modules/adjudication-engine/scripts/aws-deploy.sh
+++ modules/adjudication-engine/scripts/aws-deploy.sh
@@ -3,4 +3,5 @@ set -x -o pipefail
 echo "Docker image version copied to AWS:" $INSTALLER_VERSION "build number:" $BUILD_NUMBER
 skopeo copy docker://docker.repo.silenteight.com/ae/adjudication-engine-app:$INSTALLER_VERSION docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/adjudication-engine-app:$INSTALLER_VERSION
 # latest image for ae is snapshot release
-skopeo copy docker://docker.repo.silenteight.com/ae/adjudication-engine-app:snapshot docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/adjudication-engine-app:latest
+skopeo copy docker://docker.repo.silenteight.com/ae/adjudication-engine-app:snapshot docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/adjudication-engine-app:snapshot
+skopeo copy docker://docker.repo.silenteight.com/ae/adjudication-engine-app:latest docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/adjudication-engine-app:latest
