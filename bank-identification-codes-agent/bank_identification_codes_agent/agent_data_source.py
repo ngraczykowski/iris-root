968cdc45742bf813a5a531eaa38d4492f082df46
ksiwecki <ksiwecki@silenteight.com>
2021-10-11 14:29:01 +0200

    using data source from s8 repo, yielding class not list of values

--- bank-identification-codes-agent/bank_identification_codes_agent/agent_data_source.py
+++ bank-identification-codes-agent/bank_identification_codes_agent/agent_data_source.py
@@ -1,11 +1,14 @@
 from agent_base.agent_exchange.agent_data_source import AgentDataSource
 from silenteight.agents.v1.api.exchange.exchange_pb2 import AgentExchangeRequest
-
-from bank_identification_codes_pb2 import (
+from silenteight.datasource.api.bankidentificationcodes.v1.bank_identification_codes_pb2 import (
     BatchGetMatchBankIdentificationCodesInputsRequest,
     BatchGetMatchBankIdentificationCodesInputsResponse,
 )
-from bank_identification_codes_pb2_grpc import BankIdentificationCodesInputServiceStub
+from silenteight.datasource.api.bankidentificationcodes.v1.bank_identification_codes_pb2_grpc import (
+    BankIdentificationCodesInputServiceStub,
+)
+
+from bank_identification_codes_agent.agent import BankIdentificationCodesAgentInput
 
 
 class BankIdentificationCodesAgentDataSource(AgentDataSource):
@@ -22,11 +25,11 @@ class BankIdentificationCodesAgentDataSource(AgentDataSource):
         )
 
     def parse_response(self, response: BatchGetMatchBankIdentificationCodesInputsResponse):
-        for name_input in response.bank_identification_codes_inputs:
-            match = name_input.match
-            for feature_input in name_input.bank_identification_codes_feature_inputs:
+        for resp_bic_input in response.bank_identification_codes_inputs:
+            match = resp_bic_input.match
+            for feature_input in resp_bic_input.bank_identification_codes_feature_inputs:
                 feature = feature_input.feature
-                args = (
+                args = BankIdentificationCodesAgentInput(
                     feature_input.alerted_party_matching_field,
                     feature_input.watchlist_matching_text,
                     feature_input.watchlist_type,
