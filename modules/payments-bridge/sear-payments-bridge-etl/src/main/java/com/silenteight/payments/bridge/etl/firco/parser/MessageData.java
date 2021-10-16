d81020be6e956d042076dc688160273d3098d7de
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-10-16 17:51:53 +0200

    feat(etl): add easier handling of multi-line tags in the message data

--- modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/etl/firco/parser/MessageData.java
+++ modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/etl/firco/parser/MessageData.java
@@ -1,16 +1,58 @@
 package com.silenteight.payments.bridge.etl.firco.parser;
 
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
 
+import com.google.common.base.Splitter;
+
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 public class MessageData {
 
+  @EqualsAndHashCode.Include
+  @ToString.Include
   private final Map<String, String> tagValues;
 
-  String get(String tagName) {
+  private final Map<String, List<String>> tagLines;
+
+  MessageData(@NonNull Map<String, String> tagValues) {
+    this.tagValues = tagValues;
+    tagLines = splitTagLines(tagValues);
+  }
+
+  private static Map<String, List<String>> splitTagLines(Map<String, String> tagValues) {
+    var tagLines = new HashMap<String, List<String>>();
+    var splitter = Splitter.on('\n');
+
+    tagValues.forEach((tag, value) -> tagLines.put(tag, splitter.splitToList(value)));
+
+    return tagLines;
+  }
+
+  public int getTagCount() {
+    return tagValues.size();
+  }
+
+  public String get(String tagName) {
+    if (!tagValues.containsKey(tagName))
+      throw new IndexOutOfBoundsException("Tag not found: " + tagName);
+
     return tagValues.get(tagName);
+  }
+
+  public int getTagLineCount(String tagName) {
+    return getLines(tagName).size();
+  }
+
+  public List<String> getLines(String tagName) {
+    if (!tagLines.containsKey(tagName))
+      throw new IndexOutOfBoundsException("Tag not found: " + tagName);
+
+    return tagLines.get(tagName);
   }
 }
