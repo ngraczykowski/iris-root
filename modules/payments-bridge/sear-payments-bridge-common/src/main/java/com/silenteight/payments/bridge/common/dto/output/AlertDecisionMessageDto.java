183fa34e94d653247ec3ff2d951c9d5894dc541a
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-10-17 15:26:53 +0200

    fix(firco): try another way to ignore properties

--- modules/payments-bridge/sear-payments-bridge-common/src/main/java/com/silenteight/payments/bridge/common/dto/output/AlertDecisionMessageDto.java
+++ modules/payments-bridge/sear-payments-bridge-common/src/main/java/com/silenteight/payments/bridge/common/dto/output/AlertDecisionMessageDto.java
@@ -20,7 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonNaming(UpperCamelCaseStrategy.class)
-@JsonIgnoreProperties({ "userLogin", "userPassword" })
+@JsonIgnoreProperties({ "UserLogin", "UserPassword" })
 public class AlertDecisionMessageDto implements Serializable {
 
   private static final long serialVersionUID = -6851877425739319284L;
