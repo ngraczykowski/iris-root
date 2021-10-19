ee32d74745c35db1f8f36f4474c06d716fe957e9
wiktor Kęska <wkeska@silenteight.com>
2021-10-19 11:02:15 +0200

    Feat(learning): Extracting Name Match text feature

---
+++ modules/payments-bridge/sear-payments-bridge-svb-learning/src/main/java/com/silenteight/payments/bridge/svb/learning/features/service/NameMatchedTextFeatureExtractor.java
@@ -0,0 +1,67 @@
+package com.silenteight.payments.bridge.svb.learning.features.service;
+
+import com.silenteight.datasource.agentinput.api.v1.FeatureInput;
+import com.silenteight.datasource.api.name.v1.AlertedPartyName;
+import com.silenteight.datasource.api.name.v1.NameFeatureInput;
+import com.silenteight.datasource.api.name.v1.NameFeatureInput.EntityType;
+import com.silenteight.datasource.api.name.v1.WatchlistName;
+import com.silenteight.datasource.api.name.v1.WatchlistName.NameType;
+import com.silenteight.payments.bridge.common.dto.common.WatchlistType;
+import com.silenteight.payments.bridge.svb.learning.reader.domain.LearningMatch;
+
+import com.google.protobuf.Any;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Nonnull;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@Qualifier("nameMatchedText")
+class NameMatchedTextFeatureExtractor implements FeatureExtractor {
+
+  @Override
+  public FeatureInput extract(LearningMatch learningMatch) {
+    var watchlistNames = learningMatch
+        .getWatchlistNames()
+        .stream()
+        .map(name -> WatchlistName.newBuilder().setName(name).setType(NameType.REGULAR).build())
+        .collect(toList());
+
+    var alertedPartyNames = learningMatch
+        .getAlertedPartyNames()
+        .stream()
+        .map(name -> AlertedPartyName.newBuilder().setName(name).build())
+        .collect(toList());
+
+    return FeatureInput
+        .newBuilder()
+        .setFeature("features/nameMatchedText")
+        .setAgentFeatureInput(Any.pack(NameFeatureInput
+            .newBuilder()
+            .setFeature("features/nameMatchedText")
+            .setAlertedPartyType(learningMatch.getEntityType())
+            .addAllMatchingTexts(learningMatch.getMatchingTexts())
+            .addAllWatchlistNames(watchlistNames)
+            .addAllAlertedPartyNames(alertedPartyNames)
+            .setAlertedPartyType(mapWatchListTypeToEntityType(learningMatch.getWatchlistType()))
+            .build()))
+        .build();
+  }
+
+  @Nonnull
+  private static EntityType mapWatchListTypeToEntityType(WatchlistType watchlistType) {
+    switch (watchlistType) {
+      case INDIVIDUAL:
+        return EntityType.INDIVIDUAL;
+      case COMPANY:
+        return EntityType.ORGANIZATION;
+      case ADDRESS:
+      case VESSEL:
+        return EntityType.ENTITY_TYPE_UNSPECIFIED;
+      default:
+        throw new UnsupportedOperationException();
+    }
+  }
+}
