a4b0b9e4768fcae87ca27879b9472989c30b82f6
wiktor Kęska <wkeska@silenteight.com>
2021-10-21 17:07:29 +0200

    feat(etl): Extracting for other cases in PEP

--- modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/svb/oldetl/service/shitcode/ExtractPepAlertedPartyData.java
+++ modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/svb/oldetl/service/shitcode/ExtractPepAlertedPartyData.java
@@ -8,7 +8,6 @@ import com.silenteight.payments.bridge.svb.oldetl.response.MessageFieldStructure
 
 import com.google.common.base.Splitter;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -40,32 +39,43 @@ public class ExtractPepAlertedPartyData {
   12345
   */
 
+  private static final int LINE_1 = 0;
   private static final int LINE_2 = 1;
   private static final int LINE_3 = 2;
   private static final int LINE_4 = 3;
-  private static final int TO_LINE_4 = 4;
+  private static final int TO_LINE_3 = 2;
+  private static final int TO_LINE_4 = 3;
   protected static final Splitter LINE_SPLITTER = Splitter.on("\n");
 
   private final MessageData messageData;
   private final String hitTag;
 
   public AlertedPartyData extract(MessageFieldStructure messageFieldStructure) {
-    var lines = messageData.getLines(hitTag);
+    var values = messageData.findAllValues(hitTag).collect(Collectors.joining("\n"));
 
-    if (lines.size() != 5)
+    var lines = LINE_SPLITTER.splitToList(values);
+
+    if (lines.size() < 5 || lines.size() > 6)
       throw new IllegalArgumentException("I've no idea how to get data from PEP: " + hitTag);
 
-    return AlertedPartyData.builder()
-        .messageFieldStructure(messageFieldStructure)
-        .name(lines.get(LINE_2))
-        .address(lines.get(LINE_3))
-        .ctryTown(lines.get(LINE_4))
-        .nameAddresses(lines.subList(LINE_2, TO_LINE_4))
-        .build();
-  }
-
-  private List<String> getOrgBeneInfLines() {
-    var concat = messageData.findAllValues("ORGBENEINF").collect(Collectors.joining("\n"));
-    return LINE_SPLITTER.splitToList(concat);
+    switch (lines.size()) {
+      default:
+      case 5:
+        return AlertedPartyData.builder()
+            .messageFieldStructure(messageFieldStructure)
+            .name(lines.get(LINE_2))
+            .address(lines.get(LINE_3))
+            .ctryTown(lines.get(LINE_4))
+            .nameAddresses(lines.subList(LINE_2, TO_LINE_4))
+            .build();
+      case 6:
+        return AlertedPartyData.builder()
+            .messageFieldStructure(messageFieldStructure)
+            .name(lines.get(LINE_1))
+            .address(lines.get(LINE_2))
+            .ctryTown(lines.get(LINE_3))
+            .nameAddresses(lines.subList(LINE_1, TO_LINE_3))
+            .build();
+    }
   }
 }
