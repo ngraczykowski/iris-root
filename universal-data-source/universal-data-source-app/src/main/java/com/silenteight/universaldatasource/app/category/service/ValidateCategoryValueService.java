54e8636e3005b3152d5d185625fed86e09551d34
Jacek Gajewski <jgajewski@silenteight.com>
2021-10-22 09:17:40 +0000

    fix: Category Value validation

--- universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/category/service/ValidateCategoryValueService.java
+++ universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/category/service/ValidateCategoryValueService.java
@@ -3,6 +3,7 @@ package com.silenteight.universaldatasource.app.category.service;
 import lombok.RequiredArgsConstructor;
 
 import com.silenteight.datasource.categories.api.v2.Category;
+import com.silenteight.datasource.categories.api.v2.CategoryType;
 import com.silenteight.datasource.categories.api.v2.CategoryValue;
 import com.silenteight.datasource.categories.api.v2.CreateCategoryValuesRequest;
 import com.silenteight.universaldatasource.app.category.port.incoming.ListAvailableCategoriesUseCase;
@@ -47,10 +48,19 @@ class ValidateCategoryValueService implements ValidateCategoryValueUseCase {
   }
 
   private static void validateCategoryValue(Category category, CategoryValue categoryValue) {
-    if (!category.getAllowedValuesList().contains(categoryValue.getSingleValue())) {
+    if (!isCategoryValueAllowed(category, categoryValue) &&
+        !isCategoryAnyString(category.getType())) {
       throw new CategoryValueException(
           "Invalid value of CategoryValue: " + categoryValue.getSingleValue());
     }
+  }
+
+  private static boolean isCategoryValueAllowed(Category category, CategoryValue categoryValue) {
+    return category.getAllowedValuesList().contains(categoryValue.getSingleValue());
+  }
+
+  private static boolean isCategoryAnyString(CategoryType categoryType) {
+    return CategoryType.ANY_STRING.equals(categoryType);
   }
 }
 
