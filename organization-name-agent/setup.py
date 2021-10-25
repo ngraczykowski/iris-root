021d233ddbf3bad1c19b9dab5b94aed4fbebf30a
jenkins <jenkins@silenteight.com>
2021-10-25 21:56:41 +0000

    Bump version: 0.6.0-dev → 0.7.0-dev

--- organization-name-agent/setup.py
+++ organization-name-agent/setup.py
@@ -35,7 +35,7 @@ tests_require = [
 
 setup(
     name="company-name",
-    version="0.6.0-dev",
+    version="0.7.0-dev",
     description="",
     author="Silent Eight Pte. Ltd.",
     author_email="support@silenteight.com",
