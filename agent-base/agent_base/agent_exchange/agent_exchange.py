6d0653473a19cf60f07639ecd4a79cb1cf40b8d7
Andrzej Haczewski <ahaczewski@silenteight.com>
2021-10-25 23:43:29 +0200

    Fix Lz4 compressor settings to compatible with Java apps

--- agent-base/agent_base/agent_exchange/agent_exchange.py
+++ agent-base/agent_base/agent_exchange/agent_exchange.py
@@ -65,8 +65,17 @@ class AgentExchange(AgentService):
         request: AgentExchangeRequest = AgentExchangeRequest.FromString(body)
         response: AgentExchangeResponse = await self.process(request)
 
+        response_body = lz4.frame.compress(
+            response.SerializeToString(),
+            block_size=lz4.frame.BLOCKSIZE_MAX64KB,
+            block_linked=False,
+            compression_level=lz4.frame.COMPRESSIONLEVEL_MINHC,
+            content_checksum=True,
+            store_size=False,
+        )
+
         return aio_pika.Message(
-            lz4.frame.compress(response.SerializeToString()),
+            response_body,
             content_encoding="lz4",
             content_type="application/x-protobuf",
             delivery_mode=message.delivery_mode,
