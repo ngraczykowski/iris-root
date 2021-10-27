f8eecc1bd1f9839d8b00a405b862b78eac538f88
Zbigniew Ruchała <zbigniew.ruchala@silenteight.com>
2021-10-27 16:24:02 +0200

    fix(mockae): fetch the current analysis for tests

--- modules/payments-bridge/sear-payments-bridge-mock-ae/src/main/java/com/silenteight/payments/bridge/mock/ae/RecommendationShortPathIntegration.java
+++ modules/payments-bridge/sear-payments-bridge-mock-ae/src/main/java/com/silenteight/payments/bridge/mock/ae/RecommendationShortPathIntegration.java
@@ -1,5 +1,7 @@
 package com.silenteight.payments.bridge.mock.ae;
 
+import lombok.RequiredArgsConstructor;
+
 import com.silenteight.adjudication.api.v1.RecommendationsGenerated;
 import com.silenteight.adjudication.api.v1.RecommendationsGenerated.RecommendationInfo;
 import com.silenteight.payments.bridge.event.AlertInputAcceptedEvent;
@@ -8,6 +10,7 @@ import com.silenteight.payments.bridge.event.RecommendationGeneratedEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -16,7 +19,10 @@ import static com.silenteight.payments.bridge.common.integration.CommonChannels.
 
 @Profile("mockae")
 @MessageEndpoint
+@RequiredArgsConstructor
 class RecommendationShortPathIntegration {
+
+  private final JdbcTemplate jdbcTemplate;
 
   /*
    * Simulate issuing recommendation just after universal-data-source confirmed input values.
@@ -24,9 +30,10 @@ class RecommendationShortPathIntegration {
    */
   @ServiceActivator(inputChannel = ALERT_INPUT_ACCEPTED, outputChannel = RECOMMENDATION_GENERATED)
   RecommendationGeneratedEvent generateRecommendation(AlertInputAcceptedEvent event) {
+    var analysisName = getAnalysisName();
     var recommendationId = ThreadLocalRandom.current().nextInt(10000) + 1;
     var recommendationsGenerated = RecommendationsGenerated.newBuilder()
-        .setAnalysis("analysis/MOCKAE")
+        .setAnalysis(analysisName)
         .addRecommendationInfos(
             RecommendationInfo.newBuilder()
                 .setAlert(event.getAlertRegisteredName())
@@ -34,6 +41,12 @@ class RecommendationShortPathIntegration {
                 .build()).build();
 
     return new RecommendationGeneratedEvent(recommendationsGenerated);
+  }
+
+  private String getAnalysisName() {
+    return jdbcTemplate.queryForObject("select analysis_name from pb_analysis "
+        + "where created_at > now() - interval '15 min' order by created_at desc limit 1",
+        String.class);
   }
 
 }
