27836f93e30e7c215bba7358a20dbcb91c43a6b6
ksiwecki <ksiwecki@silenteight.com>
2021-11-12 15:20:19 +0100

    remove from API function related to country names

--- organization-name-knowledge/organization_name_knowledge/api.py
+++ organization-name-knowledge/organization_name_knowledge/api.py
@@ -27,26 +27,6 @@ def parse(name: str) -> NameInformation:
     return name_information
 
 
-def get_all_country_names(name: str) -> Set[str]:
-    """Country names returned by this function are slightly different than those returned by main
-    "parse" function - they may come from i.e. part of the name, or from inside the name string.
-    In other words, they are all of the substrings that match any of known country names.
-
-    Parameters
-    ----------
-    name : str
-        In most of use cases it is an organization name
-
-    Returns
-    -------
-    Set[str]
-        Set of name substrings that match any of known country names
-    """
-
-    countries_term_sources = KnowledgeBase.countries.countries
-    return _get_matching_tokens(name, countries_term_sources)
-
-
 def get_all_legal_terms(name: str) -> Set[str]:
     """Legal terms returned by this function are slightly different than those returned by main
     "parse" function - they may come from i.e. part of the name, or from inside the name string.
