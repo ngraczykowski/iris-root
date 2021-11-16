531df50f4d058eddf068aeae2f1e4f39b1d00720
ksiwecki <ksiwecki@silenteight.com>
2021-11-16 12:37:39 +0100

    test cases added

--- organization-name-knowledge/tests/test_freetext.py
+++ organization-name-knowledge/tests/test_freetext.py
@@ -28,9 +28,21 @@ from organization_name_knowledge import parse_freetext
                 {"base": "Hewlett and Packard", "legal": "company"},
             ],
         ),
+        (
+            "ACME CO and Google Inc",
+            [{"base": "ACME", "legal": "CO"}, {"base": "Google", "legal": "inc"}],
+        ),
+        (
+            "Paramount Pictures LLC or Walt Disney Company",
+            [
+                {"base": "paramount pictures", "legal": "LLC"},
+                {"base": "Walt Disney", "legal": "company"},
+            ],
+        ),
     ],
 )
 def test_freetext(freetext, expected_names):
+    print(len(parse_freetext(freetext)))
     for name_information, expected in zip(parse_freetext(freetext), expected_names):
         assert name_information.base.cleaned_name == expected["base"].lower()
         assert name_information.legal.cleaned_name == expected["legal"].lower()
