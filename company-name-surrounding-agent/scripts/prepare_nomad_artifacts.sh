e7b3b51438db03f88258b5195af7e88c27b831d4
ksiwecki <ksiwecki@silenteight.com>
2021-11-23 17:56:49 +0100

    nomad artifacts scripts added

---
+++ company-name-surrounding-agent/scripts/prepare_nomad_artifacts.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set  -e -o pipefail
+scriptdir="$(cd -- "$(dirname -- "${0}")" && pwd -P)"
+basedir="$(cd -- "$scriptdir"/.. && pwd -P)"
+cd "$basedir"
+
+IFS='-' read -r name version rest <<< $(basename -- "$(ls -tr ./dist/*.whl)")
+
+artifact_path="$(ls "$scriptdir"/../dist/*.pyz)"
+config="${name}-config-${version}.tgz"
+config_path="$(ls "./dist/${config}")"
+
+set -x
+
+rm -rf ./nomad/artifacts/
+mkdir -p ./nomad/artifacts/
+
+cp "$artifact_path" ./nomad/artifacts/
+cp "$config_path" ./nomad/artifacts/
+
+cd nomad
+tar -cvjSf ../dist/"${name}-nomad-${version}.tar.bz2" -- *
+cd -
