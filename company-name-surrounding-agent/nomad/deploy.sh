bc48a25dc3a1c0ec98ecc6dd38c76a86a662b27f
ksiwecki <ksiwecki@silenteight.com>
2021-11-24 14:37:27 +0100

    s3 in minio

--- company-name-surrounding-agent/nomad/deploy.sh
+++ company-name-surrounding-agent/nomad/deploy.sh
@@ -8,10 +8,10 @@ if [[ -z "$MINIO_ADDR" ]]; then
         minio_discovery=$(consul watch -type=service -service=minio | jq --raw-output '.[0].Service | [ .Address, (.Port | tostring) ] | join(":")')
     fi
 
-    MINIO_ADDR="s3::https://"${minio_discovery:-"localhost:9000"}
+    MINIO_ADDR="https://"${minio_discovery:-"localhost:9000"}
 fi
 
-MINIO_ALIAS=${MINIO_ALIAS:-minio}
+MINIO_ALIAS="s3::"${MINIO_ALIAS:-minio}
 
 export NOMAD_ADDR="${NOMAD_ADDR:-http://localhost:4646}"
 
@@ -22,10 +22,10 @@ IFS='-' read -r name version <<< "${artifact%.pyz}"
 config="${name}-config-${version}.tgz"
 
 
-export NOMAD_VAR_company_name_surrounding_agent_artifact=${NOMAD_VAR_company_name_surrounding_agent_artifact:-"${MINIO_ADDR}/artifacts/company-name-surrounding-agent/${artifact}"}
+export NOMAD_VAR_company_name_surrounding_agent_artifact=${NOMAD_VAR_company_name_surrounding_agent_artifact:-"s3::${MINIO_ADDR}/artifacts/company-name-surrounding-agent/${artifact}"}
 export NOMAD_VAR_company_name_surrounding_agent_artifact_checksum=${NOMAD_VAR_company_name_surrounding_agent_artifact_checksum:-"sha256:$(sha256sum "$artifact_path" | awk '{print $1}')"}
 export NOMAD_VAR_company_name_surrounding_agent_version="$version"
-export NOMAD_VAR_company_name_surrounding_agent_config=${NOMAD_VAR_company_name_surrounding_agent_config:-"${MINIO_ADDR}/artifacts/company-name-surrounding-agent/${config}"}
+export NOMAD_VAR_company_name_surrounding_agent_config=${NOMAD_VAR_company_name_surrounding_agent_config:-"s3::${MINIO_ADDR}/artifacts/company-name-surrounding-agent/${config}"}
 
 cd "$scriptdir"
 set -x
