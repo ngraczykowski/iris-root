30dda7563045ba18a0be05c174265fa01b7ea054
Jacek Gajewski <jgajewski@silenteight.com>
2021-12-15 09:29:47 +0000

    Revert "feat(alerts): map existing alert source labels (AEP-563)"
    This reverts commit ffafdd7b5072ba07cb829885638a186da0f9cc48.
    
    Reviewed-by: Tomasz Bożek <tbozek@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/adjudication-engine/-/merge_requests/156

--- modules/adjudication-engine/adjudication-engine-alerts/src/main/resources/db/changelog/sql/update_ae_alert_labels_source_CMAPI_to_solving.sql
+++
@@ -1,3 +0,0 @@
-UPDATE ae_alert_labels
-SET value = 'solving'
-WHERE name = 'source' AND value = 'CMAPI';
--- modules/adjudication-engine/adjudication-engine-alerts/src/main/resources/db/changelog/sql/update_ae_alert_labels_source_CSV_to_learning.sql
+++
@@ -1,3 +0,0 @@
-UPDATE ae_alert_labels
-SET value = 'learning'
-WHERE name = 'source' AND value = 'CSV'
