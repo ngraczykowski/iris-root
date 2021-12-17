f392724738f503bdb7f12efc300b5b498a32d89c
Tomasz Bożek <tbozek@silenteight.com>
2021-12-17 10:15:02 +0000

    feat(learning): New historical risk assessment job (AEP-640)
    New historical risk assessment job (AEP-640)
    
    Reviewed-by: Wiktor Kęska <wkeska@silenteight.com>
    Reviewed-by: Jacek Gajewski <jgajewski@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/sear-payments-bridge/-/merge_requests/286

---
+++ modules/payments-bridge/sear-payments-bridge-svb-learning/src/main/java/com/silenteight/payments/bridge/svb/newlearning/job/etl/historical/HistoricalRiskAssessmentJobConfiguration.java
@@ -0,0 +1,16 @@
+package com.silenteight.payments.bridge.svb.newlearning.job.etl.historical;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+@Slf4j
+class HistoricalRiskAssessmentJobConfiguration {
+
+  private final JobBuilderFactory jobBuilderFactory;
+
+}
---
+++ modules/payments-bridge/sear-payments-bridge-svb-learning/src/main/java/com/silenteight/payments/bridge/svb/newlearning/job/unregistered/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package com.silenteight.payments.bridge.svb.newlearning.job.unregistered;
+
+import javax.annotation.ParametersAreNonnullByDefault;
