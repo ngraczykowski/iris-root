b8204c1f31287e33c343b2a37c550d82fc52cb38
Wiktor Kęska <wkeska@silenteight.com>
2021-12-22 09:55:30 +0000

    feat(common): Converting to OffsetDateTime from TimeStamp (AEP-652)

--- modules/payments-bridge/sear-payments-common/src/main/java/com/silenteight/payments/common/app/OffsetTimeConverter.java
+++ modules/payments-bridge/sear-payments-common/src/main/java/com/silenteight/payments/common/app/OffsetTimeConverter.java
@@ -1,5 +1,7 @@
 package com.silenteight.payments.common.app;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
@@ -8,8 +10,8 @@ import javax.annotation.Nonnull;
 
 public class OffsetTimeConverter {
 
-  private static final DateTimeFormatter DATE_FORMAT =
-      DateTimeFormatter.ofPattern("[dd/MM/yyyy HH:mm:ss][dd/MM/yyyy H:mm:ss]");
+  private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern(
+          "[dd/MM/yyyy HH:mm:ss][dd/MM/yyyy H:mm:ss][yyyy-MM-dd HH:mm:ss][yyyy-MM-dd H:mm:ss]");
 
   @Nonnull
   public static OffsetDateTime getOffsetDateTime(String timeZone, String time) {
@@ -17,5 +19,10 @@ public class OffsetTimeConverter {
         .parse(time, DATE_FORMAT)
         .atZone(ZoneId.of(timeZone))
         .toOffsetDateTime();
+  }
+
+
+  public static OffsetDateTime getOffsetDateTime(String timeZone, Timestamp timestamp) {
+    return OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp.getTime()), ZoneId.of(timeZone));
   }
 }
