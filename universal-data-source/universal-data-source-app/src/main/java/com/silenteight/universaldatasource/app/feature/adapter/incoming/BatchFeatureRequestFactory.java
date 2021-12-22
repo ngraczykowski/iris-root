ed316de9d9c0c121e92b35c21ccc4331358e3900
Jacek Gajewski <jgajewski@silenteight.com>
2021-12-22 18:02:09 +0100

    fix: checkstyle

--- universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/feature/adapter/incoming/BatchFeatureRequestFactory.java
+++ universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/feature/adapter/incoming/BatchFeatureRequestFactory.java
@@ -1,10 +1,9 @@
 package com.silenteight.universaldatasource.app.feature.adapter.incoming;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.universaldatasource.app.feature.model.BatchFeatureRequest;
-
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
