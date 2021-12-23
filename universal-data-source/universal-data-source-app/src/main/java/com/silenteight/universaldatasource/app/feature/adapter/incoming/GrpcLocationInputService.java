801353643de1745354b8ec6ae0ed35ff477ae3d3
Jacek Gajewski <jgajewski@silenteight.com>
2021-12-23 14:36:43 +0100

    fix: checkstyle

--- universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/feature/adapter/incoming/GrpcLocationInputService.java
+++ universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/feature/adapter/incoming/GrpcLocationInputService.java
@@ -1,15 +1,13 @@
 package com.silenteight.universaldatasource.app.feature.adapter.incoming;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.datasource.api.location.v1.BatchGetMatchLocationInputsRequest;
 import com.silenteight.datasource.api.location.v1.BatchGetMatchLocationInputsResponse;
 import com.silenteight.datasource.api.location.v1.LocationInputServiceGrpc.LocationInputServiceImplBase;
 
 import io.grpc.stub.StreamObserver;
-
-import lombok.extern.slf4j.Slf4j;
-
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @Slf4j
