6372ff7b61ee81ed15f6e06d91325ae97fe9b0a7
Jacek Gajewski <jgajewski@silenteight.com>
2021-12-23 14:29:27 +0100

    deployment: temporary GrpcLocationInputService logging

--- universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/feature/adapter/incoming/GrpcLocationInputService.java
+++ universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/feature/adapter/incoming/GrpcLocationInputService.java
@@ -7,8 +7,12 @@ import com.silenteight.datasource.api.location.v1.BatchGetMatchLocationInputsRes
 import com.silenteight.datasource.api.location.v1.LocationInputServiceGrpc.LocationInputServiceImplBase;
 
 import io.grpc.stub.StreamObserver;
+
+import lombok.extern.slf4j.Slf4j;
+
 import net.devh.boot.grpc.server.service.GrpcService;
 
+@Slf4j
 @GrpcService
 @RequiredArgsConstructor
 class GrpcLocationInputService extends LocationInputServiceImplBase {
@@ -19,6 +23,16 @@ class GrpcLocationInputService extends LocationInputServiceImplBase {
   public void batchGetMatchLocationInputs(
       BatchGetMatchLocationInputsRequest request,
       StreamObserver<BatchGetMatchLocationInputsResponse> responseObserver) {
+
+    if (log.isDebugEnabled()) {
+      var matches = request.getMatchesList();
+
+      log.debug(
+          "Streaming feature inputs: agentInputType={}, features={}, matchCount={}"
+              + ", firstTenMatches={}",
+          "GEO", request.getFeaturesList(),
+          matches.size(), matches.subList(0, Math.min(10, matches.size())));
+    }
 
     featureAdapter.batchGetMatchLocationInputs(request, responseObserver::onNext);
     responseObserver.onCompleted();
