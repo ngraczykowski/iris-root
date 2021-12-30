7fd52f5054c70612356e75c35db6ef7237f4ca70
Paweł Pietrucha <ppietrucha@silenteight.com>
2021-12-30 11:56:59 +0100

    feat(metrics): Expose alert resolution time - remove Spring foam (aka. refactoring) (AEP-606)

--- modules/payments-bridge/sear-payments-bridge-app/src/main/java/com/silenteight/payments/bridge/app/metrics/efficiency/EfficiencyMetricsIncrementer.java
+++ modules/payments-bridge/sear-payments-bridge-app/src/main/java/com/silenteight/payments/bridge/app/metrics/efficiency/EfficiencyMetricsIncrementer.java
@@ -5,13 +5,16 @@ import lombok.RequiredArgsConstructor;
 import com.silenteight.payments.bridge.firco.metrics.efficiency.EfficiencyMetricIncrementerPort;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 class EfficiencyMetricsIncrementer implements EfficiencyMetricIncrementerPort {
 
-  private final EfficiencyMetricsMeter efficiencyMetricsMeter;
+  private static final String PB_EFFICIENCY_METRICS_PREFIX = "pb.metrics.efficiency";
+  private static final String TAG_TYPE = "action";
+  private final MeterRegistry meterRegistry;
 
   @Override
   public void incrementManualInvestigation() {
@@ -24,8 +27,15 @@ class EfficiencyMetricsIncrementer implements EfficiencyMetricIncrementerPort {
   }
 
   private void incrementCounter(String counterTagName) {
-    Counter counter = this.efficiencyMetricsMeter.fetchCounter(counterTagName);
+    Counter counter = this.fetchCounter(counterTagName);
     counter.increment();
+  }
+
+  Counter fetchCounter(String action) {
+    return Counter
+        .builder(PB_EFFICIENCY_METRICS_PREFIX)
+        .tag(TAG_TYPE, action)
+        .register(this.meterRegistry);
   }
 
 }
