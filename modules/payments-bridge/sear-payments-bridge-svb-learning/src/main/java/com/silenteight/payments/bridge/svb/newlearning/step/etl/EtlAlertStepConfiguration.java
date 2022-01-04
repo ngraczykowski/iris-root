fd62a122e2b2e68d1157c150403f20915a3e74ed
Wiktor Kęska <wkeska@silenteight.com>
2022-01-04 08:52:54 +0000

    Ref(learning): Removed chained processor (AEP-602)

--- modules/payments-bridge/sear-payments-bridge-svb-learning/src/main/java/com/silenteight/payments/bridge/svb/newlearning/step/etl/EtlAlertStepConfiguration.java
+++ modules/payments-bridge/sear-payments-bridge-svb-learning/src/main/java/com/silenteight/payments/bridge/svb/newlearning/step/etl/EtlAlertStepConfiguration.java
@@ -13,15 +13,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.listener.JobParameterExecutionContextCopyListener;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.support.AbstractItemStreamItemReader;
-import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 import static com.silenteight.payments.bridge.svb.newlearning.job.etl.EtlJobConstants.ETL_STEP_NAME;
 
@@ -49,24 +45,13 @@ class EtlAlertStepConfiguration {
   }
 
   @Bean
-  @StepScope
-  public ItemProcessor<AlertComposite, EtlAlert> etlAlertsCompositeProcessor(
-      MarkAlertRegistrationStatusProcessor markAlertRegistrationstatusProcessor) {
-    var etlAlertsCompositeProcessor = new CompositeItemProcessor<AlertComposite, EtlAlert>();
-    etlAlertsCompositeProcessor.setDelegates(List.of(markAlertRegistrationstatusProcessor));
-    return etlAlertsCompositeProcessor;
-  }
-
-  @Bean
   Step processUnregisterAlertStep(
-      AbstractItemStreamItemReader<AlertComposite> compositeAlertReader,
-      ItemProcessor<AlertComposite, EtlAlert> etlAlertsCompositeProcessor) {
+      AbstractItemStreamItemReader<AlertComposite> compositeAlertReader) {
     return stepBuilderFactory
         .get(ETL_STEP_NAME)
         .listener(new JobParameterExecutionContextCopyListener())
         .<AlertComposite, EtlAlert>chunk(properties.getChunkSize())
         .reader(compositeAlertReader)
-        .processor(etlAlertsCompositeProcessor)
         .writer(item -> log.debug("writing item: {}", item))
         .build();
   }
