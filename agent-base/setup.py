adb98c6249ca7fc1102261c2ade53cb974cbe051
Kamil Siwecki <ksiwecki@silenteight.com>
2022-01-11 10:07:15 +0000

    Ks/data source api version update

--- agent-base/setup.py
+++ agent-base/setup.py
@@ -1,19 +1,19 @@
 from setuptools import find_packages, setup
 
 install_require = [
-    "agents-api",
-    "aio_pika",
-    "aiohttp",
-    "data-source-api",
-    "googleapis-common-protos",
-    "grpcio",
-    "grpcio-health-checking",
-    "grpcio-reflection",
-    "grpcio-tools",
-    "lz4",
+    "agents-api==0.7.0.33",
+    "aio_pika==6.8.0",
+    "aiohttp==3.7.4.post0",
+    "data-source-api==0.18.0",
+    "googleapis-common-protos==1.53.0",
+    "grpcio==1.39.0",
+    "grpcio-health-checking==1.39.0",
+    "grpcio-reflection==1.39.0",
+    "grpcio-tools==1.39.0",
+    "lz4==3.1.3",
     "protocol-agents",
-    "python-consul2",
-    "pyyaml",
+    "python-consul2==0.1.5",
+    "PyYAML==5.4.1",
 ]
 
 tests_require = [
