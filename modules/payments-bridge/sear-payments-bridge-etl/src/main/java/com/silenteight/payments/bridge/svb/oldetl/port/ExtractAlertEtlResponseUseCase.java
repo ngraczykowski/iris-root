7ab3569b2b9f589e54c408d99f61abec567d851a
Jacek Gajewski <jgajewski@silenteight.com>
2022-01-27 10:03:39 +0000

    feat(etl): add method for retrieving only watchlist values (AEP2-6) This method is necessary for agents and categories that do not need etl-ed data.

--- modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/svb/oldetl/port/ExtractAlertEtlResponseUseCase.java
+++ modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/svb/oldetl/port/ExtractAlertEtlResponseUseCase.java
@@ -2,8 +2,14 @@ package com.silenteight.payments.bridge.svb.oldetl.port;
 
 import com.silenteight.payments.bridge.common.dto.input.AlertMessageDto;
 import com.silenteight.payments.bridge.svb.oldetl.response.AlertEtlResponse;
+import com.silenteight.payments.bridge.svb.oldetl.response.HitAndWatchlistPartyData;
+
+import java.util.Map;
 
 public interface ExtractAlertEtlResponseUseCase {
 
   AlertEtlResponse createAlertEtlResponse(AlertMessageDto alertMessageDto);
+
+  Map<String, HitAndWatchlistPartyData> getWatchlistDataForMatch(AlertMessageDto alertMessageDto);
+
 }
--- modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/svb/oldetl/service/AlertParserService.java
+++ modules/payments-bridge/sear-payments-bridge-etl/src/main/java/com/silenteight/payments/bridge/svb/oldetl/service/AlertParserService.java
@@ -21,8 +21,12 @@ import com.silenteight.payments.bridge.svb.oldetl.service.impl.*;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static com.silenteight.payments.bridge.common.dto.common.MessageStructure.STRUCTURED;
+import static com.silenteight.payments.bridge.common.dto.common.MessageStructure.ofTag;
 import static com.silenteight.payments.bridge.svb.oldetl.response.MessageFieldStructure.NAMEADDRESS_FORMAT_F;
 import static com.silenteight.payments.bridge.svb.oldetl.response.MessageFieldStructure.UNSTRUCTURED;
 import static com.silenteight.payments.bridge.svb.oldetl.util.CommonTerms.*;
@@ -41,6 +45,7 @@ public class AlertParserService implements ExtractAlertEtlResponseUseCase {
           FIRCO_FORMAT_IAT_O,
           FIRCO_FORMAT_O_F);
 
+  @Override
   public AlertEtlResponse createAlertEtlResponse(AlertMessageDto alertMessageDto) {
     if (log.isDebugEnabled()) {
       log.debug("Parsing alert for ETL: systemId={}", alertMessageDto.getSystemID());
@@ -48,9 +53,7 @@ public class AlertParserService implements ExtractAlertEtlResponseUseCase {
 
     var hitData = new ArrayList<HitData>();
     var skippedHits = new ArrayList<String>();
-    var messageData = messageParserUseCase.parse(
-        MessageFormat.valueOf(alertMessageDto.getMessageFormat()),
-        alertMessageDto.getMessageData());
+    var messageData = getMessageData(alertMessageDto);
 
     var alertMessageDtoHits = alertMessageDto.getHits();
     // XXX(ahaczewski): WATCH OUT! RegisterAlertEndpoint#getMatchIds() assumes the same iteration
@@ -58,8 +61,12 @@ public class AlertParserService implements ExtractAlertEtlResponseUseCase {
     for (int idx = 0, hitCount = alertMessageDtoHits.size(); idx < hitCount; idx++) {
       var hit = alertMessageDtoHits.get(idx).getHit();
       if (hit.isBlocking()) {
-        hitData.add(createHitData(
-            alertMessageDto.getApplicationCode(), messageData, hit, idx));
+
+        if (ofTag(hit.getTag()).equals(STRUCTURED)) {
+          hitData.add(createHitData(
+              alertMessageDto.getApplicationCode(), messageData, hit, idx));
+        }
+
       } else {
         skippedHits.add(hit.getMatchId(idx));
       }
@@ -87,7 +94,43 @@ public class AlertParserService implements ExtractAlertEtlResponseUseCase {
         .build();
   }
 
-  private HitData createHitData(
+  @Override
+  public Map<String, HitAndWatchlistPartyData> getWatchlistDataForMatch(
+      AlertMessageDto alertMessageDto) {
+
+    var hitData = new HashMap<String, HitAndWatchlistPartyData>();
+    var skippedHits = new ArrayList<String>();
+    var messageData = getMessageData(alertMessageDto);
+
+    var alertMessageDtoHits = alertMessageDto.getHits();
+    // XXX(ahaczewski): WATCH OUT! RegisterAlertEndpoint#getMatchIds() assumes the same iteration
+    //  order!!! Make sure you keep it in sync, until shit gets cleaned!!!
+    for (int idx = 0, hitCount = alertMessageDtoHits.size(); idx < hitCount; idx++) {
+      var hit = alertMessageDtoHits.get(idx).getHit();
+      if (hit.isBlocking()) {
+        hitData.put(hit.getMatchId(idx), extractHitAndWatchlistPartyData(
+            new TransactionMessageImpl(messageData),
+            hit));
+      } else {
+        skippedHits.add(hit.getMatchId(idx));
+      }
+    }
+
+    if (!skippedHits.isEmpty()) {
+      log.info("Skipping non-blocking hits: systemId={}, hits={}",
+          alertMessageDto.getSystemID(), skippedHits);
+    }
+
+    return hitData;
+  }
+
+  private MessageData getMessageData(AlertMessageDto alertMessageDto) {
+    return messageParserUseCase.parse(
+        MessageFormat.valueOf(alertMessageDto.getMessageFormat()),
+        alertMessageDto.getMessageData());
+  }
+
+  private static HitData createHitData(
       String applicationCode, MessageData messageData, HitDto hit, int index) {
 
     var alertedPartyData =
@@ -142,7 +185,7 @@ public class AlertParserService implements ExtractAlertEtlResponseUseCase {
     }
   }
 
-  private HitAndWatchlistPartyData extractHitAndWatchlistPartyData(
+  private static HitAndWatchlistPartyData extractHitAndWatchlistPartyData(
       TransactionMessage transactionMessage, HitDto hit) {
 
     var hittedEntity = hit.getHittedEntity();
