c7fa69521f55f662b7f69882b906d0552d4486c7
Stefan Wrocławski <swroclawski@silenteight.com>
2022-02-01 09:34:21 +0000

    INTERNAL: Conditional Bean for Monitoring
    Reviewed-by: Bartosz Martofel <bmartofel@silenteight.com>
    Reviewed-by: Jarosław Kątnik <jkatnik@silenteight.com>
    Merged from: https://gitlab.silenteight.com/ro/agent-commons/-/merge_requests/59

--- modules/agent-commons/agent-monitoring/src/main/java/com/silenteight/agent/monitoring/MonitoringConfiguration.java
+++ modules/agent-commons/agent-monitoring/src/main/java/com/silenteight/agent/monitoring/MonitoringConfiguration.java
@@ -2,6 +2,7 @@ package com.silenteight.agent.monitoring;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 class MonitoringConfiguration {
 
   @Bean
+  @ConditionalOnProperty("sentry.enabled")
   Monitoring monitoring() {
     return new SentryMonitoring();
   }
