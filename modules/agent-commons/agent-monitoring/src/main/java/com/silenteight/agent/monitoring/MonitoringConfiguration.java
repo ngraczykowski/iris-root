080d5e6e1fff8c75e4838922555d2de162cc1542
pzajko <pzajko@silenteight.com>
2022-02-01 13:07:37 +0100

    INTERNAL: Dummy monitoring for sentry

--- modules/agent-commons/agent-monitoring/src/main/java/com/silenteight/agent/monitoring/MonitoringConfiguration.java
+++ modules/agent-commons/agent-monitoring/src/main/java/com/silenteight/agent/monitoring/MonitoringConfiguration.java
@@ -1,6 +1,7 @@
 package com.silenteight.agent.monitoring;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -8,11 +9,22 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
+@Slf4j
 class MonitoringConfiguration {
 
   @Bean
-  @ConditionalOnProperty("sentry.enabled")
+  @ConditionalOnProperty(
+      value = "sentry.enabled",
+      havingValue = "true")
   Monitoring monitoring() {
     return new SentryMonitoring();
+  }
+
+  @Bean
+  @ConditionalOnProperty(
+      value = "sentry.enabled",
+      havingValue = "false")
+  Monitoring dummyMonitoring() {
+    return t -> log.info("Dummy monitoring for local environment");
   }
 }
