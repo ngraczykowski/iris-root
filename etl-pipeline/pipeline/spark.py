b65ba870ec8669241f0743b59650b62f22f4c9c6
Artur Tanona <atanona@silenteight.com>
2022-02-07 13:24:34 +0100

    Remove pipeline

--- etl-pipeline/pipeline/spark.py
+++
@@ -1,5 +0,0 @@
-from spark_manager.spark_client import SparkClient
-from spark_manager.spark_config import SPARK_CONF
-from spark_manager.spark_launcher import SparkLauncher
-
-spark_instance = SparkClient(SPARK_CONF)
