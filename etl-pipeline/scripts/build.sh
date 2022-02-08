870b07d4bceff7ca1c64ad8c6355666bbe9c6f67
Artur Tanona <atanona@silenteight.com>
2022-02-08 13:13:13 +0100

    Force rebuild

--- etl-pipeline/scripts/build.sh
+++ etl-pipeline/scripts/build.sh
@@ -2,7 +2,6 @@
 set -eu -o pipefail
 
 echo "Building docker image"
-docker pull docker.repo.silenteight.com/ds-anaconda
 docker-compose -f docker/prod/docker-compose.yaml build
 docker-compose -f docker/test/docker-compose-test.yaml build
 echo "Docker image ready"
