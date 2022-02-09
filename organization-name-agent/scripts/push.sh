5ccf91d716293550b117efca85cd50b1f42ae26c
Kamil Siwecki <ksiwecki@silenteight.com>
2022-02-09 18:03:11 +0000

    Ks/docker image tags

--- organization-name-agent/scripts/push.sh
+++ organization-name-agent/scripts/push.sh
@@ -2,6 +2,12 @@
 set -eu -o pipefail
 
 REMOTE_IMAGE_NAME='docker.repo.silenteight.com/organization-name-agent'
+IMAGE_VERSION=`cat IMAGE_VERSION`
 
 echo "Tagging and pushing image for 'latest'"
+docker tag "${REMOTE_IMAGE_NAME}" "${REMOTE_IMAGE_NAME}":latest
 docker push "${REMOTE_IMAGE_NAME}:latest"
+
+echo "Tagging and pushing image for '${IMAGE_VERSION}'"
+docker tag "${REMOTE_IMAGE_NAME}" "${REMOTE_IMAGE_NAME}:${IMAGE_VERSION}"
+docker push "${REMOTE_IMAGE_NAME}:${IMAGE_VERSION}"
