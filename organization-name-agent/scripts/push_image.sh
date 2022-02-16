895d703c8438ff0695d164adb938fe0056301256
ksiwecki <ksiwecki@silenteight.com>
2022-02-16 15:28:10 +0100

    commit id removed from image tag - remains package version only

--- organization-name-agent/scripts/push_image.sh
+++ organization-name-agent/scripts/push_image.sh
@@ -4,13 +4,11 @@ set -eu -o pipefail
 REMOTE_IMAGE_NAME='docker.repo.silenteight.com/organization-name-agent'
 
 package_version=`cat PACKAGE_VERSION`
-commit_id=$(git rev-parse --short HEAD)
-image_tag="${package_version}-${commit_id}"
 
 echo "Tagging and pushing image for 'latest'"
 docker tag "${REMOTE_IMAGE_NAME}" "${REMOTE_IMAGE_NAME}:latest"
 docker push "${REMOTE_IMAGE_NAME}:latest"
 
-echo "Tagging and pushing image for $image_tag"
-docker tag "${REMOTE_IMAGE_NAME}" "${REMOTE_IMAGE_NAME}:${image_tag}"
-docker push "${REMOTE_IMAGE_NAME}:${image_tag}"
+echo "Tagging and pushing image for $package_version"
+docker tag "${REMOTE_IMAGE_NAME}" "${REMOTE_IMAGE_NAME}:${package_version}"
+docker push "${REMOTE_IMAGE_NAME}:${package_version}"
