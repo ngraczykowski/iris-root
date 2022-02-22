893c258258df84362c0faa27cac178cbec131479
Jacek Gajewski <jgajewski@silenteight.com>
2022-02-22 12:01:26 +0000

    docs: comment templates for contextual agent added (AEP2-725)
    Reviewed-by: Wiktor Kęska <wkeska@silenteight.com>
    Reviewed-by: Tomasz Bożek <tbozek@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/adjudication-engine/-/merge_requests/193

---
+++ modules/adjudication-engine/adjudication-engine-comments/src/main/resources/db/changelog/sql/create_template_contextual_agent_fp.sql
@@ -0,0 +1,30 @@
+INSERT INTO ae_comment_template (template_name, created_at, revision, template) VALUES ('contextual-agent-fp.ftl', now(), 1, '<#import "match-utils.ftl" as matchUtils>
+<#import "agent-utils.ftl" as agentUtils>
+
+<#assign apAttribute="matching text">
+
+<#assign mappings={
+"FALSE_POSITIVE": "is a FALSE POSITIVE",
+"NOT_FALSE_POSITIVE": "is not a FALSE POSITIVE"
+}>
+
+<#function isSolutionConsistent alertModel matchModel featureModel>
+    <#if matchModel.solution?matches(''.*FALSE_POSITIVE'') || [''FALSE_POSITIVE'']?seq_contains(featureModel.solution)>
+        <#return true>
+    </#if>
+
+    <#return false>
+</#function>
+
+<#function shouldAdd alertModel matchModel featureModel>
+    <#if featureModel.solution?matches(''.*ERROR'')>
+        <#return false>
+    </#if>
+    <#return isSolutionConsistent(alertModel, matchModel, featureModel)>
+</#function>
+
+<#function comment alertModel matchModel featureModel>
+    <#assign solution = featureModel.solution>
+    <#return "Contextual Learning False Positive agent is a ${solution}.">
+</#function>
+');
