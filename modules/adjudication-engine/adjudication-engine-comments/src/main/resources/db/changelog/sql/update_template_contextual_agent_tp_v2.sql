56e23e34b45eaacaf0884a07f6940915e7e23db8
Jacek Gajewski <jgajewski@silenteight.com>
2022-02-23 13:45:24 +0000

    feat(comments): update hist and context agents comment templates
    Comments should have an info about a count of FP and TP
    
    Reviewed-by: Marek Sitarski <msitarski@silenteight.com>
    Reviewed-by: Tomasz Bożek <tbozek@silenteight.com>
    Reviewed-by: Paweł Pietrucha <ppietrucha@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/adjudication-engine/-/merge_requests/197

---
+++ modules/adjudication-engine/adjudication-engine-comments/src/main/resources/db/changelog/sql/update_template_contextual_agent_tp_v2.sql
@@ -0,0 +1,33 @@
+INSERT INTO ae_comment_template (template_name, created_at, revision, template) VALUES ('contextual-agent-tp.ftl', now(), 2, '<#import "match-utils.ftl" as matchUtils>
+<#import "agent-utils.ftl" as agentUtils>
+
+<#assign apAttribute="matching text">
+
+<#assign mappings={
+"TRUE_POSITIVE": "is a TRUE POSITIVE",
+"NOT_TRUE_POSITIVE": "is not a TRUE POSITIVE"
+}>
+
+<#function isSolutionConsistent alertModel matchModel featureModel>
+    <#if matchModel.solution?matches(''.*TRUE_POSITIVE'') || [''TRUE_POSITIVE'']?seq_contains(featureModel.solution)>
+        <#return true>
+    </#if>
+
+    <#return false>
+</#function>
+
+<#function shouldAdd alertModel matchModel featureModel>
+    <#if featureModel.solution?matches(''.*ERROR'')>
+        <#return false>
+    </#if>
+    <#return isSolutionConsistent(alertModel, matchModel, featureModel)>
+</#function>
+
+<#function comment alertModel matchModel featureModel>
+    <#assign solution = featureModel.solution>
+    <#assign falsePositiveCount = featureModel.reason.false_positive>
+    <#assign truePositiveCount = featureModel.reason.true_positive>
+    <#return "Contextual Learning False Positive agent is a ${solution} [FP count(${falsePositiveCount}), TP cont(${truePositiveCount})].">
+</#function>
+
+')
