d29dcd394fff1c575757eacfba41ad9b4191f4a2
snieguu <snieguu@gmail.com>
2022-03-02 16:11:57 +0100

    INTERNAL: base project configuration (FSC-37)

---
+++ modules/fab-data-prep/dataprep-app/src/main/java/com/silenteight/fab/dataprep/FabDataPreparationApplication.java
@@ -0,0 +1,14 @@
+package com.silenteight.fab.dataprep;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabDataPreparationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FabDataPreparationApplication.class, args);
+  }
+
+}
