2d8f1558a24a9276ac7a7c81f7b28b096268fd27
Damian Śnieżek <dsniezek@silenteight.com>
2022-03-08 12:47:24 +0000

    INTERNAL: GenderFeature - use alertedPartGanders from alert (FSC-20)
    Reviewed-by: Wojciech Wątroba <wwatroba@silenteight.com>
    Merged from: https://gitlab.silenteight.com/fab/fab-data-prep/-/merge_requests/17

--- modules/fab-data-prep/dataprep-app/src/main/java/com/silenteight/fab/dataprep/domain/feature/GenderFeature.java
+++ modules/fab-data-prep/dataprep-app/src/main/java/com/silenteight/fab/dataprep/domain/feature/GenderFeature.java
@@ -2,6 +2,7 @@ package com.silenteight.fab.dataprep.domain.feature;
 
 import lombok.extern.slf4j.Slf4j;
 
+import com.silenteight.fab.dataprep.domain.model.ExtractedAlert;
 import com.silenteight.fab.dataprep.domain.model.ExtractedAlert.Match;
 import com.silenteight.universaldatasource.api.library.Feature;
 import com.silenteight.universaldatasource.api.library.agentinput.v1.AgentInputIn;
@@ -9,6 +10,7 @@ import com.silenteight.universaldatasource.api.library.gender.v1.GenderFeatureIn
 
 import java.util.List;
 
+import static java.util.List.of;
 import static java.util.stream.Collectors.toList;
 
 @Slf4j
@@ -18,19 +20,25 @@ public class GenderFeature implements FabFeature {
 
   @Override
   public List<AgentInputIn<Feature>> createFeatureInput(FeatureInputsCommand featureInputsCommand) {
+    ExtractedAlert extractedAlert = featureInputsCommand.getExtractedAlert();
     return featureInputsCommand.getExtractedAlert().getMatches()
         .stream()
         .map(Match::getMatchName)
         .map(matchName ->
             AgentInputIn.builder()
                 .match(matchName)
-                .alert(featureInputsCommand.getExtractedAlert().getAlertName())
-                .featureInputs(List.of(GenderFeatureInputOut.builder()
+                .alert(extractedAlert.getAlertName())
+                .featureInputs(of(GenderFeatureInputOut.builder()
                     .feature(FEATURE_NAME)
-                    .alertedPartyGenders(List.of("M"))
-                    .watchlistGenders(List.of("F"))
+                    .alertedPartyGenders(getAlertedPartGenders(extractedAlert))
+                    .watchlistGenders(of("F"))
                     .build()))
                 .build())
         .collect(toList());
   }
+
+  private List<String> getAlertedPartGenders(ExtractedAlert extractedAlert) {
+    return of(extractedAlert.getParsedPayload().getGender());
+  }
+
 }
