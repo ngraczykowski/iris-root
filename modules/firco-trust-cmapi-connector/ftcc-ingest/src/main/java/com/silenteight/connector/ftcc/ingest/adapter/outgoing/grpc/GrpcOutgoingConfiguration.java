18b220fd5851bb6309da2530521d51ea3f4a4395
Damian Śnieżek <dsniezek@silenteight.com>
2022-03-15 08:48:02 +0000

    INTERNAL: Mock RegistrationApiClient (FSC-45)
    Reviewed-by: Paweł Pietrucha <ppietrucha@silenteight.com>
    Merged from: https://gitlab.silenteight.com/connectors/firco-trust-cmapi-connector/-/merge_requests/21

--- modules/firco-trust-cmapi-connector/ftcc-ingest/src/main/java/com/silenteight/connector/ftcc/ingest/adapter/outgoing/grpc/GrpcOutgoingConfiguration.java
+++ modules/firco-trust-cmapi-connector/ftcc-ingest/src/main/java/com/silenteight/connector/ftcc/ingest/adapter/outgoing/grpc/GrpcOutgoingConfiguration.java
@@ -1,18 +1,29 @@
 package com.silenteight.connector.ftcc.ingest.adapter.outgoing.grpc;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.silenteight.connector.ftcc.ingest.domain.port.outgoing.RegistrationApiClient;
-import com.silenteight.registration.api.library.v1.RegistrationServiceClient;
+import com.silenteight.registration.api.library.v1.*;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Slf4j
 @Configuration
 class GrpcOutgoingConfiguration {
 
+  @Profile("!mockcorebridge")
   @Bean
   RegistrationApiClient registrationApiClient(
       RegistrationServiceClient registrationServiceClient) {
 
     return new RegistrationGrpcAdapter(registrationServiceClient);
+  }
+
+  @Profile("mockcorebridge")
+  @Bean
+  RegistrationApiClient mockRegistrationApiClient() {
+    return batch -> log.info("Batch registered {}", batch.getBatchId());
   }
 }
