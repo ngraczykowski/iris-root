4855d5043ad50610ea5cd8130cddcfba489d42f8
Tomasz Drożdż <tdrozdz@silenteight.com>
2022-03-21 15:37:08 +0000

    INTERNAL: Adding script which allows migrating simulation from ES to psql and...

--- modules/warehouse/scripts/aws-deploy.sh
+++ modules/warehouse/scripts/aws-deploy.sh
@@ -7,3 +7,6 @@ skopeo copy docker://docker.repo.silenteight.com/warehouse/warehouse-app:latest 
 
 skopeo copy docker://docker.repo.silenteight.com/warehouse/warehouse-es-init:$WAREHOUSE_ES_INIT_VERSION docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/warehouse-es-init:$WAREHOUSE_ES_INIT_VERSION
 skopeo copy docker://docker.repo.silenteight.com/warehouse/warehouse-es-init:latest docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/warehouse-es-init:latest
+
+skopeo copy docker://docker.repo.silenteight.com/warehouse/warehouse-sim-migration:WAREHOUSE_SIM_MIGRATION_VERSION docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/warehouse-sim-migration:WAREHOUSE_SIM_MIGRATION_VERSION
+skopeo copy docker://docker.repo.silenteight.com/warehouse/warehouse-sim-migration:latest docker://278875966478.dkr.ecr.us-east-1.amazonaws.com/warehouse-sim-migration:latest
