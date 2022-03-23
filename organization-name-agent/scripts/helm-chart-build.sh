d66c69b6210d22c77230f7438ac47925f7c11538
ksiwecki <ksiwecki@silenteight.com>
2022-03-23 12:34:41 +0100

    fix in image pushing scripts - VERSION env var

--- organization-name-agent/scripts/helm-chart-build.sh
+++ organization-name-agent/scripts/helm-chart-build.sh
@@ -7,7 +7,7 @@ helm_repo="s8helm"
 
 helm repo add --username "$DEPLOY_USERNAME" --password "$DEPLOY_PASSWORD" $helm_repo https://repo.silenteight.com/artifactory/helm
 mkdir -p build/chart/
-helm package --app-version ${env.VERSION} --destination build/chart helm-chart
+helm package --app-version ${VERSION} --destination build/chart helm-chart
 organization_name_agent_artifact=$(basename -- "$(ls "$scriptdir"/build/chart/organization-name-agent-*.tgz)")
 echo "Creating HELM chart: $organization_name_agent_artifact"
 helm push-artifactory --skip-reindex $scriptdir/build/chart/$organization_name_agent_artifact $helm_repo
