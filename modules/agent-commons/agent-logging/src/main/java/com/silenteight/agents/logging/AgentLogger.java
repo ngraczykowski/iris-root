d24e3853c79e64f46c9d04af1666265b27d76238
Stefan Wrocławski <swroclawski@silenteight.com>
2022-03-23 11:46:33 +0000

    INTERNAL: Create a logging lib in agent-commons (SEALS-694)
    Reviewed-by: Sebastian Tomczuk <stomczuk@silenteight.com>
    Reviewed-by: Igor Wnęk <iwnek@silenteight.com>
    Merged from: https://gitlab.silenteight.com/ro/agent-commons/-/merge_requests/76

---
+++ modules/agent-commons/agent-logging/src/main/java/com/silenteight/agents/logging/AgentLogger.java
@@ -0,0 +1,178 @@
+package com.silenteight.agents.logging;
+
+import lombok.experimental.UtilityClass;
+
+import org.slf4j.Logger;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
+
+@UtilityClass
+@SuppressWarnings("unused")
+public class AgentLogger {
+
+  /**
+   * <p>Logs exception and its stacktrace</p>
+   * <p>Intended use: unrecoverable errors</p>
+   */
+  public static void error(Logger log, String message, Throwable error) {
+    log.error(message, error);
+  }
+
+  /**
+   * <p>Logs exception message without printing stacktrace</p>
+   * <p>Intended use: recoverable errors</p>
+   */
+  public static void warn(Logger log, String message, Throwable error) {
+    if (log.isWarnEnabled()) {
+      var causeMessage = defaultIfNull(getRootCauseMessage(error), "unknown cause message");
+      log.warn(message, causeMessage);
+    }
+  }
+
+  /**
+   * <p> Logs warning message</p>
+   * <pre>
+   * Intended use:
+   * - possible invalid state of application,
+   * - unrecognized formats of data
+   * etc
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values that does not need any computing to transform to String value
+   */
+  public static void warn(Logger log, String message, Object... parameters) {
+    if (log.isWarnEnabled()) {
+      log.warn(message, parameters);
+    }
+  }
+
+
+  /**
+   * <p> Logs message</p>
+   * <pre>
+   *  Intended use:
+   *  - possible invalid state of application,
+   *  - unrecognized formats of data
+   *  etc
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values coming from computational expensive methods (toString() etc)
+   */
+  @SafeVarargs
+  public static void warn(Logger log, String message, Supplier<Object>... parameters) {
+    if (log.isWarnEnabled()) {
+      log.warn(message, unpackSuppliers(parameters));
+    }
+  }
+
+  /**
+   * <p>Logs info message</p>
+   * <pre>Intended use:
+   * - Purely informative information, e.g. processing steps and results
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values that does not need any computing to transform to String value
+   */
+  public static void info(Logger log, String message, Object... parameters) {
+    if (log.isInfoEnabled()) {
+      log.info(message, parameters);
+    }
+  }
+
+  /**
+   * <p>Logs info message</p>
+   * <pre>Intended use:
+   * - Purely informative information, e.g. processing steps and results
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values coming from computational expensive methods (toString() etc)
+   */
+  @SafeVarargs
+  public static void info(Logger log, String message, Supplier<Object>... parameters) {
+    if (log.isInfoEnabled()) {
+      log.info(message, unpackSuppliers(parameters));
+    }
+  }
+
+
+  /**
+   * <p>Logs debug message</p>
+   * <pre>Intended use:
+   * - Guide developer through the processing path.
+   * - Log most important branching points or selection of algorithms
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values that does not need any computing to transform to String value
+   */
+  public static void debug(Logger log, String message, Object... parameters) {
+    if (log.isDebugEnabled()) {
+      log.debug(message, parameters);
+    }
+  }
+
+  /**
+   * <p>Logs debug message</p>
+   * <pre>Intended use:
+   * - Guide developer through the processing path.
+   * - Log most important branching points or selection of algorithms
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values coming from computational expensive methods (toString() etc)
+   */
+  @SafeVarargs
+  public static void debug(Logger log, String message, Supplier<Object>... parameters) {
+    if (log.isDebugEnabled()) {
+      log.debug(message, unpackSuppliers(parameters));
+    }
+  }
+
+  /**
+   * <p>Logs trace message</p>
+   * <pre>Intended use:
+   * - Log input and output of public methods
+   * - Any information that might be relevant to understanding flow of the processing
+   * that were not covered by DEBUG level
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values that does not need any computing to transform to String value
+   */
+  public static void trace(Logger log, String message, Object... parameters) {
+    if (log.isTraceEnabled()) {
+      log.trace(message, parameters);
+    }
+  }
+
+  /**
+   * <p>Logs trace message</p>
+   * <pre>Intended use:
+   * - Log input and output of public methods
+   * - Any information that might be relevant to understanding flow of the processing
+   * that were not covered by DEBUG level
+   * </pre>
+   *
+   * @param parameters
+   *     Parameters values coming from computational expensive methods (toString() etc)
+   */
+  @SafeVarargs
+  public static void trace(Logger log, String message, Supplier<Object>... parameters) {
+    if (log.isTraceEnabled()) {
+      log.trace(message, unpackSuppliers(parameters));
+    }
+  }
+
+  @SafeVarargs
+  private static Object[] unpackSuppliers(Supplier<Object>... parameters) {
+    return Stream.of(parameters).map(Supplier::get).toArray();
+  }
+}
