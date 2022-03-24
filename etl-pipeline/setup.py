02f743318e75b6ade6101704f1d8af99e076b1df
Jenkins <jenkins@silenteight.com>
2022-03-24 20:57:27 +0000

    Bump version: 0.5.8-dev → 0.5.9-dev

--- etl-pipeline/setup.py
+++ etl-pipeline/setup.py
@@ -26,7 +26,7 @@ tests_require = [
 
 setup(
     name="etl_pipeline",
-    version="0.5.8-dev",
+    version="0.5.9-dev",
     description="ETL pipeline",
     author="Silent Eight Pte. Ltd.",
     author_email="support@silenteight.com",
