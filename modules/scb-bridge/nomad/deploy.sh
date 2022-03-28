30489a06b52844adfb1300fead722ca40035d608
Sławomir Korbas <skorbas@silenteight.com>
2022-03-28 09:05:32 +0000

    Minio paths adjustment: ALFA-43
    Reviewed-by: Borys Wiśniewski <bwisniewski@silenteight.com>
    Reviewed-by: Maciej Głowiński <mglowinski@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/scb-bridge/-/merge_requests/49

--- modules/scb-bridge/nomad/deploy.sh
+++ modules/scb-bridge/nomad/deploy.sh
@@ -14,15 +14,15 @@ MINIO_ALIAS=${MINIO_ALIAS:-minio}
 
 export NOMAD_ADDR="${NOMAD_ADDR:-http://localhost:4646}"
 
-scb_bridge_artifact=$(basename -- "$(ls "$scriptdir"/../build/libs/scb-bridge-*-exec.jar)")
+scb_bridge_artifact=$(basename -- "$(ls "$scriptdir"/../bridge-app/build/libs/bridge-app-*-exec.jar)")
 export NOMAD_VAR_scb_bridge_artifact_artifact=${NOMAD_VAR_scb_bridge_artifact:-"s3::${MINIO_ADDR}/artifacts/scb-bridge/${scb_bridge_artifact}"}
-export NOMAD_VAR_scb_bridge_artifact_checksum=${NOMAD_VAR_scb_bridge_artifact_checksum:-"sha256:$(sha256sum "$scriptdir"/../build/libs/"$scb_bridge_artifact" | awk '{print $1}')"}
+export NOMAD_VAR_scb_bridge_artifact_checksum=${NOMAD_VAR_scb_bridge_artifact_checksum:-"sha256:$(sha256sum "$scriptdir"/../bridge-app/build/libs/"$scb_bridge_artifact" | awk '{print $1}')"}
 
 cd "$scriptdir"
 
 set -x
 
 mkdir -p artifacts/
-cp ../build/libs/"$scb_bridge_artifact"  artifacts/
+cp ../bridge-app/build/libs/"$scb_bridge_artifact"  artifacts/
 mcli cp --recursive artifacts/ "$MINIO_ALIAS"/artifacts/scb-bridge
 nomad job run "$@" scb-bridge.nomad
