f708b1b702180ec26ea7c2ea3835e5cbcde94c3b
tbozek <tbozek@silenteight.com>
2022-03-28 13:36:02 +0200

    Revert "disable"
    This reverts commit 37b7614f8c21091322daa22f68628c55365b714e.

--- modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/recommendation/integration/CommentInputsUpdatedRecommendationIntegrationFlow.java
+++ modules/adjudication-engine/adjudication-engine-analysis/src/main/java/com/silenteight/adjudication/engine/analysis/recommendation/integration/CommentInputsUpdatedRecommendationIntegrationFlow.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import com.silenteight.adjudication.engine.analysis.recommendation.RecommendationFacade;
 import com.silenteight.adjudication.internal.v1.CommentInputsUpdated;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.integration.dsl.IntegrationFlowAdapter;
 import org.springframework.integration.dsl.IntegrationFlowDefinition;
 import org.springframework.integration.handler.LoggingHandler.Level;
@@ -13,10 +12,6 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-@ConditionalOnProperty(
-    value = "ae.recommendation.comment-input-updated.enabled",
-    havingValue = "true"
-)
 class CommentInputsUpdatedRecommendationIntegrationFlow extends IntegrationFlowAdapter {
 
   private final RecommendationFacade facade;
