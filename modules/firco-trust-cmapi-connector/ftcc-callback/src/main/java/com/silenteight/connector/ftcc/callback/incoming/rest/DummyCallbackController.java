1bbd1ea330f8a1ee74322f03783b5ebdc1dce2c3
Kamil Dzięcioł <kdzieciol+gitlab@silenteight.com>
2022-04-01 14:38:15 +0000

    INTERNAL: Callback pretty print
    Reviewed-by: Paweł Pietrucha <ppietrucha@silenteight.com>
    Merged from: https://gitlab.silenteight.com/connectors/firco-trust-cmapi-connector/-/merge_requests/74

--- modules/firco-trust-cmapi-connector/ftcc-callback/src/main/java/com/silenteight/connector/ftcc/callback/incoming/rest/DummyCallbackController.java
+++ modules/firco-trust-cmapi-connector/ftcc-callback/src/main/java/com/silenteight/connector/ftcc/callback/incoming/rest/DummyCallbackController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.connector.ftcc.common.dto.output.AckDto;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +18,8 @@ public class DummyCallbackController {
 
   @PostMapping("/callback")
   public ResponseEntity<AckDto> dummyCallbackHandler(
-      @RequestBody String clientRequestDto) {
-    log.info("Request:\n{}", clientRequestDto);
+      @RequestBody JsonNode clientRequestDto) {
+    log.info("Request:\n{}", clientRequestDto.toPrettyString());
     return ResponseEntity.ok(AckDto.ok());
   }
 }
