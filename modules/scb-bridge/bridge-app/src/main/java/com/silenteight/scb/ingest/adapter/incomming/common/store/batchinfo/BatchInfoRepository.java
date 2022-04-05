9e33f4a873292489926810ac63f95ecc18d4dde8
Maciej Głowiński <mglowinski@silenteight.com>
2022-04-05 13:37:10 +0000

    INTERNAL: use primary transaction manager on BatchInfoRepo (ALFA-174)

--- modules/scb-bridge/bridge-app/src/main/java/com/silenteight/scb/ingest/adapter/incomming/common/store/batchinfo/BatchInfoRepository.java
+++ modules/scb-bridge/bridge-app/src/main/java/com/silenteight/scb/ingest/adapter/incomming/common/store/batchinfo/BatchInfoRepository.java
@@ -1,15 +1,18 @@
 package com.silenteight.scb.ingest.adapter.incomming.common.store.batchinfo;
 
+import com.silenteight.scb.ingest.adapter.incomming.common.domain.GnsSyncConstants;
 import com.silenteight.scb.ingest.domain.model.BatchStatus;
 
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BatchInfoRepository extends CrudRepository<BatchInfo, Long> {
 
   @Modifying
+  @Transactional(GnsSyncConstants.PRIMARY_TRANSACTION_MANAGER)
   @Query("UPDATE BatchInfo SET batchStatus = :status "
       + "WHERE internalBatchId = :internalBatchId")
   void update(@Param("internalBatchId") String internalBatchId,
