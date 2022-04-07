87aeb37c97d13738bc4d7c7bc696a9624ab8139a
Witold Zegarowski <wzegarowski@silenteight.com>
2022-04-07 13:17:19 +0000

    INTERNAL: Fix: @Transactional on services should work now
    Reviewed-by: Maciej Głowiński <mglowinski@silenteight.com>
    Reviewed-by: Szymon Mrozowski <smrozowski@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/scb-bridge/-/merge_requests/96

--- modules/scb-bridge/bridge-app/src/main/java/com/silenteight/scb/ScbBridgeApplication.java
+++ modules/scb-bridge/bridge-app/src/main/java/com/silenteight/scb/ScbBridgeApplication.java
@@ -3,9 +3,11 @@ package com.silenteight.scb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAsync
 @SpringBootApplication
+@EnableTransactionManagement
 public class ScbBridgeApplication {
 
   public static void main(String[] args) {
