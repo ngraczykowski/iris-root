c80e92bc2c36389b2d089022c0804af711d2a4d0
mmastylo <mmastylo@silenteight.com>
2022-04-07 12:42:08 +0200

    INTERNAL: Fix configuration

--- modules/firco-trust-cmapi-connector/ftcc-ingest/src/main/java/com/silenteight/connector/ftcc/ingest/adapter/outgoing/amqp/DataPrepIntegrationFlow.java
+++ modules/firco-trust-cmapi-connector/ftcc-ingest/src/main/java/com/silenteight/connector/ftcc/ingest/adapter/outgoing/amqp/DataPrepIntegrationFlow.java
@@ -1,10 +1,13 @@
 package com.silenteight.connector.ftcc.ingest.adapter.outgoing.amqp;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.silenteight.connector.ftcc.ingest.domain.port.outgoing.AlertMessage;
 
 import org.springframework.integration.dsl.IntegrationFlowAdapter;
 import org.springframework.integration.dsl.IntegrationFlowDefinition;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 
 import static com.silenteight.connector.ftcc.ingest.adapter.outgoing.amqp.DataPrepChannels.DATA_PREP_GATEWAY_CHANNEL;
@@ -12,6 +15,7 @@ import static com.silenteight.connector.ftcc.ingest.adapter.outgoing.amqp.DataPr
 import static java.lang.Boolean.TRUE;
 
 @Component
+@Slf4j
 class DataPrepIntegrationFlow extends IntegrationFlowAdapter {
 
   @Override
@@ -19,9 +23,18 @@ class DataPrepIntegrationFlow extends IntegrationFlowAdapter {
     return from(DATA_PREP_GATEWAY_CHANNEL)
         .enrichHeaders(enricher -> enricher
             .priorityFunction(DataPrepIntegrationFlow::getPriority, TRUE))
-        .handle(AlertMessage.class)
+        .handle(AlertMessage.class, DataPrepIntegrationFlow::logMessage)
         .transform(AlertMessage::toAlertMessageStored)
         .channel(DATA_PREP_OUTBOUND_CHANNEL);
+  }
+
+  private static AlertMessage logMessage(AlertMessage message, MessageHeaders headers) {
+    if (log.isDebugEnabled()) {
+      log.debug("Sending alert message: batchName={}, messageName={}, state={}",
+          message.getBatchName(), message.getMessageName(), message.getState());
+    }
+
+    return message;
   }
 
   private static Integer getPriority(Message<AlertMessage> message) {
