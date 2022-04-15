40720c53dcc80d703c16cb947581021bed234a39
Kamil Dzięcioł <kdzieciol@silenteight.com>
2022-04-15 11:52:15 +0200

    INTERNAL: Extend logging to info of the received message.

--- modules/firco-trust-cmapi-connector/ftcc-ingest/src/main/java/com/silenteight/connector/ftcc/ingest/adapter/incoming/rest/IngestRestController.java
+++ modules/firco-trust-cmapi-connector/ftcc-ingest/src/main/java/com/silenteight/connector/ftcc/ingest/adapter/incoming/rest/IngestRestController.java
@@ -24,7 +24,7 @@ class IngestRestController {
   @PostMapping("/v1/alert")
   public ResponseEntity<AckDto> alert(@RequestBody RequestDto request) {
     log.info("Alert received");
-    log.debug("Received request body:\n{}", request);
+    log.info("Received request body:\n{}", request);
     ingestFacade.ingest(request);
     return ResponseEntity.ok(AckDto.ok());
   }
