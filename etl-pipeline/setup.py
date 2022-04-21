e7a1e769fbcfd5c2beeabc71d13adaec790cfdb9
Jenkins <jenkins@silenteight.com>
2022-04-21 09:28:39 +0000

    Bump version: 0.5.22-dev → 0.5.23-dev

--- etl-pipeline/setup.py
+++ etl-pipeline/setup.py
@@ -16,7 +16,7 @@ tests_require = ["pandas==1.1.5"]
 
 setup(
     name="etl_pipeline",
-    version="0.5.22-dev",
+    version="0.5.23-dev",
     description="ETL pipeline",
     author="Silent Eight Pte. Ltd.",
     author_email="support@silenteight.com",
