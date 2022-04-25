ef60103874176d9ba1d2144da0891b8363ccc10c
Andrzej Haczewski <ahaczewski@silenteight.com>
2022-04-25 11:15:44 +0200

    fix: remove old aspectj plugin

--- scripts/clean-all.sh
+++ scripts/clean-all.sh
@@ -2,4 +2,4 @@
 set -x -o pipefail
 scriptdir="$(cd -- "$(dirname -- "${0}")" && pwd -P)"
 
-find "$scriptdir"/.. -type d -name '.gradle' -exec rm -rf {} +
+find "$scriptdir"/.. -type d \( -name '.gradle' -o -name 'build' \) -exec rm -rf {} +
