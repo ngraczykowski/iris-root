c41289c34ca420d12446d4569223077b930bbeca
Andrzej Haczewski <ahaczewski@silenteight.com>
2022-04-26 15:58:31 +0200

    ci: fix publishing

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -2,10 +2,13 @@
 set -e -o pipefail
 
 rev=${GIT_COMMIT:-origin/master}
+branch=${GIT_BRANCH:-$(git branch --no-color --show-current)}
 day=$(git show --no-patch --date=format:'%y%m%d' --pretty=format:'%cd' "$rev")
-last_commit_timestamp=$(git show --no-patch --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%cd' "$rev")
-commit=$(git rev-list --count origin/master --since="$last_commit_timestamp")
-build=${BUILD_NUMBER:-0}
-sha=$(git rev-parse --short "$rev")
+commit_start=$(git show --no-patch --date=format:'%Y-%m-%d 00:00:00' --pretty=format:'%cd' "$rev")
+commit=$(git rev-list --count --since="$commit_start" "$rev")
 
-echo "$day.$commit.$build+$sha"
+if [[ "$branch" =~ .*"master" ]]; then
+  echo "$day.$commit.0"
+else
+  echo "HEAD-SNAPSHOT"
+fi
