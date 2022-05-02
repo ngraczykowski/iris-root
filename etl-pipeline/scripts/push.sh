a1fb02ed6569391e06c82b9bdec8b52758c4135b
Artur Tanona <atanona@silenteight.com>
2022-05-02 09:43:27 +0200

    Remove pull mechanism

--- etl-pipeline/scripts/push.sh
+++ etl-pipeline/scripts/push.sh
@@ -10,13 +10,6 @@ VERSION=${VERSION%,}
 
 echo $VERSION
 
-REMOTE_IMAGE_NAME="docker.repo.silenteight.com/etl-pipeline"
-
-echo "Pushing image for " $REMOTE_IMAGE_NAME
-docker tag "${REMOTE_IMAGE_NAME}:latest" "${REMOTE_IMAGE_NAME}:$VERSION"
-docker push "${REMOTE_IMAGE_NAME}:$VERSION"
-docker push "${REMOTE_IMAGE_NAME}:latest"
-
 REMOTE_IMAGE_NAME="docker.repo.silenteight.com/etl-pipeline-service"
 
 echo "Pushing image for " $REMOTE_IMAGE_NAME:$VERSION
