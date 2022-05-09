6e883ea5dc00dfed180151a2f70b31deeea224e4
Artur Tanona <atanona@silenteight.com>
2022-05-09 19:35:05 +0200

    Change dataset logic

--- etl-pipeline/pipelines/ms/ms_pipeline.py
+++ etl-pipeline/pipelines/ms/ms_pipeline.py
@@ -1,6 +1,7 @@
 import logging
 from copy import deepcopy
 
+from etl_pipeline import application
 from etl_pipeline.config import load_agent_configs, pipeline_config
 from etl_pipeline.custom.ms.datatypes.field import InputRecordField
 from etl_pipeline.custom.ms.payload_loader import PayloadLoader
@@ -13,114 +14,15 @@ logger = logging.getLogger("main").getChild("etl_pipeline")
 cn = pipeline_config.cn
 
 
-CUSTOMER_S8_MAP = {
-    "R_Global_MultiParty_Wkly": "ISG_PARTY",
-    "R_Global_MultiParty_Daily": "ISG_PARTY",
-    "R_US_Active_Address": "WM_ADDRESS",
-    "R_US_Active_Party": "WM_PARTY",
-    "R_Global_MultiAccounts_Daily": "ISG_ACCOUNT",
-    "R_Global_MultiAccounts_Wkly": "ISG_ACCOUNT",
-}
-
-
 class PipelineError:
     pass
-
-
-def create_agent_input_agg_col_config(agent_input_prepended_agent_name_config):
-    """Create the source and target columns based on the standardized agent input config.
-
-    Input:
-    { 'name_agent': {'name_agent_ap': ['record_name', 'whatever_other_name'],
-                     'name_agent_ap_aliases': [],
-                     'name_agent_wl': ['name_hit'],
-                     'name_agent_wl_aliases': []
-                    }
-    }
-
-    Output:
-    {'name_agent': {'ap_all_names_aggregated': ['name_agent_ap', 'name_agent_ap_aliases'],
-                    'wl_all_names_aggregated': ['name_agent_wl', 'name_agent_wl_aliases']
-                   }
-    }
-    """
-
-    def _generate_simple_plural(word):
-        if word.lower().endswith("s"):
-            return word.lower() + "es"
-        elif word.lower().endswith("y") and word.lower()[-2:] not in [
-            "ay",
-            "ey",
-            "iy",
-            "oy",
-            "uy",
-        ]:
-            return word.lower()[:-1] + "ies"
-        else:
-            return word.lower() + "s"
-
-    def _get_ap_or_wl_agg_source_cols(level_1_value, party):
-        source_cols = []
-        for col in level_1_value.keys():
-            if (
-                col.endswith(f"_{party}")
-                or col.endswith(f"_{party}_aliases")
-                or col.startswith("hit")
-            ):
-                source_cols.append(col)
-
-        return source_cols
-
-    agent_input_agg_col_config = {}
-
-    for agent_name, config in agent_input_prepended_agent_name_config.items():
-        agent_type = agent_name.split("_agent", 1)[0]
-        agent_ap_agg_col = f"""ap_all_{_generate_simple_plural(agent_type)}_aggregated"""
-        agent_wl_agg_col = f"""wl_all_{_generate_simple_plural(agent_type)}_aggregated"""
-        agent_ap_agg_source_cols = _get_ap_or_wl_agg_source_cols(config, "ap")
-        agent_wl_agg_source_cols = _get_ap_or_wl_agg_source_cols(config, "wl")
-
-        agent_input_agg_col_config[agent_name] = {}
-        agent_input_agg_col_config[agent_name][agent_ap_agg_col] = agent_ap_agg_source_cols
-        agent_input_agg_col_config[agent_name][agent_wl_agg_col] = agent_wl_agg_source_cols
-
-    return agent_input_agg_col_config
-
-
-def prepend_agent_name_to_ap_or_wl_or_aliases_key(agent_input_config):
-    """Prepend the agent name (level 1 key) to level 2 key. So the new level 2 key will be
-
-    Input:
-    { 'name_agent': {'ap': ['record_name'],
-                    'ap_aliases': [],
-                    'wl': ['name_hit'],
-                    'wl_aliases': []
-                    }
-    }
-
-    Output:
-    { 'name_agent': {'name_agent_ap': ['record_name'],
-                    'name_agent_ap_aliases': [],
-                    'name_agent_wl': ['name_hit'],
-                    'name_agent_wl_aliases': []
-                    }
-    }
-    """
-    result = {}
-    for agent_name, config in agent_input_config.items():
-        result[agent_name] = {}
-
-        for ap_or_wl_or_aliases, source_cols in config.items():
-            prepended_key_name = "_".join([agent_name, ap_or_wl_or_aliases])
-            result[agent_name][prepended_key_name] = source_cols
-
-    return result
 
 
 class MSPipeline(ETLPipeline):
     def __init__(self, engine, config=None):
         super().__init__(engine, config.config)
         self.alert_agents_config = load_agent_configs()
+        self.dataset_config = config.dataset_config
 
     def reload_config(self):
         self.alert_agents_config = load_agent_configs()
@@ -291,10 +193,10 @@ class MSPipeline(ETLPipeline):
             matches = payload[cn.WATCHLIST_PARTY][cn.MATCH_RECORDS]
             agent_config, yaml_conf = self.alert_agents_config["alert_type"]
             agent_input_prepended_agent_name_config = (
-                prepend_agent_name_to_ap_or_wl_or_aliases_key(agent_config)
+                application.prepend_agent_name_to_ap_or_wl_or_aliases_key(agent_config)
             )
 
-            agent_input_agg_col_config = create_agent_input_agg_col_config(
+            agent_input_agg_col_config = application.create_agent_input_agg_col_config(
                 agent_input_prepended_agent_name_config
             )
 
@@ -322,7 +224,7 @@ class MSPipeline(ETLPipeline):
                 ]
                 fields = input_records[0][cn.INPUT_FIELD]
                 self.handle_hit_type_agent(match, agent_config, fields)
-                self.select_ap_for_ap_id_tp_marked_agent(payload, match)
+                self.select_ap_for_ap_id_tp_marked_agent(match)
                 self.set_up_entity_type_match(match)
 
                 self.engine.sql_to_merge_specific_columns_to_standardized(
@@ -383,16 +285,16 @@ class MSPipeline(ETLPipeline):
         existing_fields = [field for field in requested_json_keys if fields.get(field, None)]
         return existing_fields
 
-    def select_ap_for_ap_id_tp_marked_agent(self, payload, match):
-        dataset_name = payload[cn.ALERTED_PARTY_FIELD][cn.HEADER_INFO][cn.DATASET_NAME]
-        if dataset_name in ["R_Global_MultiParty_Daily", "R_Global_MultiParty_Wkly"]:
+    def select_ap_for_ap_id_tp_marked_agent(self, match):
+        dataset_name = match[cn.DATASET_TYPE]
+        if dataset_name == "ISG_PARTY":
             match["ap_id_tp_marked_agent_input"] = match["ap_id_tp_marked_agent_input"][0]
-        elif dataset_name in ["R_US_Active_Address", "R_US_Active_Party"]:
+        elif dataset_name in ["WM_ADDRESS", "WM_PARTY"]:
             match["ap_id_tp_marked_agent_input"] = match["ap_id_tp_marked_agent_input"][1]
-        elif dataset_name in ["R_Global_MultiAccounts_Wkly", "R_Global_MultiAccounts_Daily"]:
+        elif dataset_name == "ISG_ACCOUNT":
             match["ap_id_tp_marked_agent_input"] = match["ap_id_tp_marked_agent_input"][2]
-        if not match["ap_id_tp_marked_agent_input"]:
-            logger.warning("No input for ap_id_tp_marked_agent")
+        else:
+            logger.warning("No input for ap_id_tp_marked_agent. Setting up default")
             match["ap_id_tp_marked_agent_input"] = ""
 
     def set_up_party_type(self, payload, match):
@@ -411,6 +313,6 @@ class MSPipeline(ETLPipeline):
             match["ENTITY_TYPE_MATCH"] = "N"
 
     def set_up_dataset_type_match(self, payload, match):
-        match[cn.DATASET_TYPE] = CUSTOMER_S8_MAP.get(
-            payload[cn.ALERTED_PARTY_FIELD][cn.HEADER_INFO][cn.DATASET_NAME], ""
+        match[cn.DATASET_TYPE] = self.dataset_config.get(
+            payload[cn.ALERTED_PARTY_FIELD][cn.HEADER_INFO][cn.DATASET_NAME]
         )
--- etl-pipeline/tests/test_json/test_json_pipeline.py
+++ etl-pipeline/tests/test_json/test_json_pipeline.py
@@ -4,7 +4,7 @@ import pickle
 import pandas as pd
 import pytest
 
-from etl_pipeline.config import pipeline_config
+from etl_pipeline.config import DatasetConfigError, pipeline_config
 from etl_pipeline.custom.ms.datatypes.field import (  # noqa F401; required for unpickling
     InputRecordField,
 )
@@ -16,6 +16,10 @@ cn = pipeline_config.cn
 
 ALERT_INTERNAL_ID = cn.ALERT_INTERNAL_ID
 TEST_PATH = "tests/shared/test_ms_pipeline/"
+
+
+def get_dummy_payload_for_dataset(dataset_name):
+    return {cn.ALERTED_PARTY_FIELD: {cn.HEADER_INFO: {cn.DATASET_NAME: dataset_name}}}
 
 
 def run_pipeline(uut, file_path):
@@ -87,54 +91,172 @@ def check_payload(out_payload, reference_file):
         try:
             pd.testing.assert_series_equal(out_payload[cols], reference_payload[cols])
         except (AssertionError, TypeError):
-            if isinstance(out_payload[cols].values[0], list):
-                reference = remove_nulls_from_aggegated(
-                    flatten([i for i in reference_payload[cols].values])
-                )
-                output = remove_nulls_from_aggegated(
-                    flatten([i for i in out_payload[cols].values])
-                )
-                assert_list(output, reference)
-
-            else:
-                assert output == reference
+            reference = remove_nulls_from_aggegated(
+                flatten([i for i in reference_payload[cols].values])
+            )
+            output = remove_nulls_from_aggegated(flatten([i for i in out_payload[cols].values]))
+            assert_list(output, reference)
 
 
-def assert_length_and_content_match(requested_length, parsed_payloads, reference_file):
+def assert_length_and_content_match(
+    requested_length, parsed_payloads, reference_file, ap_id_tp_marked_agent_input
+):
     assert len(parsed_payloads) == requested_length
     for i in parsed_payloads:
         assert len(i[cn.ALERTED_PARTY_FIELD][cn.INPUT_RECORD_HIST]) == 1
         assert len(i[cn.WATCHLIST_PARTY][cn.MATCH_RECORDS]) == 1
+        assert (
+            i[cn.WATCHLIST_PARTY][cn.MATCH_RECORDS][0]["ap_id_tp_marked_agent_input"]
+            == ap_id_tp_marked_agent_input
+        )
     check_payload(parsed_payloads, reference_file)
 
 
 @pytest.mark.parametrize(
-    ["payload_file", "reference_file", "reference_length"],
+    ["payload_file", "reference_file", "reference_length", "ap_id_tp_marked_agent_input"],
     [
         (
             "notebooks/sample/wm_address_in_payload_format.json",
             "tests/shared/parsed_payload.pkl",
             2,
+            "A05003324172",
         ),
         (
             "notebooks/sample/wm_address_in_payload_format_2_input_3_match_records.json",
             "tests/shared/parsed_payload_2_payload.pkl",
             3,
+            "A05003324172",
         ),
         (
             "notebooks/sample/wm_party_payload_without_supplemental_info.json",
             "tests/shared/empty_payload.pkl",
             2,
+            "A05003324172",
         ),
         (
             "notebooks/sample/wm_party_payload_with_partial_supplemental_info.json",
             "tests/shared/empty_payload.pkl",
             2,
+            "A05003324172",
         ),
     ],
 )
-def test_pipeline(payload_file, reference_file, reference_length):
+def test_pipeline(payload_file, reference_file, reference_length, ap_id_tp_marked_agent_input):
     json_engine = JsonProcessingEngine(pipeline_config)
     uut = MSPipeline(json_engine, config=pipeline_config)
     parsed_payloads = run_pipeline(uut, payload_file)
-    assert_length_and_content_match(reference_length, parsed_payloads, reference_file)
+    assert_length_and_content_match(
+        reference_length, parsed_payloads, reference_file, ap_id_tp_marked_agent_input
+    )
+
+
+@pytest.fixture(scope="module")
+def pipeline_resource(request):
+    json_engine = JsonProcessingEngine(pipeline_config)
+    uut = MSPipeline(json_engine, config=pipeline_config)
+    yield uut
+
+
+@pytest.mark.parametrize(
+    ["match", "expected_result"],
+    [
+        (
+            {
+                cn.DATASET_TYPE: "ISG_PARTY",
+                "ap_id_tp_marked_agent_input": [
+                    "DUMMY_ISG_PARTY_ID",
+                    "DUMMY_ADDRESS_PARTY_ID",
+                    "DUMMY_ISG_ACCOUNT_ID",
+                ],
+            },
+            "DUMMY_ISG_PARTY_ID",
+        ),
+        (
+            {
+                cn.DATASET_TYPE: "WM_ADDRESS",
+                "ap_id_tp_marked_agent_input": [
+                    "DUMMY_ISG_PARTY_ID",
+                    "DUMMY_ADDRESS_PARTY_ID",
+                    "DUMMY_ISG_ACCOUNT_ID",
+                ],
+            },
+            "DUMMY_ADDRESS_PARTY_ID",
+        ),
+        (
+            {
+                cn.DATASET_TYPE: "WM_PARTY",
+                "ap_id_tp_marked_agent_input": [
+                    "DUMMY_ISG_PARTY_ID",
+                    "DUMMY_ADDRESS_PARTY_ID",
+                    "DUMMY_ISG_ACCOUNT_ID",
+                ],
+            },
+            "DUMMY_ADDRESS_PARTY_ID",
+        ),
+        (
+            {
+                cn.DATASET_TYPE: "ISG_ACCOUNT",
+                "ap_id_tp_marked_agent_input": [
+                    "DUMMY_ISG_PARTY_ID",
+                    "DUMMY_ADDRESS_PARTY_ID",
+                    "DUMMY_ISG_ACCOUNT_ID",
+                ],
+            },
+            "DUMMY_ISG_ACCOUNT_ID",
+        ),
+        (
+            {
+                cn.DATASET_TYPE: "UNKNOWN",
+                "ap_id_tp_marked_agent_input": [
+                    "DUMMY_ISG_PARTY_ID",
+                    "DUMMY_ADDRESS_PARTY_ID",
+                    "DUMMY_ISG_ACCOUNT_ID",
+                ],
+            },
+            "",
+        ),
+    ],
+)
+def test_select_ap_for_ap_id_tp_marked_agent(match, expected_result, pipeline_resource):
+    pipeline_resource.select_ap_for_ap_id_tp_marked_agent(match)
+    assert match["ap_id_tp_marked_agent_input"] == expected_result
+
+
+@pytest.mark.parametrize(
+    ["payload", "expected_result", "is_data_config_error_expected"],
+    [
+        (get_dummy_payload_for_dataset("DUMMY COLUMN"), None, True),
+        (get_dummy_payload_for_dataset("R_Global_MultiParty_Daily"), "ISG_PARTY", False),
+        (get_dummy_payload_for_dataset("R_GLOBAL_Us_Active_Address"), "WM_ADDRESS", False),
+        (get_dummy_payload_for_dataset("R_US_Active_Party"), "WM_PARTY", False),
+        (get_dummy_payload_for_dataset("R_Global_MultiParty_Wkly"), "ISG_PARTY", False),
+        (get_dummy_payload_for_dataset("R_Global_MultiAccounts_Daily_HK"), "ISG_ACCOUNT", False),
+        (get_dummy_payload_for_dataset("R_Global_MultiAccounts_Daily"), "ISG_ACCOUNT", False),
+        (
+            get_dummy_payload_for_dataset("R_Global_MultiAccounts_Daily_Chinese"),
+            "ISG_ACCOUNT",
+            False,
+        ),
+        (get_dummy_payload_for_dataset("R_Global_MultiAccounts_Wkly"), "ISG_ACCOUNT", False),
+        (
+            get_dummy_payload_for_dataset("R_Global_MultiAccounts_Wkly_Chinese"),
+            "ISG_ACCOUNT",
+            False,
+        ),
+        (
+            get_dummy_payload_for_dataset("R_Global_MultiAccounts_Wkly_EDDCBI"),
+            "ISG_ACCOUNT",
+            False,
+        ),
+    ],
+)
+def test_set_up_dataset_type_match(
+    payload, expected_result, is_data_config_error_expected, pipeline_resource
+):
+    match = {}
+    if is_data_config_error_expected:
+        with pytest.raises(DatasetConfigError):
+            pipeline_resource.set_up_dataset_type_match(payload, match)
+    else:
+        pipeline_resource.set_up_dataset_type_match(payload, match)
+        assert match[cn.DATASET_TYPE] == expected_result
