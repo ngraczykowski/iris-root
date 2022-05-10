57e521a7dc24ce0afeaad2029b2a552a0b675801
wiktor Kęska <wkeska@silenteight.com>
2022-05-10 11:33:24 +0200

    fix: cmapi callback disabled as default

--- modules/payments-bridge/sear-payments-bridge-firco/src/main/java/com/silenteight/payments/bridge/firco/callback/service/CallbackRequestProperties.java
+++ modules/payments-bridge/sear-payments-bridge-firco/src/main/java/com/silenteight/payments/bridge/firco/callback/service/CallbackRequestProperties.java
@@ -15,14 +15,14 @@ class CallbackRequestProperties {
 
   private String endpoint;
 
-  private boolean enabled;
+  private boolean enabled = false;
 
   /**
    * The name of the client registration in OAuth2 client configuration.
    * <p/>
    * For more information, see {@link Registration}.
    */
-  private String clientRegistrationId;
+  private String clientRegistrationId = "callback";
 
   private Duration connectionTimeout = Duration.ofSeconds(10);
 
