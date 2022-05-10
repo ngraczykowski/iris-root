7402c288e8eccd1452df87423af3340d03334819
Patryk Zajko <pzajko@silenteight.com>
2022-05-10 09:03:01 +0000

    INTERNAL: Missing outExchange in RabbitBrokerTestConfiguration fix (MI6-1523)
    Closes MI6-1523
    
    Reviewed-by: Bartosz Martofel <bmartofel@silenteight.com>
    Reviewed-by: Erwin Mużyczuk <emuzyczuk@silenteight.com>
    Merged from: https://gitlab.silenteight.com/ro/agent-commons/-/merge_requests/102

--- modules/agent-commons/agent-facade-core/src/test/java/com/silenteight/agent/facade/exchange/RabbitBrokerTestConfiguration.java
+++ modules/agent-commons/agent-facade-core/src/test/java/com/silenteight/agent/facade/exchange/RabbitBrokerTestConfiguration.java
@@ -2,12 +2,7 @@ package com.silenteight.agent.facade.exchange;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -21,6 +16,7 @@ import org.springframework.context.annotation.Conditional;
 public class RabbitBrokerTestConfiguration {
 
   static final String TEST_FACADE_OUT_QUEUE = "agent.outQueue";
+  private final AgentFacadeProperties agentFacadeProperties;
 
   private final AmqpAdmin amqpAdmin;
 
@@ -31,6 +27,15 @@ public class RabbitBrokerTestConfiguration {
         .build();
     amqpAdmin.declareQueue(queue);
     return queue;
+  }
+
+  @Bean("outExchange")
+  TopicExchange outExchange() {
+    TopicExchange exchange = ExchangeBuilder
+        .topicExchange(agentFacadeProperties.getOutboundExchangeName())
+        .build();
+    amqpAdmin.declareExchange(exchange);
+    return exchange;
   }
 
   @Bean
