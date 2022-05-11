debb5cf062b09fa5a62d157f6b463f0b7c08af29
Borys Wiśniewski <bwisniewski@silenteight.com>
2022-05-11 06:23:23 +0000

    Update nomad/deploy.sh

--- organization-name-agent/nomad/deploy.sh
+++ organization-name-agent/nomad/deploy.sh
@@ -2,13 +2,7 @@
 set  -e -o pipefail
 scriptdir="$(cd -- "$(dirname -- "${0}")" && pwd -P)"
 
-if [[ -z "$MINIO_ADDR" ]]; then
-    if [[ -x $(command -v consul) && -x $(command -v jq) ]]; then
-        minio_discovery=$(consul watch -type=service -service=minio | jq --raw-output '.[0].Service | [ .Address, (.Port | tostring) ] | join(":")')
-    fi
-
-    MINIO_ADDR="http://"${minio_discovery:-"localhost:9000"}
-fi
+MINIO_ADDR=https://minio.silenteight.com
 
 MINIO_ADDR="s3::"${MINIO_ADDR}
 
