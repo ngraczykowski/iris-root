014e05f9bce79173aa591169cdf93f89db27e15b
Mateusz Bartecki <mbartecki@silenteight.com>
2022-05-12 12:26:10 +0000

    feat(app): Learning data reprocessing - 3 files at once (AEP2-881)

--- modules/payments-bridge/sear-payments-bridge-app/src/main/java/com/silenteight/payments/bridge/app/batch/BatchProcessingConfiguration.java
+++ modules/payments-bridge/sear-payments-bridge-app/src/main/java/com/silenteight/payments/bridge/app/batch/BatchProcessingConfiguration.java
@@ -1,11 +1,13 @@
 package com.silenteight.payments.bridge.app.batch;
 
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -13,14 +15,18 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @EnableBatchProcessing
 @Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(BatchProcessingProperties.class)
 public class BatchProcessingConfiguration {
 
+  private final BatchProcessingProperties batchProcessingProperties;
 
   @Primary
   @SneakyThrows
   @Bean
   public JobLauncher searJobLauncher(final JobRepository jobRepository) {
     final SimpleAsyncTaskExecutor taskExecutor = new SimpleAsyncTaskExecutor();
+    taskExecutor.setConcurrencyLimit(batchProcessingProperties.getConcurrencyLimit());
     final SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
     jobLauncher.setJobRepository(jobRepository);
     jobLauncher.setTaskExecutor(taskExecutor);
