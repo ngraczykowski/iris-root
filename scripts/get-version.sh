6bf3e2390afffb112d4bc3707ac9cfada7785e33
Andrzej Haczewski <ahaczewski@silenteight.com>
2022-05-13 12:29:57 +0200

    ci: determine version without looking at the branch
    Signed-off-by: Andrzej Haczewski <ahaczewski@silenteight.com>

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -2,13 +2,8 @@
 set -e -o pipefail
 
 rev=${GIT_COMMIT:-origin/master}
-branch=${GIT_BRANCH:-$(git branch --no-color --show-current)}
 day=$(git show --no-patch --date=format:'%y%m%d' --pretty=format:'%cd' "$rev")
 commit_start=$(git show --no-patch --date=format:'%Y-%m-%d 00:00:00' --pretty=format:'%cd' "$rev")
 commit=$(git rev-list --count --since="$commit_start" "$rev")
 
-if [[ "$branch" =~ .*"master" ]]; then
-  echo "$day.$commit.0"
-else
-  echo "HEAD-SNAPSHOT"
-fi
+echo "$day.$commit.0"
