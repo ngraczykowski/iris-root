d5a0540a7705dfef673178f8299e11b693ee13fd
Andrzej Legucki <andrzej.legucki@silenteight.com>
2022-05-17 07:01:51 +0200

    Feature: Fix error handling in UserRestController#listUsersWithRole (WEB-2319)

--- modules/sep-usermanagement-api/src/main/java/com/silenteight/sep/usermanagement/api/role/RoleNotFoundException.java
+++ modules/sep-usermanagement-api/src/main/java/com/silenteight/sep/usermanagement/api/role/RoleNotFoundException.java
@@ -2,11 +2,17 @@ package com.silenteight.sep.usermanagement.api.role;
 
 import lombok.NonNull;
 
+import org.jetbrains.annotations.NotNull;
+
 public class RoleNotFoundException extends RuntimeException {
 
   private static final long serialVersionUID = -8575226276120649160L;
 
-  public RoleNotFoundException(@NonNull String roleName) {
-    super(String.format("Role with name %s was not found", roleName));
+  public RoleNotFoundException(@NotNull String roleName) {
+    this(roleName, null);
+  }
+
+  public RoleNotFoundException(@NonNull String roleName, Throwable cause) {
+    super(String.format("Role with name %s was not found", roleName), cause);
   }
 }
