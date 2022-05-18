512db4df46ce658d96c3bb6ec37e338c8e39ffea
Kordian Kicza <kordian.kicza@silenteight.com>
2022-05-18 11:44:12 +0200

    Extended reporting for Cucumber and Rest Assured

--- tests/e2e-tests/src/test/java/steps/Hooks.java
+++ tests/e2e-tests/src/test/java/steps/Hooks.java
@@ -1,14 +1,30 @@
 package steps;
 
+import io.cucumber.core.backend.TestCaseState;
+import io.cucumber.core.gherkin.Pickle;
+import io.cucumber.core.gherkin.Step;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.Scenario;
+import io.cucumber.plugin.event.TestCase;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+
+import lombok.SneakyThrows;
+
 import utils.AuthUtils;
+import utils.CustomLogFilter;
 import utils.ScenarioContext;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 public class Hooks {
 
   public static ScenarioContext scenarioContext = new ScenarioContext();
+  static CustomLogFilter customLogFilter = new CustomLogFilter();
+  int stepNo = 0;
+  String scenarioName = "";
 
   @BeforeAll
   public static void setupRestAssured() {
@@ -18,7 +34,50 @@ public class Hooks {
         .build()
         .header("Authorization", "Bearer " + new AuthUtils().getAuthToken())
         .given()
+        .filter(customLogFilter)
         .log()
         .all();
+  }
+
+  @AfterStep
+  public void afterStep(Scenario scenario) {
+    /*
+      Cucumber do not provide steps info, so We had to create this mechanism
+      We check scenario name to select index for desired step
+     */
+    String currentScenarioName = scenario.getName();
+    if (!scenarioName.equals(currentScenarioName)) {
+      scenarioName = currentScenarioName;
+      stepNo = 0;
+    }
+    List<Step> stepList = getStepListFromScenario(scenario);
+    String currentStepName = stepList.get(stepNo).getText();
+
+    if (!currentStepName.startsWith("Prepare")) {
+      scenario.attach(
+          "\n" + "API Request: " + customLogFilter.getRequestBuilder()
+              + "\n" + "API Response: " + customLogFilter.getResponseBuilder(), "application/json",
+          "API communication for: " + currentStepName);
+    }
+    stepNo++;
+  }
+
+  @SneakyThrows
+  private List<Step> getStepListFromScenario(Scenario scenario) {
+    /*
+      Hack to get cucumber steps name
+     */
+    Field f = scenario.getClass().getDeclaredField("delegate");
+    f.setAccessible(true);
+    TestCaseState testCaseState = (TestCaseState) f.get(scenario);
+    Field x = testCaseState.getClass().getDeclaredField("testCase");
+    x.setAccessible(true);
+    TestCase testCase = (TestCase) x.get(testCaseState);
+    testCase.getTestSteps();
+    Field y = testCase.getClass().getDeclaredField("pickle");
+    y.setAccessible(true);
+    Pickle pickle = (Pickle) y.get(testCase);
+
+    return pickle.getSteps();
   }
 }
---
+++ tests/e2e-tests/src/test/java/utils/CustomLogFilter.java
@@ -0,0 +1,91 @@
+package utils;
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
+import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
+
+public class CustomLogFilter implements Filter {
+  private StringBuilder requestBuilderLogs;
+  private StringBuilder responseBuilderLogs;
+
+  @Override
+  public Response filter(FilterableRequestSpecification filterableRequestSpecification, FilterableResponseSpecification filterableResponseSpecification, FilterContext filterContext) {
+    Response response = filterContext.next(filterableRequestSpecification, filterableResponseSpecification);
+    requestBuilderLogs = new StringBuilder();
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Request method: ")
+        .append(objectValidation(filterableRequestSpecification.getMethod()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Request URI: ")
+        .append(objectValidation(filterableRequestSpecification.getURI()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Form Params: ")
+        .append(objectValidation(filterableRequestSpecification.getFormParams()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Request Param: ")
+        .append(objectValidation(filterableRequestSpecification.getRequestParams()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Headers: ")
+        .append(objectValidation(filterableRequestSpecification.getHeaders()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Cookies: ")
+        .append(objectValidation(filterableRequestSpecification.getCookies()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Proxy: ")
+        .append(objectValidation(filterableRequestSpecification.getProxySpecification()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs
+        .append("Body: ")
+        .append(objectValidation(filterableRequestSpecification.getBody()));
+    requestBuilderLogs.append("\n");
+    requestBuilderLogs.append("******************************");
+    responseBuilderLogs = new StringBuilder();
+    responseBuilderLogs.append("""
+
+
+
+        """);
+    responseBuilderLogs.append("Status Code: ")
+        .append(response.getStatusCode());
+    responseBuilderLogs.append("\n");
+    responseBuilderLogs.append("Status Line: ")
+        .append(response.getStatusLine());
+    responseBuilderLogs.append("\n");
+    responseBuilderLogs.append("Response Cookies: ")
+        .append(response.getDetailedCookies());
+    responseBuilderLogs.append("\n");
+    responseBuilderLogs.append("Response Content Type: ")
+        .append(response.getContentType());
+    responseBuilderLogs.append("\n");
+    responseBuilderLogs.append("Response Headers: ")
+        .append(response.getHeaders());
+    responseBuilderLogs.append("\n");
+    responseBuilderLogs.append("Response Body: " + "\n")
+        .append(response.getBody().prettyPrint());
+    return response;
+  }
+
+  public String getRequestBuilder() {
+    return requestBuilderLogs.toString();
+  }
+
+  public String getResponseBuilder() {
+    return responseBuilderLogs.toString();
+  }
+
+  public String objectValidation(Object o) {
+    if (o == null)
+      return null;
+    else
+      return o.toString();
+  }
+
+}
