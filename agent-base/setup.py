c6315653c22a8a336df4ec011147e6db1824ea40
Jenkins <jenkins@silenteight.com>
2022-05-24 13:25:35 +0000

    Bump version: 0.21.0-dev → 0.22.0-dev

--- agent-base/setup.py
+++ agent-base/setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name="agent-base",
-    version="0.21.0-dev",
+    version="0.22.0-dev",
     description="",
     author="Silent Eight Pte. Ltd.",
     author_email="support@silenteight.com",
