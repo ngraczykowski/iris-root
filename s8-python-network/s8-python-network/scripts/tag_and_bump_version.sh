acfae4f940592b478ed89df2b0439d59dda2ca9e
ksiwecki <ksiwecki@silenteight.com>
2022-05-27 16:15:12 +0200

    tox out from tag_and_bump_version.sh

--- s8-python-network/s8-python-network/scripts/tag_and_bump_version.sh
+++ s8-python-network/s8-python-network/scripts/tag_and_bump_version.sh
@@ -6,6 +6,6 @@ git tag ${VERSION}
 git push --tags origin HEAD:master
 
 echo 'Bumping version - patch for master'
-pip install --index-url "${PYPI_URL}" bump2version tox
+pip install --index-url "${PYPI_URL}" bump2version
 bump2version patch
 git push origin HEAD:master
