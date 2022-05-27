f3c97b48b577ce6f43aa6459ef8e0fc519ccecc8
ksiwecki <ksiwecki@silenteight.com>
2022-05-27 14:34:34 +0200

    tag before bumpversion -> bump for commit only

--- s8-python-network/s8-python-network/scripts/release.sh
+++ s8-python-network/s8-python-network/scripts/release.sh
@@ -7,8 +7,8 @@ function gitRelease() {
   echo 'Building'
   scripts/clean.sh
   scripts/build.sh "$@"
-  echo 'Resetting release commit'
-  git reset --hard HEAD~1 # remove release commit after tag
+#  echo 'Resetting release commit'
+#  git reset --hard HEAD~1 # remove release commit after tag
 }
 
 branch="${CI_COMMIT_REF_NAME:-}"
@@ -19,6 +19,8 @@ fi
 if [ "${branch}" == "master" ]; then
   echo 'Releasing - bumping minor'
   gitRelease "$@"
+  echo 'Bumping patch'
+  bump2version patch # for master
 else
   echo 'Not a release branch'
   exit 0
