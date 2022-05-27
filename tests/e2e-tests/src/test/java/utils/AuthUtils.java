82fd7b066a4103674c476143849d12ffffb63c82
Damian Śnieżek <dsniezek@silenteight.com>
2022-05-27 16:34:18 +0200

    feat: auth.dev.s8ops.com as default keycloak (ARC-87) (#65)

--- tests/e2e-tests/src/test/java/utils/AuthUtils.java
+++ tests/e2e-tests/src/test/java/utils/AuthUtils.java
@@ -3,14 +3,17 @@ package utils;
 import static io.restassured.RestAssured.*;
 
 public class AuthUtils {
+  private static final String OIDC_TOKEN_ENDPOINT_TEMPLATE = "%s/realms/%s/protocol/openid-connect/token";
 
   public String getAuthToken() {
+    String realm = System.getProperty("test.realm");
+    String authServerUrl = System.getProperty("test.authServerUrl");
     return given()
         .param("grant_type", "password")
         .param("username", System.getProperty("test.username"))
         .param("password", System.getProperty("test.password"))
         .param("client_id", System.getProperty("test.clientId"))
-        .post("https://auth.silenteight.com/realms/sens-webapp/protocol/openid-connect/token")
+        .post(String.format(OIDC_TOKEN_ENDPOINT_TEMPLATE, authServerUrl, realm))
         .then()
         .statusCode(200)
         .extract()
