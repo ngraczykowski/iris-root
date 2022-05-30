8c34c9388716cb3671b3d6f10776ca1b49b25cf6
Jenkins <jenkins@silenteight.com>
2022-05-30 13:56:42 +0000

    Bump version: 0.2.3 → 0.2.4

--- s8-python-network/s8-python-network/setup.py
+++ s8-python-network/s8-python-network/setup.py
@@ -27,7 +27,7 @@ tests_require = [
 
 setup(
     name="s8-python-network",
-    version="0.2.3",
+    version="0.2.4",
     description="",
     author="Silent Eight Pte. Ltd.",
     author_email="support@silenteight.com",
