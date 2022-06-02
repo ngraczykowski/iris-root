4a5ee4c4f5b28ed35f94de9e71629ddec383089d
Maciej Głowiński <mglowinski@silenteight.com>
2022-06-02 06:18:04 +0000

    INTERNAL: set transactionManager as primary - fix for error with no unique bean on creating transaction in spring-batch (ALFA-289)
    INTERNAL: set transactionManager as primary - fix for error with no unique bean on creating transaction in spring-batch (ALFA-289)
    
    Closes ALFA-289
    
    Reviewed-by: Norbert Graczykowski <ngraczykowski@silenteight.com>
    Reviewed-by: Sławomir Korbas <skorbas@silenteight.com>
    Merged from: https://gitlab.silenteight.com/sens/scb-bridge/-/merge_requests/207

--- modules/scb-bridge/bridge-app/src/main/java/com/silenteight/scb/ingest/adapter/incomming/common/config/SyncDataSourcesConfiguration.java
+++ modules/scb-bridge/bridge-app/src/main/java/com/silenteight/scb/ingest/adapter/incomming/common/config/SyncDataSourcesConfiguration.java
@@ -1,24 +1,50 @@
 package com.silenteight.scb.ingest.adapter.incomming.common.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import com.silenteight.scb.ingest.adapter.incomming.common.domain.GnsSyncConstants;
 import com.silenteight.scb.ingest.adapter.incomming.common.mode.OnAlertProcessorCondition;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class SyncDataSourcesConfiguration {
 
   private final ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers;
+  private final ApplicationContext context;
+
+  @PostConstruct
+  public void setTransactionManagerBeanPrimary() {
+    ConfigurableListableBeanFactory beanFactory =
+        ((AbstractApplicationContext) context).getBeanFactory();
+    try {
+      BeanDefinition transactionManager =
+          beanFactory.getBeanDefinition(GnsSyncConstants.PRIMARY_TRANSACTION_MANAGER);
+      transactionManager.setPrimary(true);
+    } catch (NoSuchBeanDefinitionException ex) {
+      log.error("Transaction manager bean not found. Cannot make it primary", ex);
+    }
+  }
 
   @Bean
   @Primary
