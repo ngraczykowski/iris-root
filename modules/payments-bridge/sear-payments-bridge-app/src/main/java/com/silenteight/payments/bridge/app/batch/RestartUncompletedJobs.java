324587f446fc87f759c176c4a66a5e682f60e9ad
ppietruha <106063053+ppietruha@users.noreply.github.com>
2022-06-07 12:07:29 +0200

    feat(pb): used `pb.batch.restart-failed-jobs` property to restart unc… (#158)
    feat(pb): used `pb.batch.restart-failed-jobs` property to restart uncompleted jobs, default value: `false`

--- modules/payments-bridge/sear-payments-bridge-app/src/main/java/com/silenteight/payments/bridge/app/batch/RestartUncompletedJobs.java
+++ modules/payments-bridge/sear-payments-bridge-app/src/main/java/com/silenteight/payments/bridge/app/batch/RestartUncompletedJobs.java
@@ -3,17 +3,24 @@ package com.silenteight.payments.bridge.app.batch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@ConditionalOnProperty(
+    value = "pb.batch.restart-failed-jobs",
+    havingValue = "true"
+)
 class RestartUncompletedJobs {
 
   private final JobMaintainer jobMaintainer;
 
+  @Async
   @EventListener(classes = { ApplicationStartedEvent.class })
   public void onApplicationStartup() {
     log.info("Uncompleted job restart procedure has been started.");
