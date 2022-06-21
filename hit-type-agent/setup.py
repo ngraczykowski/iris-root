0932ba635e5ccef7ef673ed513b41a4dc64817d0
jenkins <jenkins@silenteight.com>
2022-06-21 11:28:57 +0000

    Bump version: 0.4.0 → 0.4.1

--- hit-type-agent/setup.py
+++ hit-type-agent/setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name="hit-type",
-    version="0.4.0",
+    version="0.4.1",
     description="",
     author="Silent Eight Pte. Ltd.",
     author_email="support@silenteight.com",
