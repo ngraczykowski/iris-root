cd96f43cca2760ef3a4c691ffbc40c4ae00ab27a
Kordian Kicza <105921514+kkicza@users.noreply.github.com>
2022-06-23 09:59:10 +0200

    feat(e2e): Auto chromedriver setup (#348)
    Auto chromedriver setup

--- tests/e2e-tests/src/test/java/steps/Hooks.java
+++ tests/e2e-tests/src/test/java/steps/Hooks.java
@@ -1,5 +1,7 @@
 package steps;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import lombok.SneakyThrows;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -40,6 +42,7 @@ public class Hooks implements En {
     Before(
         0,
         (Scenario scenario) -> {
+          WebDriverManager.chromedriver().setup();
           scenarioContext.clearScenarioContext();
           scenarioContext.setUser(
               "admin",
