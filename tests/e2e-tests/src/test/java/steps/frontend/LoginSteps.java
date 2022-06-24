ffcdc454f1e8ace4da203cb6d1366a3a3ba38337
Kordian Kicza <105921514+kkicza@users.noreply.github.com>
2022-06-24 12:34:29 +0200

    feat(e2e): Session clear mechanism Selenide (#370)
    Session clear mechanism Selenide

--- tests/e2e-tests/src/test/java/steps/frontend/LoginSteps.java
+++ tests/e2e-tests/src/test/java/steps/frontend/LoginSteps.java
@@ -1,5 +1,6 @@
 package steps.frontend;
 
+import com.codeborne.selenide.Selenide;
 import io.cucumber.java8.En;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -8,6 +9,12 @@ import static utils.pom.LoginPage.loginAsUserViaFrontend;
 public class LoginSteps implements En {
   public LoginSteps() {
     Given("User login as admin", () -> {
+      //Temporary workaround for session clear
+      try {
+        Selenide.webdriver().object().close();
+      } catch (Exception e) {
+        System.out.println("Webdriver is not alive");
+      }
       open(System.getProperty("test.url"));
       loginAsUserViaFrontend(System.getProperty("test.admin.username"), System.getProperty("test.admin.password"));
     });
