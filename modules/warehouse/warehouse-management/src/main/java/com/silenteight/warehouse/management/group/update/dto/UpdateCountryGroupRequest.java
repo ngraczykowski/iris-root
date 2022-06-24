70405ed2b1a08899b19f6a398cd95d495e4906fd
Rafal Pajdak <30751691+rpajdak@users.noreply.github.com>
2022-06-24 12:50:31 +0200

    feat(wh): Prepare the regex validation for Warehouse (WEB-2377) (#371)

--- modules/warehouse/warehouse-management/src/main/java/com/silenteight/warehouse/management/group/update/dto/UpdateCountryGroupRequest.java
+++ modules/warehouse/warehouse-management/src/main/java/com/silenteight/warehouse/management/group/update/dto/UpdateCountryGroupRequest.java
@@ -5,11 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+import javax.validation.constraints.Pattern;
+
+import static com.silenteight.warehouse.common.web.rest.RestValidationConstants.FIELD_REGEX;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateCountryGroupRequest {
 
   @NonNull
+  @Pattern(regexp = FIELD_REGEX)
   private String name;
 }
--- modules/warehouse/warehouse-management/src/test/java/com/silenteight/warehouse/management/group/update/UpdateCountryGroupControllerTest.java
+++ modules/warehouse/warehouse-management/src/test/java/com/silenteight/warehouse/management/group/update/UpdateCountryGroupControllerTest.java
@@ -3,11 +3,15 @@ package com.silenteight.warehouse.management.group.update;
 import com.silenteight.warehouse.common.testing.rest.BaseRestControllerTest;
 import com.silenteight.warehouse.common.testing.rest.testwithrole.TestWithRole;
 import com.silenteight.warehouse.management.group.domain.CountryGroupService;
+import com.silenteight.warehouse.management.group.domain.dto.CountryGroupDto;
 import com.silenteight.warehouse.management.group.update.dto.UpdateCountryGroupRequest;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static com.silenteight.warehouse.common.opendistro.roles.RolesFixtures.COUNTRY_GROUP_ID;
 import static com.silenteight.warehouse.common.testing.rest.TestRoles.*;
@@ -16,6 +20,8 @@ import static com.silenteight.warehouse.management.group.CountryGroupFixtures.NA
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.anything;
 import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
@@ -48,5 +54,27 @@ class UpdateCountryGroupControllerTest extends BaseRestControllerTest {
     patch(UPDATE_COUNTRY_GROUP_URL, dto)
         .contentType(anything())
         .statusCode(FORBIDDEN.value());
+  }
+
+  @ParameterizedTest
+  @MethodSource("com.silenteight.warehouse.management.group.CountryGroupFixtures#getForbiddenCharsAsInput")
+  @WithMockUser(username = USERNAME, authorities = USER_ADMINISTRATOR)
+  void its400_whenUpdateCountryGroupRequestContainsForbiddenCharacters(String name) {
+    UpdateCountryGroupRequest dto = new UpdateCountryGroupRequest(name);
+
+    patch(UPDATE_COUNTRY_GROUP_URL, dto)
+        .contentType(anything())
+        .statusCode(BAD_REQUEST.value());
+  }
+
+  @ParameterizedTest
+  @MethodSource("com.silenteight.warehouse.management.group.CountryGroupFixtures#getAllowedCharsAsInput")
+  @WithMockUser(username = USERNAME, authorities = USER_ADMINISTRATOR)
+  void its201_whenUpdateCountryGroupRequestContainsAllowedCharacters(String name) {
+    UpdateCountryGroupRequest dto = new UpdateCountryGroupRequest(name);
+
+    patch(UPDATE_COUNTRY_GROUP_URL, dto)
+        .contentType(anything())
+        .statusCode(NO_CONTENT.value());
   }
 }
