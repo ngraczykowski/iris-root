1896632fe1823a01b1fc999bf41597ec8942f9d7
Jacek Gajewski <106075208+jgajewskis8@users.noreply.github.com>
2022-06-27 12:54:11 +0200

    refactor(pb,uds): added some logs regarding communication between pb and uds (#410)
    refactor(pb,uds): added some logs regarding communication between pb and uds (#410)

--- modules/payments-bridge/sear-payments-bridge-datasource/src/main/java/com/silenteight/payments/bridge/datasource/category/infrastructure/values/DatasourceCategoryValueClient.java
+++ modules/payments-bridge/sear-payments-bridge-datasource/src/main/java/com/silenteight/payments/bridge/datasource/category/infrastructure/values/DatasourceCategoryValueClient.java
@@ -3,15 +3,13 @@ package com.silenteight.payments.bridge.datasource.category.infrastructure.value
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import com.silenteight.datasource.categories.api.v2.BatchCreateCategoryValuesRequest;
-import com.silenteight.datasource.categories.api.v2.BatchCreateCategoryValuesResponse;
+import com.silenteight.datasource.categories.api.v2.*;
 import com.silenteight.datasource.categories.api.v2.CategoryValueServiceGrpc.CategoryValueServiceBlockingStub;
-import com.silenteight.datasource.categories.api.v2.CreateCategoryValuesRequest;
-import com.silenteight.datasource.categories.api.v2.CreatedCategoryValue;
 
 import io.grpc.Deadline;
 
 import java.time.Duration;
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -28,58 +26,75 @@ final class DatasourceCategoryValueClient {
 
   private final ExecutorService datasourceThreadPool;
 
+  void create(BatchCreateCategoryValuesRequest createCategoryValuesRequest) {
+    if (!createCategoryValuesRequest.getRequestsList().isEmpty()) {
+      sendToDatasource(createCategoryValuesRequest);
+    } else {
+      log.debug("Batch category value request is empty. Data won't be send to datasource service");
+    }
+  }
+
+  private void sendToDatasource(BatchCreateCategoryValuesRequest createCategoryValuesRequest) {
+    CompletableFuture.runAsync(
+            () -> {
+              var deadline = Deadline.after(timeout.toMillis(), TimeUnit.MILLISECONDS);
+
+              logRequest(createCategoryValuesRequest);
+
+              var response =
+                  blockingStub
+                      .withDeadline(deadline)
+                      .batchCreateCategoryValues(createCategoryValuesRequest);
+
+              logResponse(response);
+            },
+            this.datasourceThreadPool)
+        .exceptionally(
+            throwable -> {
+              log.error("Request with category values to the datasource service failed", throwable);
+              return null;
+            });
+  }
+
   private static void logRequest(BatchCreateCategoryValuesRequest createCategoryValuesRequest) {
     if (log.isDebugEnabled()) {
-      var categoryValues = createCategoryValuesRequest.getRequestsList();
-      var categoryNames = categoryValues.stream()
-          .map(CreateCategoryValuesRequest::getCategory)
-          .distinct()
-          .collect(toList());
+      var categoryValuesRequest = createCategoryValuesRequest.getRequestsList();
+      var categoryNames =
+          categoryValuesRequest.stream()
+              .map(CreateCategoryValuesRequest::getCategory)
+              .distinct()
+              .collect(toList());
 
-      log.debug("Sending category values request: count={}, categories={}",
-          categoryValues.size(), categoryNames);
+      var matchNames =
+          categoryValuesRequest.stream()
+              .map(CreateCategoryValuesRequest::getCategoryValuesList)
+              .flatMap(Collection::stream)
+              .map(CategoryValue::getMatch)
+              .distinct()
+              .limit(10)
+              .collect(toList());
+
+      log.debug(
+          "Sending category values request: count={}, categories={}, firstTenMatches={}",
+          categoryValuesRequest.size(),
+          categoryNames,
+          matchNames);
     }
   }
 
   private static void logResponse(BatchCreateCategoryValuesResponse response) {
     if (log.isDebugEnabled()) {
 
-      var matchesSaved = response.getCreatedCategoryValuesList().stream()
-          .map(CreatedCategoryValue::getMatch)
-          .distinct()
-          .collect(toList());
+      var matchesSaved =
+          response.getCreatedCategoryValuesList().stream()
+              .map(CreatedCategoryValue::getMatch)
+              .distinct()
+              .collect(toList());
 
       log.debug(
           "Category values saved for matches, matchCount={}, firstTenMatches={}",
           matchesSaved.size(),
           matchesSaved.subList(0, Math.min(10, matchesSaved.size())));
     }
-  }
-
-  void create(BatchCreateCategoryValuesRequest createCategoryValuesRequest) {
-    if (!createCategoryValuesRequest.getRequestsList().isEmpty()) {
-      sendToDatasource(createCategoryValuesRequest);
-    } else {
-      log.debug(
-          "Batch category value request is empty. Data won't be send to datasource service");
-    }
-  }
-
-  private void sendToDatasource(BatchCreateCategoryValuesRequest createCategoryValuesRequest) {
-    CompletableFuture.runAsync(() -> {
-      var deadline = Deadline.after(timeout.toMillis(), TimeUnit.MILLISECONDS);
-
-      logRequest(createCategoryValuesRequest);
-
-      var response = blockingStub
-          .withDeadline(deadline)
-          .batchCreateCategoryValues(createCategoryValuesRequest);
-
-      logResponse(response);
-
-    }, this.datasourceThreadPool).exceptionally(throwable -> {
-      log.error("Request with category values to the datasource service failed", throwable);
-      return null;
-    });
   }
 }
--- modules/universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/category/service/CreateCategoryValuesService.java
+++ modules/universal-data-source/universal-data-source-app/src/main/java/com/silenteight/universaldatasource/app/category/service/CreateCategoryValuesService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import com.silenteight.datasource.categories.api.v2.BatchCreateCategoryValuesResponse;
+import com.silenteight.datasource.categories.api.v2.CategoryValue;
 import com.silenteight.datasource.categories.api.v2.CreateCategoryValuesRequest;
 import com.silenteight.datasource.categories.api.v2.CreatedCategoryValue;
 import com.silenteight.sep.base.aspects.metrics.Timed;
@@ -13,6 +14,7 @@ import com.silenteight.universaldatasource.app.category.port.outgoing.CategoryVa
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -28,29 +30,47 @@ class CreateCategoryValuesService implements CreateCategoryValuesUseCase {
 
   @Timed(
       value = "uds.category.use_cases",
-      extraTags = { "action", "createCategoryValues" },
+      extraTags = {"action", "createCategoryValues"},
       histogram = true,
-      percentiles = { 0.5, 0.95, 0.99 }
-  )
+      percentiles = {0.5, 0.95, 0.99})
   @Override
   public BatchCreateCategoryValuesResponse createCategoryValues(
       List<CreateCategoryValuesRequest> categoryValues) {
 
+    logRequest(categoryValues);
+
     var createdCategoryValues = saveCategoryValues(categoryValues);
 
-    if (log.isDebugEnabled()) {
-      var categoryNames = categoryValues.stream()
-          .map(CreateCategoryValuesRequest::getCategory)
-          .distinct()
-          .collect(toList());
-
-      log.debug("Saved category values: count={}, categories={}",
-          createdCategoryValues.size(), categoryNames);
-    }
+    logRepositoryResponse(categoryValues, createdCategoryValues);
 
     return BatchCreateCategoryValuesResponse.newBuilder()
         .addAllCreatedCategoryValues(createdCategoryValues)
         .build();
+  }
+
+  private static void logRequest(List<CreateCategoryValuesRequest> categoryValues) {
+    if (log.isDebugEnabled()) {
+      var categoryNames =
+          categoryValues.stream()
+              .map(CreateCategoryValuesRequest::getCategory)
+              .distinct()
+              .collect(toList());
+
+      var matchNames =
+          categoryValues.stream()
+              .map(CreateCategoryValuesRequest::getCategoryValuesList)
+              .flatMap(Collection::stream)
+              .map(CategoryValue::getMatch)
+              .distinct()
+              .limit(10)
+              .collect(toList());
+
+      log.debug(
+          "Saving category values: count={}, categories={}, firstTenMatches={}",
+          categoryValues.size(),
+          categoryNames,
+          matchNames);
+    }
   }
 
   private List<CreatedCategoryValue> saveCategoryValues(
@@ -59,8 +79,25 @@ class CreateCategoryValuesService implements CreateCategoryValuesUseCase {
     return categoryValueDataAccess.saveAll(categoryValues);
   }
 
-  private void validateCategoryValueBatch(
-      List<CreateCategoryValuesRequest> categoryValues) {
+  private void validateCategoryValueBatch(List<CreateCategoryValuesRequest> categoryValues) {
     validateCategoryValue.isValid(categoryValues);
+  }
+
+  private static void logRepositoryResponse(
+      List<CreateCategoryValuesRequest> categoryValues,
+      List<CreatedCategoryValue> createdCategoryValues) {
+
+    if (log.isDebugEnabled()) {
+      var categoryNames =
+          categoryValues.stream()
+              .map(CreateCategoryValuesRequest::getCategory)
+              .distinct()
+              .collect(toList());
+
+      log.debug(
+          "Saved category values: count={}, categories={}",
+          createdCategoryValues.size(),
+          categoryNames);
+    }
   }
 }
