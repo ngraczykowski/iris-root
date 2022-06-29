3002eca7d91f4691220268d49d9e5b5251fb0117
ksiwecki <ksiwecki@silenteight.com>
2022-06-29 17:05:26 +0200

    use agent-base 0.23.3 with uds reconnect timeout

--- organization-name-agent/setup.py
+++ organization-name-agent/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 install_require = [
-    "agent-base==0.23.0",
+    "agent-base==0.23.3",
     "agents-api~=0.27.0",
     "aiohttp==3.7.4.post0",
     "data-source-api~=0.18.0",
