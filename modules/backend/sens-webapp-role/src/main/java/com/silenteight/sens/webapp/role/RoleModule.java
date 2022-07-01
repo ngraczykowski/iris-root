757a10e369551b8d9ba11241e0c1c2a2925d32c0
Rafal Pajdak <rpajdak@silenteight.com>
2022-07-01 08:08:10 +0200

    feat: Remove sens-webapp-role and code related to it in sens-webapp-user(S8DEV-476)

--- modules/backend/sens-webapp-role/src/main/java/com/silenteight/sens/webapp/role/RoleModule.java
+++
@@ -1,4 +0,0 @@
-package com.silenteight.sens.webapp.role;
-
-public interface RoleModule {
-}
--- modules/backend/sens-webapp-role/src/main/java/com/silenteight/sens/webapp/role/validate/RoleAssignmentValidator.java
+++
@@ -1,8 +0,0 @@
-package com.silenteight.sens.webapp.role.validate;
-
-import lombok.NonNull;
-
-public interface RoleAssignmentValidator {
-
-  boolean isAssigned(@NonNull String roleName);
-}
