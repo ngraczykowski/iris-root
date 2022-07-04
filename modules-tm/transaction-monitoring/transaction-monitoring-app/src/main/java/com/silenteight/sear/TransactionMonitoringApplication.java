27c58232c4a55d3d566341dfc3dcb7e636b3fe86
Wiktor Kęska <106071611+wkeska@users.noreply.github.com>
2022-07-04 14:57:54 +0200

    feat(tm): Added walking skeleton for transaction monitoring (TM-8) (#527)
    We added new Gradle modules with dummy application to prepare skeleton for further development

---
+++ modules-tm/transaction-monitoring/transaction-monitoring-app/src/main/java/com/silenteight/sear/TransactionMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.silenteight.sear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMonitoringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TransactionMonitoringApplication.class, args);
+  }
+}
---
+++ modules-tm/transaction-monitoring/transaction-monitoring-app/src/test/java/com/silenteight/sear/HelloWorldControllerTest.java
@@ -0,0 +1,24 @@
+package com.silenteight.sear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HelloWorldControllerTest {
+
+  @LocalServerPort private int port;
+
+  @Autowired private TestRestTemplate restTemplate;
+
+  @Test
+  void dummyReturnsMessage() {
+    assertThat(restTemplate.getForObject("http://localhost:" + port + "/hello", String.class))
+        .isEqualTo("Hello World!");
+  }
+}
